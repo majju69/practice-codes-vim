0ff20e56d5c289e06caf2749f9a47ba0113d5cda
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-12 20:55:27 +0530

    https://codeforces.com/contest/1111/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,76 +7,23 @@ using namespace std;
 	#define debug(x)
 #endif
 
-map<string,int> mp={{"DR",0},{"DL",1},{"UR",2},{"UL",3}};
+typedef long long ll;
 
-inline pair<pair<int,int>,int> get(int i,int j,int n,int m,int d)
+ll solve(ll lo,ll hi,ll a,ll b,vector<ll> &pos)
 {
-	if(d==0)
+	if(lo==hi)
 	{
-		int x=i+1,y=j+1;
-		if(x<n&&y<m)
-		{
-			return {{x,y},0};
-		}
-		if(x>=n&&y>=m)
-		{
-			return {{i-1,j-1},3};
-		}
-		if(x<n&&y>=m)
-		{
-			return {{x,j-1},1};
-		}
-		return {{i-1,y},2};
+		ll cnt=upper_bound(pos.begin(),pos.end(),hi)-lower_bound(pos.begin(),pos.end(),lo);
+		return ((cnt==0)?a:b*cnt);
 	}
-	if(d==1)
+	ll len=hi-lo+1; 
+	ll cnt=upper_bound(pos.begin(),pos.end(),hi)-lower_bound(pos.begin(),pos.end(),lo);
+	if(cnt==0)
 	{
-		int x=i+1,y=j-1;
-		if(x<n&&y>=0)
-		{
-			return {{x,y},1};
-		}
-		if(x>=n&&y<0)
-		{
-			return {{i-1,j+1},2};
-		}
-		if(x<n&&y<0)
-		{
-			return {{x,j+1},0};
-		}
-		return {{i-1,y},3};
+		return a;
 	}
-	if(d==2)
-	{
-		int x=i-1,y=j+1;
-		if(x>=0&&y<m)
-		{
-			return {{x,y},2};
-		}
-		if(x<0&&y>=m)
-		{
-			return {{i+1,j-1},1};
-		}
-		if(x>=0&&y>=m)
-		{
-			return {{x,j-1},3};
-		}
-		return {{i+1,y},0};
-	}
-	// d==3
-	int x=i-1,y=j-1;
-	if(x>=0&&y>=0)
-	{
-		return {{x,y},3};
-	}
-	if(x<0&&y<0)
-	{
-		return {{i+1,j+1},0};
-	}
-	if(x>=0&&y<0)
-	{
-		return {{x,j+1},2};
-	}
-	return {{i+1,y},1};
+	ll ans1=solve(lo,lo+(len>>1)-1,a,b,pos)+solve(lo+(len>>1),hi,a,b,pos),ans2=b*cnt*len;
+	return min(ans1,ans2);
 }
 
 int main()
@@ -84,38 +31,15 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	ll n,k,a,b;
+	cin>>n>>k>>a>>b;
+	vector<ll> pos(k);
+	for(auto &v:pos)
 	{
-		int n,m,i1,j1,i2,j2,ans=-1;
-		string d;
-		queue<pair<pair<int,int>,pair<int,int>>> q;
-		cin>>n>>m>>i1>>j1>>i2>>j2>>d;
-		i1--;
-		j1--;
-		i2--;
-		j2--;
-		vector<vector<vector<bool>>> vis(n,vector<vector<bool>>(m,vector<bool>(4,0)));
-		q.push({{i1,j1},{mp[d],0}});
-		vis[i1][j1][mp[d]]=1;
-		while(q.size())
-		{
-			int i=q.front().first.first,j=q.front().first.second,dir=q.front().second.first,dist=q.front().second.second;
-			q.pop();
-			if(i==i2&&j==j2)
-			{
-				ans=dist;
-				break;
-			}
-			pair<pair<int,int>,int> p=get(i,j,n,m,dir);
-			if(!vis[p.first.first][p.first.second][p.second])
-			{
-				vis[p.first.first][p.first.second][p.second]=1;
-				q.push({{p.first.first,p.first.second},{p.second,dist+(dir!=p.second)}});
-			}
-		}
-		cout<<ans<<'\n';
+		cin>>v;
+		v--;
 	}
+	sort(pos.begin(),pos.end());
+	cout<<solve(0,(1LL<<n)-1,a,b,pos)<<'\n';
 	return 0;
 }
