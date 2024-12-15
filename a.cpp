dcb787c89c008a1ad296317959851bb3f0e88070
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-16 05:22:20 +0530

    https://codeforces.com/contest/1185/problem/C2 accepted

--- a.cpp
+++ a.cpp
@@ -12,85 +12,31 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,m;
+	map<int,int> mp;
+	cin>>n>>m;
+	for(int i=0;i<n;++i)
 	{
-		int n,zero=0;
-		deque<int> pos,neg;
-		cin>>n;
-		for(int i=0;i<n;++i)
+		int x,sum=0,cnt=1;
+		cin>>x;
+		sum+=x;
+		for(auto &v:mp)
 		{
-			int x;
-			cin>>x;
-			if(x<0)
+			if(sum+v.first*v.second<=m)
 			{
-				neg.push_back(x);
-			}
-			else if(x>0)
-			{
-				pos.push_back(x);
+				sum+=v.first*v.second;
+				cnt+=v.second;
 			}
 			else
 			{
-				zero++;
+				// sum+x*v.first<=m
+				// x<=(m-sum)/v.first
+				cnt+=(m-sum)/v.first;
+				break;
 			}
 		}
-		if(zero==n)
-		{
-			cout<<"No\n";
-		}
-		else
-		{
-			cout<<"Yes\n";
-			for(int i=0;i<zero;++i)
-			{
-				cout<<0<<' ';
-			}
-			int ptr=0,sum=0;
-			while((int)pos.size()>0||(int)neg.size()>0)
-			{
-				if(ptr==0)
-				{
-					if((int)pos.size()>0)
-					{
-						cout<<pos[0]<<' ';
-						sum+=pos[0];
-						pos.pop_front();
-					}
-					else
-					{
-						cout<<neg[0]<<' ';
-						sum+=neg[0];
-						neg.pop_front();
-					}
-					if(sum>0)
-					{
-						ptr=1;
-					}
-				}
-				else
-				{
-					if((int)neg.size()>0)
-					{
-						cout<<neg[0]<<' ';
-						sum+=neg[0];
-						neg.pop_front();
-					}
-					else
-					{
-						cout<<pos[0]<<' ';
-						sum+=pos[0];
-						pos.pop_front();
-					}
-					if(sum<=0)
-					{
-						ptr=0;
-					}
-				}
-			}
-			cout<<'\n';
-		}
+		cout<<i+1-cnt<<' ';
+		mp[x]++;
 	}
 	return 0;
 }
