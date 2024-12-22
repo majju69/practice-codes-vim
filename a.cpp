dc6f85bfe36e62233dad114e6392ce3a8acb1ae6
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-22 22:28:38 +0530

    https://codeforces.com/contest/2051/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -9,33 +9,25 @@ using namespace std;
 
 typedef long long ll;
 
-ll gcd(ll a,ll b)
+ll getProfit(ll x,ll k,vector<ll> &a,vector<ll> &b)
 {
-	return ((b==0)?a:gcd(b,a%b));
-}
-
-vector<long long> lpf(200001,0);
-vector<long long> primes;
-
-void leastPrimeFactor()
-{
-    long long n=lpf.size();
-    for(long long i=2;i<n;++i)
-    {
-        if(lpf[i]==0)
-        {
-            lpf[i]=i;
-            primes.push_back(i);
-        }
-        for(long long j=0;i*primes[j]<n;++j)
-        {
-            lpf[i*primes[j]]=primes[j];
-            if(primes[j]==lpf[i])
-            {
-                break;
-            }
-        }
-    }
+	ll n=a.size(),neg=0;
+	ll cnt=upper_bound(b.begin(),b.end(),1e18)-lower_bound(b.begin(),b.end(),x);
+	ll profit=cnt*x;
+	ll lb1=0,ub1=upper_bound(a.begin(),a.end(),x-1)-a.begin()-1,lb2=lower_bound(b.begin(),b.end(),x)-b.begin(),ub2=n-1;
+	if(lb1<=ub1&&lb2<=ub2)
+	{
+		ll lb=max(lb1,lb2),ub=min(ub1,ub2);
+		if(lb<=ub)
+		{
+			neg=ub-lb+1;
+		}
+	}
+	if(neg>k)
+	{
+		profit=0;
+	}
+	return profit;
 }
 
 int main()
@@ -43,90 +35,30 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	leastPrimeFactor();
-	ll n,cnt1=0,g=0;
-	map<ll,map<ll,ll>> mp;
-	cin>>n;
-	for(ll i=0;i<n;++i)
+	ll tc;
+	cin>>tc;
+	while(tc--)
 	{
-		ll x;
-		cin>>x;
-		if(x==1)
+		ll n,k,ans=0;
+		cin>>n>>k;
+		vector<ll> a(n),b(n);
+		for(auto &v:a)
 		{
-			cnt1++;
-			continue;
+			cin>>v;
 		}
-		g=gcd(g,x);
-		while(x>1)
+		for(auto &v:b)
 		{
-			ll cur_prime=lpf[x],cnt=0;
-			while(x%cur_prime==0)
-			{
-				cnt++;
-				x/=cur_prime;
-			}
-			mp[cur_prime][cnt]++;
+			cin>>v;
 		}
-	}
-	if(cnt1>=2)
-	{
-		cout<<1<<'\n';
-	}
-	else if(cnt1==1)
-	{
-		cout<<g<<'\n';
-	}
-	else
-	{
-		ll ans=1;
-		for(auto &v:mp)
+		sort(a.begin(),a.end());
+		sort(b.begin(),b.end());
+		for(auto &v:a)
 		{
-			ll tot_cnt=0;
-			for(auto &v1:v.second)
-			{
-				tot_cnt+=v1.second;
-			}
-			if(n-tot_cnt>=2)
-			{
-				continue;
-			}
-			else if(n-tot_cnt==1)
-			{
-				auto it=v.second.begin();
-				ll cur_cnt=it->first;
-				while(cur_cnt--)
-				{
-					ans*=v.first;
-				}
-			}
-			else
-			{
-				auto it=v.second.begin();
-				ll cur_cnt=it->second,cur_mn=it->first;
-				if(cur_cnt>1)
-				{
-					while(cur_mn--)
-					{
-						ans*=v.first;
-					}
-				}
-				else
-				{
-					ll cur_mn_sec=1e18;
-					for(auto &v1:v.second)
-					{
-						if(v1.first!=cur_mn)
-						{
-							cur_mn_sec=v1.first;
-							break;
-						}
-					}
-					while(cur_mn_sec--)
-					{
-						ans*=v.first;
-					}
-				}
-			}
+			ans=max(ans,getProfit(v,k,a,b));
+		}
+		for(auto &v:b)
+		{
+			ans=max(ans,getProfit(v,k,a,b));
 		}
 		cout<<ans<<'\n';
 	}
