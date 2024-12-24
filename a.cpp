23a13de86c9ecea89dc22a1429b5fa3eeeb51ad5
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-24 06:39:44 +0530

    https://codeforces.com/contest/1359/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,33 +7,54 @@ using namespace std;
 	#define debug(x)
 #endif
 
-pair<int,int> check(int mid,vector<vector<int>> &a)
+typedef long long ll;
+
+const ll mod=998244353;
+
+const ll add(ll a,ll b)
 {
-	int n=a.size(),m=a[0].size();
-	vector<int> mask_idx((1<<m),-1);
-	for(int i=0;i<n;++i)
+	return (a%mod+b%mod)%mod;
+}
+
+vector<long long> fact(500001);
+
+void fillFact()
+{
+	long long n=fact.size();
+	fact[0]=1;
+	for(long long i=1;i<n;++i)
 	{
-		int mask=0;
-		for(int j=0;j<m;++j)
-		{
-			if(a[i][j]>=mid)
-			{
-				mask+=(1<<j);
-			}
-		}
-		mask_idx[mask]=i;
+		fact[i]=(i%mod*fact[i-1]%mod)%mod;
 	}
-	for(int i=0;i<(1<<m);++i)
+}
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+{
+	long long ans=1;
+	a=a%mod;
+	while(b)
 	{
-		for(int j=i;j<(1<<m);++j)
+		if(b&1)
 		{
-			if(mask_idx[i]!=-1&&mask_idx[j]!=-1&&((i|j)==(1<<m)-1))
-			{
-				return {mask_idx[i],mask_idx[j]};
-			}
+			ans=(ans*a)%mod;
 		}
+		a=(a*a)%mod;
+		b>>=1;
 	}
-	return {-1,-1};
+	return ans%mod;
+}
+
+long long nCr(long long n,long long r)         // Ensure that fillFact() is called before this function is used
+{
+	if(n<r||n<0||r<0)
+	{
+		return 0;
+	}
+	if(r==n||r==0)
+	{
+		return 1;
+	}
+	return (fact[n]*power(fact[r],mod-2)%mod*power(fact[n-r],mod-2)%mod)%mod;
 }
 
 int main()
@@ -41,33 +62,22 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,m,lo=1e9,hi=-1e9;
-	pair<int,int> ans={-1,-1};
-	cin>>n>>m;
-	vector<vector<int>> a(n,vector<int>(m,0));
-	for(auto &vec:a)
+	ll n,k;
+	cin>>n>>k;
+	if(k>n)
 	{
-		for(auto &v:vec)
-		{
-			cin>>v;
-			lo=min(lo,v);
-			hi=max(hi,v);
-		}
+		cout<<0<<'\n';
 	}
-	while(hi>=lo)
+	else
 	{
-		int mid=lo+(hi-lo)/2;
-		pair<int,int> p=check(mid,a);
-		if(p.first!=-1)
+		fillFact();
+		ll ans=0;
+		for(ll i=1;i<=n;++i)
 		{
-			ans=p;
-			lo=mid+1;
+			ll candidates=n/i-1;
+			ans=add(ans,nCr(candidates,k-1));
 		}
-		else
-		{
-			hi=mid-1;
-		}
+		cout<<ans<<'\n';
 	}
-	cout<<ans.first+1<<' '<<ans.second+1<<'\n';
 	return 0;
 }
