ce5374b8b6d8ed6685aa759d731fae5cda7d005d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-25 09:21:48 +0530

    https://codeforces.com/contest/59/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,98 +7,93 @@ using namespace std;
 	#define debug(x)
 #endif
 
-pair<int,int> getRange(int l,int r,vector<int> &a)
-{
-	int mn=0,mx=0,mxEnd=0,mnEnd=0;
-	for(int i=l;i<=r;++i)
-	{
-		mxEnd=max(mxEnd+a[i],a[i]);
-		mnEnd=min(mnEnd+a[i],a[i]);
-		mn=min(mn,mnEnd);
-		mx=max(mx,mxEnd);
-	}
-	return {mn,mx};
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,m,k,mn=1e9;
+	pair<int,int> cur={-1,-1};
+	set<pair<int,pair<int,int>>> forbidden;
+	queue<pair<int,int>> q;
+	cin>>n>>m>>k;
+	vector<int> adj[n];
+	vector<vector<int>> dist(n,vector<int>(n,1e9));    // {node,par}
+	vector<vector<pair<int,int>>> par(n,vector<pair<int,int>>(n,{-1,-1}));
+	for(int i=0;i<m;++i)
 	{
-		int n,idx=-1;
-		cin>>n;
-		vector<int> a(n);
-		for(int i=0;i<n;++i)
+		int u,v;
+		cin>>u>>v;
+		u--;
+		v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	for(int i=0;i<k;++i)
+	{
+		int a,b,c;
+		cin>>a>>b>>c;
+		a--;
+		b--;
+		c--;
+		forbidden.insert({a,{b,c}});
+	}
+	dist[0][0]=0;
+	q.push({0,0});
+	while(q.size())
+	{
+		int node=q.front().first,p=q.front().second;
+		q.pop();
+		for(auto &v:adj[node])
 		{
-			cin>>a[i];
-			if(abs(a[i])!=1)
+			if(forbidden.count({p,{node,v}}))
 			{
-				idx=i;
+				continue;
+			}
+			if(dist[v][node]>1+dist[node][p])
+			{
+				q.push({v,node});
+				dist[v][node]=1+dist[node][p];
+				par[v][node]=make_pair(node,p);
 			}
 		}
-		if(idx==-1)
+	}
+	for(int i=0;i<n-1;++i)
+	{
+		if(cur==make_pair(-1,-1))
 		{
-			pair<int,int> range=getRange(0,n-1,a);
-			cout<<range.second-range.first+1<<'\n';
-			for(int i=range.first;i<=range.second;++i)
-			{
-				cout<<i<<' ';
-			}
-			cout<<'\n';
+			mn=dist[i][n-1];
+			cur={i,n-1};
 		}
 		else
 		{
-			int s=0,x=a[idx];
-			pair<int,int> l_exc_range=getRange(0,idx-1,a),r_exc_range=getRange(idx+1,n-1,a);
-			pair<int,int> exc_range={min(l_exc_range.first,r_exc_range.first),max(l_exc_range.second,r_exc_range.second)};
-			pair<int,int> l_inc_range={0,0},r_inc_range={0,0};
-			pair<int,int> inc_range={2e9,-2e9};
-			for(int i=idx-1;i>=0;--i)
+			if(dist[cur.first][cur.second]>dist[i][n-1])
 			{
-				s+=a[i];
-				l_inc_range.first=min(l_inc_range.first,s);
-				l_inc_range.second=max(l_inc_range.second,s);
-			}
-			s=0;
-			for(int i=idx+1;i<n;++i)
-			{
-				s+=a[i];
-				r_inc_range.first=min(r_inc_range.first,s);
-				r_inc_range.second=max(r_inc_range.second,s);
-			}
-			inc_range={min({x+l_inc_range.first,x+r_inc_range.first,x+l_inc_range.first+r_inc_range.first}),max({x+l_inc_range.second,x+r_inc_range.second,x+l_inc_range.second+r_inc_range.second})};
-			if(inc_range.first>exc_range.second||exc_range.first>inc_range.second)
-			{
-				if(inc_range>exc_range)
-				{
-					swap(inc_range,exc_range);
-				}
-				cout<<inc_range.second+exc_range.second-inc_range.first-exc_range.first+2<<'\n';
-				for(int i=inc_range.first;i<=inc_range.second;++i)
-				{
-					cout<<i<<' ';
-				}
-				for(int i=exc_range.first;i<=exc_range.second;++i)
-				{
-					cout<<i<<' ';
-				}
-				cout<<'\n';
-			}
-			else
-			{
-				pair<int,int> range={min(inc_range.first,exc_range.first),max(inc_range.second,exc_range.second)};
-				cout<<range.second-range.first+1<<'\n';
-				for(int i=range.first;i<=range.second;++i)
-				{
-					cout<<i<<' ';
-				}
-				cout<<'\n';
+				mn=dist[i][n-1];
+				cur={i,n-1};
 			}
 		}
+	}
+	if(mn==(int)1e9)
+	{
+		cout<<-1<<'\n';
+	}
+	else
+	{
+		vector<int> ans;
+		while(cur!=make_pair(0,0))
+		{
+			ans.push_back(cur.second);
+			ans.push_back(cur.first);
+			cur=par[cur.first][cur.second];
+		}
+		reverse(ans.begin(),ans.end());
+		cout<<mn-1<<'\n';
+		for(int i=1;i<(int)ans.size();i+=2)
+		{
+			cout<<ans[i]+1<<' ';
+		}
+		cout<<'\n';
 	}
 	return 0;
 }
