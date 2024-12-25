35b6de0ff50851956312905ce13335ac63c7ef82
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-26 03:12:38 +0530

    https://www.codechef.com/problems/LOLBSGNJ6PK8 accepted

--- a.cpp
+++ a.cpp
@@ -9,54 +9,102 @@ using namespace std;
 
 typedef long long ll;
 
+const ll mod=1e9+7;
+
+vector<long long> lpf(10000001,0);
+vector<long long> primes;
+
+void leastPrimeFactor()
+{
+    long long n=lpf.size();
+    for(long long i=2;i<n;++i)
+    {
+        if(lpf[i]==0)
+        {
+            lpf[i]=i;
+            primes.push_back(i);
+        }
+        for(long long j=0;i*primes[j]<n;++j)
+        {
+            lpf[i*primes[j]]=primes[j];
+            if(primes[j]==lpf[i])
+            {
+                break;
+            }
+        }
+    }
+}
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+{
+	long long ans=1;
+	a=a%mod;
+	while(b)
+	{
+		if(b&1)
+		{
+			ans=(ans*a)%mod;
+		}
+		a=(a*a)%mod;
+		b>>=1;
+	}
+	return ans%mod;
+}
+
+inline ll mul(ll a,ll b)
+{
+	return (a%mod*b%mod)%mod;
+}
+
+inline ll divide(ll a,ll b)
+{
+	return mul(a,power(b,mod-2));
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,m;
-	set<pair<ll,ll>> st;
+	leastPrimeFactor();
+	ll n,m,pro=1;
+	map<ll,ll> mp_m;
 	cin>>n>>m;
-	vector<pair<ll,ll>> adj[n];
-	vector<ll> a(n),dist(n,1e18);
-	for(ll i=0;i<m;++i)
+	for(auto &p:primes)
 	{
-		ll u,v,w;
-		cin>>u>>v>>w;
-		u--;
-		v--;
-		w<<=1;
-		adj[u].push_back({v,w});
-		adj[v].push_back({u,w});
-	}
-	for(auto &v:a)
-	{
-		cin>>v;
+		if(m/p==0)
+		{
+			break;
+		}
+		ll cur=p,cnt=0;
+		while(m/cur!=0)
+		{
+			cnt+=(m/cur);
+			cur*=p;
+		}
+		mp_m[p]=cnt;
+		pro=mul(pro,1+cnt);
 	}
 	for(ll i=0;i<n;++i)
 	{
-		dist[i]=a[i];
-		st.insert({a[i],i});
-	}
-	while(st.size())
-	{
-		auto it=st.begin();
-		ll node=it->second,dis=it->first;
-		st.erase(*it);
-		for(auto &v:adj[node])
+		ll x,ans=pro;
+		cin>>x;
+		while(x>1)
 		{
-			ll curNode=v.first,curDist=v.second;
-			if(dist[curNode]>dis+curDist)
+			ll p=lpf[x],cnt=0,pvs=0;
+			while(x%p==0)
 			{
-				st.erase({dist[curNode],curNode});
-				dist[curNode]=dis+curDist;
-				st.insert({dist[curNode],curNode});
+				cnt++;
+				x/=p;
 			}
+			if(mp_m.count(p))
+			{
+				pvs=mp_m[p];
+			}
+			ans=divide(ans,pvs+1);
+			ans=mul(ans,pvs+cnt+1);
 		}
-	}
-	for(auto &v:dist)
-	{
-		cout<<v<<' ';
+		cout<<ans<<' ';
 	}
 	cout<<'\n';
 	return 0;
