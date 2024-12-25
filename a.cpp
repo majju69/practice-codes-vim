f9b5731f3076708c4bef11affa5b851033e45140
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-25 10:30:41 +0530

    https://codeforces.com/contest/938/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,93 +7,57 @@ using namespace std;
 	#define debug(x)
 #endif
 
+typedef long long ll;
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,m,k,mn=1e9;
-	pair<int,int> cur={-1,-1};
-	set<pair<int,pair<int,int>>> forbidden;
-	queue<pair<int,int>> q;
-	cin>>n>>m>>k;
-	vector<int> adj[n];
-	vector<vector<int>> dist(n,vector<int>(n,1e9));    // {node,par}
-	vector<vector<pair<int,int>>> par(n,vector<pair<int,int>>(n,{-1,-1}));
-	for(int i=0;i<m;++i)
+	ll n,m;
+	set<pair<ll,ll>> st;
+	cin>>n>>m;
+	vector<pair<ll,ll>> adj[n];
+	vector<ll> a(n),dist(n,1e18);
+	for(ll i=0;i<m;++i)
 	{
-		int u,v;
-		cin>>u>>v;
+		ll u,v,w;
+		cin>>u>>v>>w;
 		u--;
 		v--;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
+		w<<=1;
+		adj[u].push_back({v,w});
+		adj[v].push_back({u,w});
 	}
-	for(int i=0;i<k;++i)
+	for(auto &v:a)
 	{
-		int a,b,c;
-		cin>>a>>b>>c;
-		a--;
-		b--;
-		c--;
-		forbidden.insert({a,{b,c}});
+		cin>>v;
 	}
-	dist[0][0]=0;
-	q.push({0,0});
-	while(q.size())
+	for(ll i=0;i<n;++i)
 	{
-		int node=q.front().first,p=q.front().second;
-		q.pop();
+		dist[i]=a[i];
+		st.insert({a[i],i});
+	}
+	while(st.size())
+	{
+		auto it=st.begin();
+		ll node=it->second,dis=it->first;
+		st.erase(*it);
 		for(auto &v:adj[node])
 		{
-			if(forbidden.count({p,{node,v}}))
+			ll curNode=v.first,curDist=v.second;
+			if(dist[curNode]>dis+curDist)
 			{
-				continue;
-			}
-			if(dist[v][node]>1+dist[node][p])
-			{
-				q.push({v,node});
-				dist[v][node]=1+dist[node][p];
-				par[v][node]=make_pair(node,p);
+				st.erase({dist[curNode],curNode});
+				dist[curNode]=dis+curDist;
+				st.insert({dist[curNode],curNode});
 			}
 		}
 	}
-	for(int i=0;i<n-1;++i)
+	for(auto &v:dist)
 	{
-		if(cur==make_pair(-1,-1))
-		{
-			mn=dist[i][n-1];
-			cur={i,n-1};
-		}
-		else
-		{
-			if(dist[cur.first][cur.second]>dist[i][n-1])
-			{
-				mn=dist[i][n-1];
-				cur={i,n-1};
-			}
-		}
+		cout<<v<<' ';
 	}
-	if(mn==(int)1e9)
-	{
-		cout<<-1<<'\n';
-	}
-	else
-	{
-		vector<int> ans;
-		while(cur!=make_pair(0,0))
-		{
-			ans.push_back(cur.second);
-			ans.push_back(cur.first);
-			cur=par[cur.first][cur.second];
-		}
-		reverse(ans.begin(),ans.end());
-		cout<<mn-1<<'\n';
-		for(int i=1;i<(int)ans.size();i+=2)
-		{
-			cout<<ans[i]+1<<' ';
-		}
-		cout<<'\n';
-	}
+	cout<<'\n';
 	return 0;
 }
