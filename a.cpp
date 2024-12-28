738b91dca210f635cf06502a8f9e69897997787f
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-28 22:41:11 +0530

    https://codeforces.com/contest/735/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,54 +7,16 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-const ll mod=1e9+7;
-
-vector<long long> fact(1001);
-
-void fillFact()
+bool is_prime(int n)
 {
-	long long n=fact.size();
-	fact[0]=1;
-	for(long long i=1;i<n;++i)
+	for(int i=2;i*i<=n;++i)
 	{
-		fact[i]=(i%mod*fact[i-1]%mod)%mod;
-	}
-}
-
-long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
-{
-	long long ans=1;
-	a=a%mod;
-	while(b)
-	{
-		if(b&1)
+		if(n%i==0)
 		{
-			ans=(ans*a)%mod;
+			return 0;
 		}
-		a=(a*a)%mod;
-		b>>=1;
 	}
-	return ans%mod;
-}
-
-long long nCr(long long n,long long r)         // Ensure that fillFact() is called before this function is used
-{
-	if(n<r||n<0||r<0)
-	{
-		return 0;
-	}
-	if(r==n||r==0)
-	{
-		return 1;
-	}
-	return (fact[n]*power(fact[r],mod-2)%mod*power(fact[n-r],mod-2)%mod)%mod;
-}
-
-inline ll mul(ll a,ll b)
-{
-	return (a%mod*b%mod)%mod;
+	return 1;
 }
 
 int main()
@@ -62,32 +24,22 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	fillFact();
-	ll tc;
-	cin>>tc;
-	while(tc--)
+	int n;
+	cin>>n;
+	if(is_prime(n))
 	{
-		ll n,k,ans=1;
-		map<ll,ll> mp,mp_req;
-		set<ll> st;
-		cin>>n>>k;
-		vector<ll> a(n);
-		for(auto &v:a)
+		cout<<1<<'\n';
+	}
+	else
+	{
+		if(n&1)
 		{
-			cin>>v;
-			mp[v]++;
-			st.insert(v);
+			cout<<((is_prime(n-2))?2:3)<<'\n';
 		}
-		sort(a.rbegin(),a.rend());
-		for(ll i=0;i<k;++i)
+		else
 		{
-			mp_req[a[i]]++;
+			cout<<2<<'\n';
 		}
-		for(auto &v:st)
-		{
-			ans=mul(ans,nCr(mp[v],mp_req[v]));
-		}
-		cout<<ans<<'\n';
 	}
 	return 0;
 }
