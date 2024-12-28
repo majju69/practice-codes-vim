375d5f28a2bc6a52f1b32ed7467553063cafb5e6
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-28 16:36:41 +0530

    https://codeforces.com/contest/1334/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,47 +9,33 @@ using namespace std;
 
 typedef long long ll;
 
-ll get(ll n)
-{
-	ll cnt=0;
-	while(n)
-	{
-		n/=10;
-		cnt++;
-	}
-	return cnt;
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,k,ans=0;
-	vector<map<ll,ll>> mp_a(10);
-	cin>>n>>k;
-	vector<ll> a(n);
-	for(auto &v:a)
+	ll tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cin>>v;
-		mp_a[get(v)-1][v%k]++;
-	}
-	for(auto &v:a)
-	{
-		ll cur=10;
-		mp_a[get(v)-1][v%k]--;
-		for(ll i=0;i<10;++i)
+		ll n,ans=0,mn=1e18;
+		cin>>n;
+		vector<pair<ll,ll>> a(n);
+		for(auto &v:a)
 		{
-			ll r=(cur%k*v%k)%k;
-			ll req=(k-r)%k;
-			if(mp_a[i].count(req))
-			{
-				ans+=mp_a[i][req];
-			}
-			cur*=10;
+			cin>>v.first>>v.second;
 		}
-		mp_a[get(v)-1][v%k]++;
+		for(ll i=0;i<n;++i)
+		{
+			ll req=max(0LL,a[i].first-a[(i-1+n)%n].second);
+			ans+=req;
+			a[i].first-=req;
+		}
+		for(auto &v:a)
+		{
+			mn=min(mn,v.first);
+		}
+		cout<<ans+mn<<'\n';
 	}
-	cout<<ans<<'\n';
 	return 0;
 }
