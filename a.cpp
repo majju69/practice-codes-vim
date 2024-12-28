bea708af0f9e0a7caeb944a399d74efe79237b5b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-12-28 21:30:25 +0530

    https://codeforces.com/contest/1374/problem/E1 accepted

--- a.cpp
+++ a.cpp
@@ -7,16 +7,13 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-ll gcd(ll a,ll b)
+void pre(vector<int> &a)
 {
-	return ((b==0)?a:gcd(b,a%b));
-}
-
-inline ll leastCommonMultiple(ll a,ll b)
-{
-	return (a/gcd(a,b))*b;
+	int n=a.size();
+	for(int i=1;i<n;++i)
+	{
+		a[i]+=a[i-1];
+	}
 }
 
 int main()
@@ -24,38 +21,48 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll tc;
-	cin>>tc;
-	while(tc--)
+	int n,k,mn=2e9+10;
+	vector<int> common,alice,bob;
+	cin>>n>>k;
+	for(int i=0;i<n;++i)
 	{
-		ll a,b,q,lcm=1;
-		vector<ll> rem;
-		cin>>a>>b>>q;
-		lcm=leastCommonMultiple(a,b);
-		for(ll i=0;i<lcm;++i)
+		int t,a,b;
+		cin>>t>>a>>b;
+		if(a==1&&b==1)
 		{
-			if((i%a)%b==(i%b)%a)
-			{
-				rem.push_back(i);
-			}
+			common.push_back(t);
 		}
-		while(q--)
+		if(a==1&&b==0)
 		{
-			ll l,r,ans=0;
-			cin>>l>>r;
-			for(auto &v:rem)
-			{
-				ll tmp_l=l,tmp_r=r;
-				tmp_l+=(lcm+(v-tmp_l%lcm+lcm)%lcm)%lcm;
-				tmp_r-=(lcm+(tmp_r%lcm-v+lcm)%lcm)%lcm;
-				if(tmp_l<=tmp_r)
-				{
-					ans+=(tmp_r-tmp_l+lcm)/lcm;
-				}
-			}
-			cout<<r-l+1-ans<<' ';
+			alice.push_back(t);
 		}
-		cout<<'\n';
+		if(a==0&&b==1)
+		{
+			bob.push_back(t);
+		}
 	}
+	sort(common.begin(),common.end());
+	sort(alice.begin(),alice.end());
+	sort(bob.begin(),bob.end());
+	pre(common);
+	pre(alice);
+	pre(bob);
+	for(int i=0;i<=(int)common.size();++i)
+	{
+		if(i>k)
+		{
+			break;
+		}
+		int c_sum=((i==0)?0:common[i-1]),a_sum=((k-i<=(int)alice.size())?((k==i)?0:alice[k-i-1]):-1),b_sum=((k-i<=(int)bob.size())?((k==i)?0:bob[k-i-1]):-1);
+		if(a_sum!=-1&&b_sum!=-1)
+		{
+			mn=min(mn,c_sum+a_sum+b_sum);
+		}
+	}
+	if(mn>(int)2e9)
+	{
+		mn=-1;
+	}
+	cout<<mn<<'\n';
 	return 0;
 }
