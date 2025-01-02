73a4921c99882eadfd3507eb8f080b4bea47ffb0
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-02 07:14:42 +0530

    https://codeforces.com/contest/1901/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,86 +7,69 @@ using namespace std;
 	#define debug(x)
 #endif
 
-void dfs(int node,int p,vector<int> adj[],vector<int> &depth)
-{
-	for(auto &v:adj[node])
-	{
-		if(v!=p)
-		{
-			depth[v]=depth[node]+1;
-			dfs(v,node,adj,depth);
-		}
-	}
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,ans=2e9;
+	cin>>n;
+	vector<int> a(n),right_inc(n),left_inc(n),left_idx(n,-1),right_idx(n,-1);
+	for(int i=0;i<n;++i)
 	{
-		int n,k,node0=-1,node1=-1,mn=1e9;
-		cin>>n>>k;
-		vector<bool> marked(n,0);
-		vector<int> adj[n],depth0(n,0),depth1(n,0);
-		while(k--)
-		{
-			int x;
-			cin>>x;
-			x--;
-			marked[x]=1;
-			node0=x;
-		}
-		for(int i=1;i<n;++i)
-		{
-			int u,v;
-			cin>>u>>v;
-			u--;
-			v--;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-		dfs(node0,-1,adj,depth0);
-		for(int i=0;i<n;++i)
-		{
-			if(marked[i])
-			{
-				if(node1==-1||depth0[node1]<depth0[i])
-				{
-					node1=i;
-				}
-			}
-		}
-		dfs(node1,-1,adj,depth1);
-		node0=-1;
-		for(int i=0;i<n;++i)
-		{
-			if(marked[i])
-			{
-				if(node0==-1||depth1[node0]<depth1[i])
-				{
-					node0=i;
-				}
-			}
-		}
-		depth0.assign(n,0);
-		depth1.assign(n,0);
-		if(node0!=-1)
-		{
-			dfs(node0,-1,adj,depth0);
-		}
-		if(node1!=-1)
-		{
-			dfs(node1,-1,adj,depth1);
-		}
-		for(int i=0;i<n;++i)
-		{
-			mn=min(mn,max(depth0[i],depth1[i]));
-		}
-		cout<<mn<<'\n';
+		cin>>a[i];
+		left_inc[i]=a[i]-i;
+		right_inc[i]=a[i]+i;
 	}
+	for(int i=n-2;i>=0;--i)
+	{
+		if(i==n-2)
+		{
+			right_idx[i]=n-1;
+		}
+		else
+		{
+			if(right_inc[i+1]>right_inc[right_idx[i+1]])
+			{
+				right_idx[i]=i+1;
+			}
+			else
+			{
+				right_idx[i]=right_idx[i+1];
+			}
+		}
+	}
+	for(int i=1;i<n;++i)
+	{
+		if(i==1)
+		{
+			left_idx[i]=0;
+		}
+		else
+		{
+			if(left_inc[i-1]>left_inc[left_idx[i-1]])
+			{
+				left_idx[i]=i-1;
+			}
+			else
+			{
+				left_idx[i]=left_idx[i-1];
+			}
+		}
+	}
+	for(int i=0;i<n;++i)
+	{
+		int mx=a[i];
+		if(left_idx[i]!=-1)
+		{
+			mx=max(mx,n-1+a[left_idx[i]]-left_idx[i]);
+		}
+		if(right_idx[i]!=-1)
+		{
+			mx=max(mx,a[right_idx[i]]+right_idx[i]);
+		}
+		ans=min(ans,mx);
+	}
+	cout<<ans<<'\n';
 	return 0;
 }
