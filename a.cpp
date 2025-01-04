83a7d3977a103a9d95b74523afc4fdc1a923b718
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-04 07:41:16 +0530

    https://codeforces.com/contest/798/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,53 +7,52 @@ using namespace std;
 	#define debug(x)
 #endif
 
+int gcd(int a,int b)
+{
+	return ((b==0)?a:gcd(b,a%b));
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,g=0;
+	cin>>n;
+	vector<int> a(n);
+	for(auto &v:a)
 	{
-		int n,ans=0;
-		cin>>n;
-		vector<string> a(n);
-		vector<vector<int>> pre(n,vector<int>(n,0)),pre_left(n,vector<int>(n,0)),pre_right(n,vector<int>(n,0));
+		cin>>v;
+		g=gcd(v,g);
+	}
+	cout<<"YES\n";
+	if(g!=1)
+	{
+		cout<<0<<'\n';
+	}
+	else
+	{
+		int cnt=0,ans=0;
 		for(auto &v:a)
 		{
-			cin>>v;
-		}
-		for(int i=0;i<n;++i)
-		{
-			pre[0][i]=(a[0][i]=='1');
-			pre_left[0][i]=(a[0][i]=='1');
-			pre_right[0][i]=(a[0][i]=='1');
-			ans+=(a[0][i]=='1');
-		}
-		for(int i=1;i<n;++i)
-		{
-			for(int j=0;j<n;++j)
+			if(v&1)
 			{
-				pre[i][j]=pre[i-1][j];
-				if(j>0)
-				{
-					pre[i][j]+=pre_left[i-1][j-1];
-					pre_left[i][j]=pre_left[i-1][j-1];
-				}
-				if(j+1<n)
-				{
-					pre[i][j]+=pre_right[i-1][j+1];
-					pre_right[i][j]=pre_right[i-1][j+1];
-				}
-				if((pre[i][j]+(a[i][j]-'0'))&1)
-				{
-					ans++;
-					pre[i][j]++;
-					pre_left[i][j]++;
-					pre_right[i][j]++;
-				}
+				cnt++;
 			}
+			else
+			{
+				ans+=(cnt>>1);
+				if(cnt&1)
+				{
+					ans+=2;
+				}
+				cnt=0;
+			}
+		}
+		ans+=(cnt>>1);
+		if(cnt&1)
+		{
+			ans+=2;
 		}
 		cout<<ans<<'\n';
 	}
