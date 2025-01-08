fcf1099985d3f1009993eea648f5ed4ba5a3d280
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-08 08:42:25 +0530

    https://codeforces.com/contest/1481/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,87 +7,45 @@ using namespace std;
 	#define debug(x)
 #endif
 
-int dp[200001][3];
-
-int solve(int i,int pvs,vector<int> &idx)	// pvs 1 --> taken the pvs one
+pair<int,pair<int,int>> get(vector<string> &adjMat,char ch)
 {
-	if(i>=(int)idx.size())
-	{
-		return 0;
-	}
-	if(dp[i][pvs]!=-1)
-	{
-		return dp[i][pvs];
-	}
-	int ans=solve(i+1,0,idx);
-	if(pvs!=1||idx[i]-idx[i-1]>1)
-	{
-		ans=max(ans,solve(i+1,1,idx)+1);
-	}
-	return dp[i][pvs]=ans;
-}
-
-int maxTakenIndices(vector<int> &idx)
-{
-	int n=idx.size();
-	if(n==0)
-	{
-		return 0;
-	}
-	for(int i=0;i<=n;++i)
-	{
-		dp[i][0]=dp[i][1]=dp[i][2]=-1;
-	}
-	return max(solve(1,0,idx),1+solve(1,1,idx));
-}
-
-bool check(int mid,int k,vector<int> &a)		// check whether the given expression can be made <= mid
-{
-	int n=a.size();
-	bool ok=0;
-	vector<int> idx;
+	int n=adjMat.size();
+	vector<vector<bool>> same_ch(n,vector<bool>(n,0));
+	vector<pair<int,int>> edges;
 	for(int i=0;i<n;++i)
 	{
-		if(a[i]<=mid)
+		for(int j=0;j<n;++j)
 		{
-			idx.push_back(i);
+			if(adjMat[i][j]==ch)
+			{
+				same_ch[i][j]=1;
+				edges.push_back({i,j});
+			}
 		}
 	}
-	if(k&1)
+	for(auto &edge:edges)
 	{
-		vector<int> tmp=idx;
-		int req=(k>>1)+1;
-		ok=(ok||maxTakenIndices(tmp)>=req);
-		req-=1;
-		if((int)tmp.size()>0&&tmp[0]==0)
+		int u=edge.first,v=edge.second;
+		for(int i=0;i<n;++i)
 		{
-			reverse(tmp.begin(),tmp.end());
-			tmp.pop_back();
-			reverse(tmp.begin(),tmp.end());
+			if(same_ch[v][i])
+			{
+				return {u,{v,i}};
+			}
 		}
-		if((int)tmp.size()>0&&tmp.back()==n-1)
-		{
-			tmp.pop_back();
-		}
-		ok=(ok||maxTakenIndices(tmp)>=req);
 	}
-	else
+	return {-1,{-1,-1}};
+}
+
+void print(int u,int v,int m)
+{
+	int cur=u;
+	m++;
+	while(m--)
 	{
-		vector<int> odd=idx,even=idx;
-		int req=(k>>1);
-		if((int)even.size()>0&&even[0]==0)
-		{
-			reverse(even.begin(),even.end());
-			even.pop_back();
-			reverse(even.begin(),even.end());
-		}
-		if((int)odd.size()>0&&odd.back()==n-1)
-		{
-			odd.pop_back();
-		}
-		ok=(ok||maxTakenIndices(odd)>=req||maxTakenIndices(even)>=req);
+		cout<<cur+1<<' ';
+		cur=u+v-cur;
 	}
-	return ok;
 }
 
 int main()
@@ -95,26 +53,76 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,k,lo=1,hi=1e9,ans=-1;
-	cin>>n>>k;
-	vector<int> a(n);
-	for(auto &v:a)
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cin>>v;
-	}
-	while(lo<=hi)
-	{
-		int mid=lo+(hi-lo)/2;
-		if(check(mid,k,a))
+		int n,m;
+		cin>>n>>m;
+		vector<string> adjMat(n);
+		for(auto &v:adjMat)
 		{
-			ans=mid;
-			hi=mid-1;
+			cin>>v;
+		}
+		if(m&1)
+		{
+			cout<<"YES\n";
+			print(0,1,m);
+			cout<<'\n';
 		}
 		else
 		{
-			lo=mid+1;
+			int i1=-1,i2=-1;
+			for(int i=0;i<n-1;++i)
+			{
+				if(i1!=-1)
+				{
+					break;
+				}
+				for(int j=i+1;j<n;++j)
+				{
+					if(adjMat[i][j]==adjMat[j][i])
+					{
+						i1=i;
+						i2=j;
+						break;
+					}
+				}
+			}
+			if(i1!=-1)
+			{
+				cout<<"YES\n";
+				print(i1,i2,m);
+				cout<<'\n';
+			}
+			else
+			{
+				pair<int,pair<int,int>> p=get(adjMat,'a');
+				if(p.first==-1)
+				{
+					p=get(adjMat,'b');
+				}
+				if(p.first==-1)
+				{
+					cout<<"NO\n";
+				}
+				else
+				{
+					cout<<"YES\n";
+					int x=(m>>1);
+					if(x&1)
+					{
+						print(p.first,p.second.first,x);
+					}
+					else
+					{
+						print(p.second.first,p.first,x);
+					}
+					print(p.second.second,p.second.first,x-1);
+					cout<<'\n';
+				}
+			}
 		}
 	}
-	cout<<ans<<'\n';
 	return 0;
 }
