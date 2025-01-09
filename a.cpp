1358201c4e273966042808474fa41e1611e25c2e
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-09 09:44:29 +0530

    https://codeforces.com/contest/1407/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -1,57 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef LOCAL
-	#include"debug.h"
-#else
-	#define debug(x)
-#endif
-
-void dfs(int node,int p,vector<int> adj[],vector<int> &a,vector<int> &sub)
+inline int ask(int i,int j)
 {
-	sub[node]=a[node];
-	for(auto &v:adj[node])
+	cout<<"? "<<i+1<<' '<<j+1<<endl;
+	int x;
+	cin>>x;
+	if(x==-1)
 	{
-		if(v!=p)
-		{
-			dfs(v,node,adj,a,sub);
-			sub[node]+=sub[v];
-		}
+		exit(1);
 	}
+	return x;
+}
+
+void reply(vector<int> &a)
+{
+	cout<<"! ";
+	for(auto &v:a)
+	{
+		cout<<v<<' ';
+	}
+	cout<<endl;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	int n;
 	cin>>n;
-	vector<int> adj[n],a(n,0),sub(n,0),ans;
-	for(int i=1;i<n;++i)
+	vector<int> a(n,-1),unknown(n,-1);
+	iota(unknown.begin(),unknown.end(),0);
+	while((int)unknown.size()>1)
 	{
-		int u,v,t;
-		cin>>u>>v>>t;
-		u--;
-		v--;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-		a[u]+=(t==2);
-		a[v]+=(t==2);
-	}
-	dfs(0,0,adj,a,sub);
-	for(int i=0;i<n;++i)
-	{
-		if(sub[i]==1)
+		vector<int> tmp;
+		int len=unknown.size();
+		for(int i=0;i<len-1;i+=2)
 		{
-			ans.push_back(i+1);
+			int a1=ask(unknown[i],unknown[i+1]),a2=ask(unknown[i+1],unknown[i]);
+			if(a1>a2)
+			{
+				a[unknown[i]]=a1;
+				tmp.push_back(unknown[i+1]);
+			}
+			else
+			{
+				a[unknown[i+1]]=a2;
+				tmp.push_back(unknown[i]);
+			}
 		}
+		if(len&1)
+		{
+			tmp.push_back(unknown[len-1]);
+		}
+		unknown=tmp;
 	}
-	cout<<(int)ans.size()<<'\n';
-	for(auto &v:ans)
-	{
-		cout<<v<<' ';
-	}
-	cout<<'\n';
+	a[unknown[0]]=n;
+	reply(a);
 	return 0;
-}	
+}
