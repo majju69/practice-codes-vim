de645ecdfdbe2c14ffca2ff1d2d4fbfaf2e5722d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-11 16:37:20 +0530

    https://codeforces.com/contest/1475/problem/G accepted

--- a.cpp
+++ a.cpp
@@ -7,145 +7,103 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
+vector<vector<int>> divisors(200001);
 
-class SegmentTree
+void fillDivisors()
 {
-
-private:
-
-	vector<ll> seg;
-
-public:
-
-	SegmentTree(ll n)
+	int n=divisors.size();
+	for(int i=2;i<n;i++)
 	{
-		seg.resize(4*n+1);
+		for(int j=i;j<n;j+=i)
+		{
+			divisors[j].push_back(i);
+		}
 	}
+}
 
-	void update(ll ind,ll lo,ll hi,ll i,ll val)
+int dp[200001];
+
+int solve(int i,vector<pair<int,int>> &a,vector<int> &idx)
+{
+	if(i>=(int)a.size())
 	{
-		if(lo==hi)
-		{
-			seg[ind]+=val;
-			return;
-		}
-		ll mid=lo+(hi-lo)/2;
-		if(i<=mid)
-		{
-			update(2*ind+1,lo,mid,i,val);
-		}
-		else
-		{
-			update(2*ind+2,mid+1,hi,i,val);
-		}
-		seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+		return 0;
 	}
-
-	ll query(ll ind,ll lo,ll hi,ll l,ll r)
+	if(dp[i]!=-1)
 	{
-		if(l>hi||lo>r)
-		{
-			return 0;
-		}
-		if(l<=lo&&hi<=r)
-		{
-			return seg[ind];
-		}
-		ll mid=lo+(hi-lo)/2;
-		return query(2*ind+1,lo,mid,l,r)+query(2*ind+2,mid+1,hi,l,r);
+		return dp[i];
 	}
+	int ans=a[i].second;
+	for(auto &v:divisors[a[i].first])
+	{
+		if(v==a[i].first||idx[v]==-1)
+		{
+			continue;
+		}
+		ans=max(ans,solve(idx[v],a,idx)+a[i].second);
+	}
+	return dp[i]=ans;
+}
 
-};
+int maxTaken(vector<pair<int,int>> &a,vector<int> &idx)
+{
+	int n=a.size(),ans=0;
+	for(int i=0;i<=n;++i)
+	{
+		dp[i]=-1;
+	}
+	for(int i=0;i<n;++i)
+	{
+		ans=max(ans,solve(i,a,idx));
+	}
+	return ans;
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,k,_n=0,total=0;
-	map<ll,ll> mp;
-	cin>>n>>k;
-	vector<ll> a(n);
-	for(auto &v:a)
+	fillDivisors();
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cin>>v;
-		mp[v]=0;
-	}
-	for(auto &v:mp)
-	{
-		v.second=_n++;
-	}
-	for(auto &v:a)
-	{
-		v=mp[v];
-	}
-	SegmentTree st(n);
-	for(auto &v:a)
-	{
-		if(v!=_n-1)
+		int n,mx=-1;
+		map<int,int> mp;
+		vector<pair<int,int>> a;
+		cin>>n;
+		for(int i=0;i<n;++i)
 		{
-			total+=(st.query(0,0,_n-1,v+1,_n-1));
+			int x;
+			cin>>x;
+			mp[x]++;
 		}
-		st.update(0,0,_n-1,v,1);
-	}
-	if(total<=k)
-	{
-		cout<<n*(n-1)/2<<'\n';
-	}
-	else
-	{
-		// total-cnt<=k => cnt>=total-k => cnt>=req
-		ll i=1,j=0,cnt=0,req=total-k,ans=0;
-		SegmentTree st_left(_n),st_right(_n);
-		for(int idx=0;idx<n;++idx)
+		for(auto &v:mp)
 		{
-			if(idx==0)
+			if(v.first!=1)
 			{
-				st_left.update(0,0,_n-1,a[idx],1);
-			}
-			else
-			{
-				st_right.update(0,0,_n-1,a[idx],1);
+				a.push_back(v);
+				mx=max(mx,v.first);
 			}
 		}
-		while(i<n-1&&j<n-1)
+		if(mx==-1)
 		{
-			if(cnt<req)
-			{
-				while(cnt<req)
-				{
-					j++;
-					st_right.update(0,0,_n-1,a[j],-1);
-					if(a[j]!=_n-1)
-					{
-						cnt+=st_left.query(0,0,_n-1,a[j]+1,_n-1);
-					}
-					if(a[j]!=0)
-					{
-						cnt+=st_right.query(0,0,_n-1,0,a[j]-1);
-					}
-				}
-			}
-			else
-			{
-				while(cnt>=req)		// j....n-2   n-1-j
-				{
-					ans+=n-1-j;
-					st_left.update(0,0,_n-1,a[i],1);
-					if(a[i]!=_n-1)
-					{
-						cnt-=st_left.query(0,0,_n-1,a[i]+1,_n-1);
-					}
-					if(a[i]!=0)
-					{
-						cnt-=st_right.query(0,0,_n-1,0,a[i]-1);
-					}
-					i++;
-				}
-			}
+			cout<<0<<'\n';
 		}
-		cout<<ans<<'\n';
+		else
+		{
+			int total=0;
+			vector<int> idx(mx+1,-1);
+			reverse(a.begin(),a.end());
+			n=a.size();
+			for(int i=0;i<n;++i)
+			{
+				idx[a[i].first]=i;
+				total+=a[i].second;
+			}
+			cout<<total-maxTaken(a,idx)<<'\n';
+		}
 	}
 	return 0;
 }
