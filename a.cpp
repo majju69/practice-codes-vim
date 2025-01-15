4f8363ef8d7e15f7fb5f8e2ff08f46e451c94d00
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-15 17:46:40 +0530

    https://codeforces.com/contest/710/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -9,62 +9,65 @@ using namespace std;
 
 typedef long long ll;
 
-const ll mod=998244353;
+const ll N=1e7+10;
+vector<ll> dist(N,1e18);
 
-inline ll mul(ll a,ll b)
+void bfs(ll x,ll y)
 {
-	return (a%mod*b%mod)%mod;
-}
-
-ll gcd(ll a,ll b)
-{
-	return ((b==0)?a:gcd(b,a%b));
-}
-
-ll square_root(ll n)
-{
-	if(n<=1)
+	ll mx=max(x,y);
+	deque<ll> q;
+	dist[0]=0;
+	q.push_front(0);
+	while(q.size())
 	{
-		return n;
-	}
-	ll lo=1,hi=sqrtl(n),ans=-1;
-	while(hi>=lo)
-	{
-		ll mid=lo+(hi-lo)/2;
-		if(mid*mid>n)
+		ll node=q.front();
+		q.pop_front();
+		if(node+1<N)
 		{
-			hi=mid-1;
+			if(dist[node]+x<dist[node+1])
+			{
+				dist[node+1]=dist[node]+x;
+				if(x==mx)
+				{
+					q.push_back(node+1);
+				}
+				else
+				{
+					q.push_front(node+1);
+				}
+			}
 		}
-		else
+		if(node-1>=0)
 		{
-			ans=mid;
-			lo=mid+1;
+			if(dist[node]+x<dist[node-1])
+			{
+				dist[node-1]=dist[node]+x;
+				if(x==mx)
+				{
+					q.push_back(node-1);
+				}
+				else
+				{
+					q.push_front(node-1);
+				}
+			}
+		}
+		if((node<<1)<N)
+		{
+			if(dist[node]+y<dist[(node<<1)])
+			{
+				dist[(node<<1)]=dist[node]+y;
+				if(y==mx)
+				{
+					q.push_back(node<<1);
+				}
+				else
+				{
+					q.push_front(node<<1);
+				}
+			}
 		}
 	}
-	return ans;
-}
-
-ll cube_root(ll n)
-{
-	if(n<=1)
-	{
-		return n;
-	}
-	ll lo=1,hi=pow(n,0.34),ans=-1;
-	while(lo<=hi)
-	{
-		ll mid=lo+(hi-lo)/2;
-		if(mid*mid*mid>n)
-		{
-			hi=mid-1;
-		}
-		else
-		{
-			ans=mid;
-			lo=mid+1;
-		}
-	}
-	return ans;
 }
 
 int main()
@@ -72,79 +75,9 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,ans=1;
-	map<ll,ll> mp,freq;
-	vector<pair<ll,ll>> a;
-	vector<bool> factorized;
-	cin>>n;
-	for(ll i=0;i<n;++i)
-	{
-		ll x;
-		cin>>x;
-		freq[x]++;
-	}
-	n=freq.size();
-	for(auto &v:freq)
-	{
-		a.push_back(v);
-		factorized.push_back(0);
-	}
-	for(ll i=0;i<n;++i)
-	{
-		ll s=square_root(a[i].first),c=cube_root(a[i].first);
-		if(s*s==a[i].first)
-		{
-			ll f=square_root(s);
-			if(f*f==s)
-			{
-				mp[f]+=4*a[i].second;
-			}
-			else
-			{
-				mp[s]+=2*a[i].second;
-			}
-			factorized[i]=1;
-		}
-		if(c*c*c==a[i].first)
-		{
-			mp[c]+=3*a[i].second;
-			factorized[i]=1;
-		}
-	}
-	for(ll i=0;i<n-1;++i)
-	{
-		for(ll j=i+1;j<n;++j)
-		{
-			ll g=gcd(a[i].first,a[j].first);
-			if(g==1)
-			{
-				continue;
-			}
-			if(!factorized[i])
-			{
-				mp[g]+=a[i].second;
-				mp[a[i].first/g]+=a[i].second;
-				factorized[i]=1;
-			}
-			if(!factorized[j])
-			{
-				mp[g]+=a[j].second;
-				mp[a[j].first/g]+=a[j].second;
-				factorized[j]=1;
-			}
-		}
-	}
-	for(ll i=0;i<n;++i)
-	{
-		if(!factorized[i])
-		{
-			ans=mul(ans,mul(1+a[i].second,1+a[i].second));
-		}
-	}
-	for(auto &v:mp)
-	{
-		ans=mul(ans,1+v.second);
-	}
-	cout<<ans<<endl;
+	ll n,x,y;
+	cin>>n>>x>>y;
+	bfs(x,y);
+	cout<<dist[n]<<'\n';
 	return 0;
-}	
+}
