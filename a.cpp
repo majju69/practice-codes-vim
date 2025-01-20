bd2aa85ffd00b9a43be1babaf8a45ad0cd6d6244
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-20 12:32:49 +0530

    https://codeforces.com/contest/1604/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -1,80 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-inline int ask(int l,int r)
-{
-	cout<<"? "<<l+1<<' '<<r+1<<endl;
-	int x;
-	cin>>x;
-	return x-1;
-}
+#ifdef LOCAL
+	#include"debug.h"
+#else
+	#define debug(x)
+#endif
 
-inline void reply(int x)
-{
-	cout<<"! "<<x+1<<endl;
-}
+typedef long long ll;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,idx=-1,left=-1;
-	map<pair<int,int>,int> mp;
-	cin>>n;
-	idx=ask(0,n-1);
-	mp[{0,n-1}]=idx;
-	if(idx!=0)
+	ll tc;
+	cin>>tc;
+	while(tc--)
 	{
-		left=ask(0,idx);
-		mp[{0,idx}]=left;
-		if(left!=idx)
+		ll x,y;
+		cin>>x>>y;
+		if(x<y)
 		{
-			left=-1;
+			ll q=y/x,r=y%x;
+			cout<<q*x+(r>>1)<<'\n';
 		}
-	}
-	if(left==-1)
-	{
-		int lo=idx+1,hi=n-1,ans=-1;
-		while(lo<=hi)
+		else if(x>y)
 		{
-			int mid=lo+(hi-lo)/2;
-			int cur=((mp.count({idx,mid}))?mp[{idx,mid}]:ask(idx,mid));
-			mp[{idx,mid}]=cur;
-			if(cur==idx)
-			{
-				ans=mid;
-				hi=mid-1;
-			}
-			else
-			{
-				lo=mid+1;
-			}
+			cout<<y*(x+1)<<'\n';
 		}
-		reply(ans);
-	}
-	else
-	{
-		int lo=0,hi=idx-1,ans=-1;
-		while(lo<=hi)
+		else
 		{
-			int mid=lo+(hi-lo)/2;
-			int cur=((mp.count({mid,idx}))?mp[{mid,idx}]:ask(mid,idx));
-			mp[{mid,idx}]=cur;
-			if(cur==idx)
-			{
-				ans=mid;
-				lo=mid+1;
-			}
-			else
-			{
-				hi=mid-1;
-			}
+			cout<<x<<'\n';
 		}
-		reply(ans);
 	}
 	return 0;
 }
-
-
-// The same code for hard version
