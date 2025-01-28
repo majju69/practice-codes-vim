fd942e0ff55fa5403ec0e778b10a844174579158
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-28 15:21:11 +0530

    https://codeforces.com/contest/1780/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,38 +7,59 @@ using namespace std;
 	#define debug(x)
 #endif
 
+inline int ask(int n)
+{
+	cout<<"- "<<n<<endl;
+	int x;
+	cin>>x;
+	if(x==-1)
+	{
+		exit(1);
+	}
+	return x;
+}
+
+inline void reply(int x)
+{
+	cout<<"! "<<x<<endl;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	string s;
-	cin>>s;
-	int q,n=s.size();
-	vector<vector<int>> dp(n,vector<int>(n,0));
-	for(int i=n-1;i>=0;--i)
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		dp[i][i]=1;
-		for(int j=i+1;j<n;++j)
+		int cnt,real_cnt=-1,i=0,ans=0;
+		cin>>cnt;
+		real_cnt=cnt;
+		while(real_cnt>0&&i<30)
 		{
-			dp[i][j]=(s[i]==s[j]&&(i+1>j-1||dp[i+1][j-1]));
+			int pvs=cnt;
+			cnt=ask(1<<i);
+			if(pvs>cnt)
+			{
+				ans+=(1<<i);
+				i++;
+			}
+			else
+			{
+				ans+=(1<<(i+cnt-pvs+1));
+				i+=cnt-pvs+2;
+			}
+			real_cnt--;
 		}
-	}
-	for(int i=0;i<n;++i)
-	{
-		for(int j=0;j<n;++j)
-		{
-			dp[i][j]+=((i>=1)?dp[i-1][j]:0)+((j>=1)?dp[i][j-1]:0)-((i>=1&&j>=1)?dp[i-1][j-1]:0);
-		}
-	}
-	cin>>q;
-	while(q--)
-	{
-		int l,r;
-		cin>>l>>r;
-		l--;
-		r--;
-		cout<<dp[r][r]-((l>=1)?(dp[l-1][r]+dp[r][l-1]-dp[l-1][l-1]):0)<<'\n';
+		reply(ans);
 	}
 	return 0;
 }
+
+
+/*
+
+10100
+
+*/
