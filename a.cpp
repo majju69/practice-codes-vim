eacc44e3f2a1b1cb13e64ac1502a4f892c07bb58
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-01-29 22:58:12 +0530

    https://codeforces.com/contest/346/problem/A accepted

--- a.cpp
+++ a.cpp
@@ -7,21 +7,9 @@ using namespace std;
 	#define debug(x)
 #endif
 
-inline int ask(int n)
+int gcd(int a,int b)
 {
-	cout<<"- "<<n<<endl;
-	int x;
-	cin>>x;
-	if(x==-1)
-	{
-		exit(1);
-	}
-	return x;
-}
-
-inline void reply(int x)
-{
-	cout<<"! "<<x<<endl;
+	return ((b==0)?a:gcd(b,a%b));
 }
 
 int main()
@@ -29,37 +17,15 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,g=0,mx=0;
+	cin>>n;
+	for(int i=0;i<n;++i)
 	{
-		int cnt,real_cnt=-1,i=0,ans=0;
-		cin>>cnt;
-		real_cnt=cnt;
-		while(real_cnt>0&&i<30)
-		{
-			int pvs=cnt;
-			cnt=ask(1<<i);
-			if(pvs>cnt)
-			{
-				ans+=(1<<i);
-				i++;
-			}
-			else
-			{
-				ans+=(1<<(i+cnt-pvs+1));
-				i+=cnt-pvs+2;
-			}
-			real_cnt--;
-		}
-		reply(ans);
+		int x;
+		cin>>x;
+		g=gcd(g,x);
+		mx=max(mx,x);
 	}
+	cout<<(((mx/g-n)&1)?"Alice":"Bob")<<'\n';
 	return 0;
 }
-
-
-/*
-
-10100
-
-*/
