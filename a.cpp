6a6183fefdff0075c95597c4d786923e71078cd3
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-03 17:26:54 +0530

    https://codeforces.com/contest/2022/problem/D1 accepted

--- a.cpp
+++ a.cpp
@@ -7,9 +7,21 @@ using namespace std;
 	#define debug(x)
 #endif
 
-inline int get(int u,int v,int n)
+inline int ask(int i,int j)
 {
-	return u*n+v;
+	cout<<"? "<<i+1<<' '<<j+1<<endl;
+	int x;
+	cin>>x;
+	if(x==-1)
+	{
+		exit(1);
+	}
+	return x;
+}
+
+inline void reply(int x)
+{
+	cout<<"! "<<x+1<<endl;
 }
 
 int main()
@@ -21,89 +33,38 @@ int main()
 	cin>>tc;
 	while(tc--)
 	{
-		int n,s1,s2,m1,m2,ans=2e9;
-		set<pair<int,int>> st;
-		vector<pair<int,int>> edges1,edges2,common;
-		cin>>n>>s1>>s2;
-		s1--;
-		s2--;
-		vector<pair<int,int>> adj[n*n];
-		vector<int> dist(n*n,2e9);
-		vector<bool> present(n*n,0);
-		cin>>m1;
-		for(int i=0;i<m1;++i)
+		int n;
+		cin>>n;
+		vector<int> candy;
+		for(int i=0;i<n-1;i+=2)
 		{
-			int u,v;
-			cin>>u>>v;
-			u--;
-			v--;
-			if(u>v)
+			int a1=ask(i,i+1),a2=ask(i+1,i);
+			if(a1!=a2)
 			{
-				swap(u,v);
-			}
-			edges1.push_back({u,v});
-			present[get(u,v,n)]=1;
-		}
-		cin>>m2;
-		for(int i=0;i<m2;++i)
-		{
-			int u,v;
-			cin>>u>>v;
-			u--;
-			v--;
-			if(u>v)
-			{
-				swap(u,v);
-			}
-			edges2.push_back({u,v});
-			if(present[get(u,v,n)])
-			{
-				common.push_back({u,v});
+				candy.push_back(i);
+				candy.push_back(i+1);
 			}
 		}
-		for(auto &edge1:edges1)
+		if((int)candy.size()==0)
 		{
-			for(auto &edge2:edges2)
-			{
-				int u1=edge1.first,u2=edge1.second,v1=edge2.first,v2=edge2.second;
-				adj[get(u1,v1,n)].push_back({get(u2,v2,n),abs(u2-v2)});
-				adj[get(u2,v2,n)].push_back({get(u1,v1,n),abs(u1-v1)});
-				adj[get(u1,v2,n)].push_back({get(u2,v1,n),abs(u2-v1)});
-				adj[get(u2,v1,n)].push_back({get(u1,v2,n),abs(u1-v2)});
-			}
+			reply(n-1);
 		}
-		dist[get(s1,s2,n)]=0;
-		st.insert({0,get(s1,s2,n)});
-		while(st.size())
+		else
 		{
-			auto it=st.begin();
-			int node=it->second,dis=it->first;
-			st.erase(*it);
-			for(auto &v:adj[node])
+			int idx=-1,ans=-1,a1=-1,a2=-1;
+			for(int i=0;i<n;++i)
 			{
-				int curNode=v.first,curDist=v.second;
-				if(dist[curNode]>dis+curDist)
+				if(i!=candy[0]&&i!=candy[1])
 				{
-					if(dist[curNode]!=2e9)
-					{
-						st.erase({dist[curNode],curNode});
-					}
-					dist[curNode]=dis+curDist;
-					st.insert({dist[curNode],curNode});
+					idx=i;
+					break;
 				}
 			}
+			a1=ask(idx,candy[0]);
+			a2=ask(candy[0],idx);
+			ans=((a1==a2)?candy[1]:candy[0]);
+			reply(ans);
 		}
-		for(auto &edge:common)
-		{
-			int u=edge.first,v=edge.second;
-			ans=min(ans,dist[get(u,u,n)]);
-			ans=min(ans,dist[get(v,v,n)]);
-		}
-		if(ans==2e9)
-		{
-			ans=-1;
-		}
-		cout<<ans<<'\n';
 	}
 	return 0;
 }
