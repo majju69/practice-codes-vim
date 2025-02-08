9d7f05809cd143ef130ea6bbfaaf50eb5f83f201
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-08 22:41:09 +0530

    https://codeforces.com/contest/365/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,19 +9,15 @@ using namespace std;
 
 typedef long long ll;
 
-ll gcd(ll a,ll b,ll *x,ll *y)
+ll get(ll d,vector<ll> &pre)
 {
-	if(a==0)
+	ll n=pre.size(),ans=0;
+	for(ll i=0;i<n;++i)
 	{
-		*x=0;
-		*y=1;
-		return b;
+		ll req=d+((i==0)?0:pre[i-1]);
+		ans+=upper_bound(pre.begin()+i,pre.end(),req)-lower_bound(pre.begin()+i,pre.end(),req);
 	}
-	ll x1,y1;
-	ll g=gcd(b%a,a,&x1,&y1);
-	*x=y1-(b/a)*x1;
-	*y=x1;
-	return g;
+	return ans;
 }
 
 int main()
@@ -29,17 +25,33 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll a,b,c,x,y,g=-1;
-	cin>>a>>b>>c;
-	g=gcd(a,b,&x,&y);
-	if(c%g)
+	ll a,n=0,ans=0;
+	string s;
+	cin>>a>>s;
+	n=s.size();
+	vector<ll> pre(n,0);
+	pre[0]=(s[0]-'0');
+	for(ll i=1;i<n;++i)
 	{
-		cout<<-1<<'\n';
+		pre[i]=pre[i-1]+(s[i]-'0');
+	}
+	if(a==0)
+	{
+		ll cnt=get(0,pre);
+		ans=n*(n+1)*cnt-cnt*cnt;
 	}
 	else
 	{
-		c/=g;
-		cout<<-c*x<<' '<<-c*y<<'\n';
+		for(ll i=1;i*i<=a;++i)
+		{
+			if(a%i==0)
+			{
+				ll d1=i,d2=a/i;
+				ll cnt=get(d1,pre)*get(d2,pre);
+				ans+=(1+(i*i!=a))*cnt;
+			}
+		}
 	}
+	cout<<ans<<'\n';
 	return 0;
 }
