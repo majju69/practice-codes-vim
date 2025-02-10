ee96e4be3476ccb49bfb2e06ee23186fe74ba184
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-11 02:35:53 +0530

    https://codeforces.com/contest/1287/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,23 +7,83 @@ using namespace std;
 	#define debug(x)
 #endif
 
-inline int ask(int i,int j,char ch)
+int dp[101][101][101][3];
+
+int solve(int i,int odd,int even,int pvs,vector<int> &a)	// pvs=1 => pvs odd
 {
-	cout<<((ch=='|')?"or ":"and ")<<i+1<<' '<<j+1<<endl;
-	int x;
-	cin>>x;
-	return x;
+	if(i>=(int)a.size())
+	{
+		return 0;
+	}
+	if(dp[i][odd][even][pvs]!=-1)
+	{
+		return dp[i][odd][even][pvs];
+	}
+	if(a[i]!=-1)
+	{
+		return dp[i][odd][even][pvs]=solve(i+1,odd,even,(a[i]&1),a)+((a[i]+pvs)&1);
+	}
+	else
+	{
+		int ans=1e9;
+		if(odd>=1)
+		{
+			ans=min(ans,solve(i+1,odd-1,even,1,a)+(pvs==0));
+		}
+		if(even>=1)
+		{
+			ans=min(ans,solve(i+1,odd,even-1,0,a)+(pvs==1));
+		}
+		return dp[i][odd][even][pvs]=ans;
+	}
 }
 
-inline void reply(vector<int> &a,int k)
+int minComplexity(vector<int> &a)
 {
-	sort(a.begin(),a.end());
-	cout<<"finish "<<a[k]<<endl;
-}
-
-inline int bit(int a,int i)
-{
-	return a>>i&1;
+	int n=a.size();
+	int even=((n-1)>>1)+1;
+	int odd=n-even;
+	for(auto &v:a)
+	{
+		if(v!=-1)
+		{
+			if(v&1)
+			{
+				odd--;
+			}
+			else
+			{
+				even--;
+			}
+		}
+	}
+	for(int i=0;i<=n;++i)
+	{
+		for(int j=0;j<=odd;++j)
+		{
+			for(int k=0;k<=even;++k)
+			{
+				dp[i][j][k][0]=dp[i][j][k][1]=dp[i][j][k][2]=-1;
+			}
+		}
+	}
+	if(a[0]!=-1)
+	{
+		return solve(1,odd,even,(a[0]&1),a);
+	}
+	else
+	{
+		int ans=1e9;
+		if(odd>=1)
+		{
+			ans=min(ans,solve(1,odd-1,even,1,a));
+		}
+		if(even>=1)
+		{
+			ans=min(ans,solve(1,odd,even-1,0,a));
+		}
+		return ans;
+	}
 }
 
 int main()
@@ -31,94 +91,14 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,k,and12=-1,or12=-1,and23=-1,or23=-1,and13=-1,or13=-1;
-	cin>>n>>k;
-	k--;
-	vector<int> a(n,0),a0(30,-1),a1(30,-1),a2(30,-1);
-	and12=ask(0,1,'&');
-	or12=ask(0,1,'|');
-	and23=ask(1,2,'&');
-	or23=ask(1,2,'|');
-	and13=ask(0,2,'&');
-	or13=ask(0,2,'|');
-	for(int i=0;i<30;++i)
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	for(auto &v:a)
 	{
-		if(bit(or12,i)==0)
-		{
-			a0[i]=a1[i]=0;
-		}
-		if(bit(or23,i)==0)
-		{
-			a1[i]=a2[i]=0;
-		}
-		if(bit(or13,i)==0)
-		{
-			a0[i]=a2[i]=0;
-		}
-		if(bit(and12,i)==1)
-		{
-			a0[i]=a1[i]=1;
-		}
-		if(bit(and23,i)==1)
-		{
-			a1[i]=a2[i]=1;
-		}
-		if(bit(and13,i)==1)
-		{
-			a0[i]=a2[i]=1;
-		}
+		cin>>v;
+		v--;
 	}
-	for(int i=0;i<30;++i)
-	{
-		if(a0[i]==-1)
-		{
-			a0[i]=1-a1[i];
-		}
-		if(a1[i]==-1)
-		{
-			a1[i]=1-a0[i];
-		}
-		if(a2[i]==-1)
-		{
-			a2[i]=1-a0[i];
-		}
-	}
-	for(int i=0;i<30;++i)
-	{
-		if(a0[i])
-		{
-			a[0]+=(1<<i);
-		}
-		if(a1[i])
-		{
-			a[1]+=(1<<i);
-		}
-		if(a2[i])
-		{
-			a[2]+=(1<<i);
-		}
-	}
-	for(int i=3;i<n;++i)
-	{
-		int _or=ask(0,i,'|'),_and=ask(0,i,'&');
-		for(int j=0;j<30;++j)
-		{
-			if(bit(_or,j)==0)
-			{
-				continue;
-			}
-			if(bit(_and,j)==1)
-			{
-				a[i]+=(1<<j);
-				continue;
-			}
-			if(bit(a[0],j)==0)
-			{
-				a[i]+=(1<<j);
-				continue;
-			}
-		}
-	}
-	reply(a,k);
+	cout<<minComplexity(a)<<'\n';
 	return 0;
 }
