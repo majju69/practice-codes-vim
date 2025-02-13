d093b297d49a4f0d48c9fe89f024a42874d83aa2
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-14 02:17:37 +0530

    https://codeforces.com/contest/25/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,105 +9,42 @@ using namespace std;
 
 typedef long long ll;
 
-inline pair<ll,ll> merge(pair<ll,ll> a,pair<ll,ll> b)
-{
-	return {min(a.first,a.second+b.first),a.second+b.second};
-}
-
-class SegmentTree
-{
-
-private:
-
-	vector<pair<ll,ll>> seg;	// {pref,sum}
-
-public:
-
-	SegmentTree(ll n)
-	{
-		seg.resize(4*n+1);
-	}
-
-	void build(ll ind,ll lo,ll hi,vector<ll> &a)
-	{
-		if(lo==hi)
-		{
-			seg[ind]={min(a[lo],0LL),a[lo]};
-			return;
-		}
-		ll mid=lo+(hi-lo)/2;
-		build(2*ind+1,lo,mid,a);
-		build(2*ind+2,mid+1,hi,a);
-		seg[ind]=merge(seg[2*ind+1],seg[2*ind+2]);
-	}
-
-	pair<ll,ll> query(ll ind,ll lo,ll hi,ll l,ll r)
-	{
-		if(l>hi||lo>r)
-		{
-			return {1e18,0};
-		}
-		if(l<=lo&&hi<=r)
-		{
-			return seg[ind];
-		}
-		ll mid=lo+(hi-lo)/2;
-		return merge(query(2*ind+1,lo,mid,l,r),query(2*ind+2,mid+1,hi,l,r));
-	}
-
-};
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll tc;
-	cin>>tc;
-	while(tc--)
+	ll n,q;
+	cin>>n;
+	vector<vector<ll>> a(n,vector<ll>(n,0));
+	for(auto &vec:a)
 	{
-		ll n,s,lo=1,hi=0,ans_l=-1,ans_r=-1;
-		cin>>n>>s;
-		vector<ll> a(n);
-		for(auto &v:a)
+		for(auto &v:vec)
 		{
 			cin>>v;
 		}
-		SegmentTree st(n);
-		st.build(0,0,n-1,a);
-		hi=n;
-		while(lo<=hi)
-		{
-			ll mid=lo+(hi-lo)/2;
-			ll idx=-1;
-			for(ll i=0;i<n-mid+1;++i)
-			{
-				pair<ll,ll> p=st.query(0,0,n-1,i,i+mid-1);
-				if(p.first+s>=0)
-				{
-					idx=i;
-					break;
-				}
-			}
-			if(idx!=-1)
-			{
-				ans_l=idx;
-				ans_r=idx+mid-1;
-				lo=mid+1;
-			}
-			else
-			{
-				hi=mid-1;
-			}
-		}
-		if(ans_l==-1)
-		{
-			cout<<-1<<'\n';
-		}
-		else
-		{
-			cout<<ans_l+1<<' '<<ans_r+1<<'\n';
-		}
 	}
+	cin>>q;
+	while(q--)
+	{
+		ll x,y,c,sum=0;
+		cin>>x>>y>>c;
+		x--;
+		y--;
+		a[x][y]=min(a[x][y],c);
+		a[y][x]=min(a[y][x],c);
+		for(ll i=0;i<n;++i)
+		{
+			for(ll j=0;j<n;++j)
+			{
+				ll new_dist=min(a[i][x]+a[x][y]+a[y][j],a[i][y]+a[y][x]+a[x][j]);
+				a[i][j]=min(a[i][j],new_dist);
+				sum+=a[i][j];
+			}
+		}
+		sum>>=1;
+		cout<<sum<<' ';
+	}
+	cout<<'\n';
 	return 0;
 }
