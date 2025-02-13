565ba0a56c608d67e2cbef0135a50bac1a0931ca
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-14 03:28:02 +0530

    https://codeforces.com/contest/1471/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -9,42 +9,86 @@ using namespace std;
 
 typedef long long ll;
 
+vector<long long> lpf(1000001,0);
+vector<long long> primes;
+
+void leastPrimeFactor()
+{
+    long long n=lpf.size();
+    for(long long i=2;i<n;++i)
+    {
+        if(lpf[i]==0)
+        {
+            lpf[i]=i;
+            primes.push_back(i);
+        }
+        for(long long j=0;i*primes[j]<n;++j)
+        {
+            lpf[i*primes[j]]=primes[j];
+            if(primes[j]==lpf[i])
+            {
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,q;
-	cin>>n;
-	vector<vector<ll>> a(n,vector<ll>(n,0));
-	for(auto &vec:a)
+	leastPrimeFactor();
+	ll tc;
+	cin>>tc;
+	while(tc--)
 	{
-		for(auto &v:vec)
-		{
-			cin>>v;
-		}
-	}
-	cin>>q;
-	while(q--)
-	{
-		ll x,y,c,sum=0;
-		cin>>x>>y>>c;
-		x--;
-		y--;
-		a[x][y]=min(a[x][y],c);
-		a[y][x]=min(a[y][x],c);
+		ll n,q,mx=0,even=0,odd=0;
+		map<ll,ll> mp;
+		cin>>n;
 		for(ll i=0;i<n;++i)
 		{
-			for(ll j=0;j<n;++j)
+			ll x,real_x=1;
+			map<ll,ll> tmp;
+			cin>>x;
+			while(x>1)
 			{
-				ll new_dist=min(a[i][x]+a[x][y]+a[y][j],a[i][y]+a[y][x]+a[x][j]);
-				a[i][j]=min(a[i][j],new_dist);
-				sum+=a[i][j];
+				tmp[lpf[x]]++;
+				x/=lpf[x];
+			}
+			for(auto &v:tmp)
+			{
+				if(v.second&1)
+				{
+					real_x*=v.first;
+				}
+			}
+			mp[real_x]++;
+			mx=max(mx,mp[real_x]);
+		}
+		for(auto &v:mp)
+		{
+			if(v.first>1)
+			{
+				if(v.second&1)
+				{
+					odd=max(odd,v.second);
+					continue;
+				}
+				even+=v.second;
+			}
+			else
+			{
+				even+=v.second;
 			}
 		}
-		sum>>=1;
-		cout<<sum<<' ';
+		cin>>q;
+		while(q--)
+		{
+			ll x;
+			cin>>x;
+			cout<<((x==0)?mx:max(even,odd))<<'\n';
+		}
 	}
-	cout<<'\n';
 	return 0;
 }
