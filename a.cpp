20495466bb980d174efab07727c2b3f1dc0fa3ed
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-17 00:45:44 +0530

    http://codeforces.com/contest/1744/problem/E2 accepted

--- a.cpp
+++ a.cpp
@@ -7,67 +7,70 @@ using namespace std;
 	#define debug(x)
 #endif
 
-vector<vector<int>> divisors(200001);
+typedef long long ll;
 
-void fillDivisors()
-{
-	int n=divisors.size();
-	for(int i=2;i<n;i++)
-	{
-		for(int j=i;j<n;j+=i)
-		{
-			divisors[j].push_back(i);
-		}
-	}
-}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	fillDivisors();
-	int tc;
+	ll tc;
 	cin>>tc;
 	while(tc--)
 	{
-		int n,x,ans=1;
-		map<int,int> mp;
-		cin>>n>>x;
-		vector<int> div=divisors[x];
-		for(int i=0;i<n;++i)
+		ll a,b,c,d,x=-1,y=-1;
+		vector<ll> div_a,div_b;
+		cin>>a>>b>>c>>d;
+		for(ll i=1;i*i<=a;++i)
 		{
-			int tmp;
-			set<int> st;
-			cin>>tmp;
-			if(tmp==1||x%tmp)
+			if(a%i==0)
 			{
-				continue;
-			}
-			for(auto &d:div)
-			{
-				if(d%tmp)
+				div_a.push_back(a/i);
+				if(i*i!=a)
 				{
-					continue;
+					div_a.push_back(i);
 				}
-				if(mp.count(d/tmp))
-				{
-					st.insert(d);
-				}
-			}
-			for(auto &v:st)
-			{
-				mp[v]++;
-			}
-			mp[tmp]++;
-			if(mp.count(x))
-			{
-				ans++;
-				mp.clear();
-				mp[tmp]=1;
 			}
 		}
-		cout<<ans<<'\n';
+		for(ll i=1;i*i<=b;++i)
+		{
+			if(b%i==0)
+			{
+				div_b.push_back(b/i);
+				if(i*i!=b)
+				{
+					div_b.push_back(i);
+				}
+			}
+		}
+		for(auto &d_a:div_a)
+		{
+			if(x!=-1)
+			{
+				break;
+			}
+			for(auto &d_b:div_b)
+			{
+				ll tmp_x=d_a*d_b;
+				ll tmp_y=(a*b)/tmp_x;
+				if((c/tmp_x)*tmp_x>a&&(d/tmp_y)*tmp_y>b)
+				{
+					x=(c/tmp_x)*tmp_x;
+					y=(d/tmp_y)*tmp_y;
+					break;
+				}
+				tmp_y=d_a*d_b;
+				tmp_x=(a*b)/tmp_y;
+				if((c/tmp_x)*tmp_x>a&&(d/tmp_y)*tmp_y>b)
+				{
+					x=(c/tmp_x)*tmp_x;
+					y=(d/tmp_y)*tmp_y;
+					break;
+				}
+			}
+		}
+		cout<<x<<' '<<y<<'\n';
 	}
 	return 0;
 }
