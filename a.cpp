9f0d30f41232520cc9aa60f592d6b6520b4bdb21
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-02-26 04:52:26 +0530

    https://codeforces.com/contest/2072/problem/F accepted

--- a.cpp
+++ a.cpp
@@ -7,65 +7,37 @@ using namespace std;
 	#define debug(x)
 #endif
 
+typedef long long ll;
+
+ll get(ll n)
+{
+	ll ans=0,cur=2;
+	while(cur<=n)
+	{
+		ans+=n/cur;
+		cur<<=1;
+	}
+	return ans;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
+	ll tc;
 	cin>>tc;
 	while(tc--)
 	{
-		int n,k,q,i=0,j=-1;
-		map<int,int> mp;
-		multiset<int> st;
-		cin>>n>>k>>q;
-		vector<int> a(n),ans;
-		for(int i=0;i<n;++i)
+		ll n,k;
+		cin>>n>>k;
+		n--;
+		for(ll i=0;i<=n;++i)
 		{
-			cin>>a[i];
-			a[i]-=i;
+			ll num=get(n),den=get(i)+get(n-i);
+			cout<<((num>den)?0LL:k)<<' ';
 		}
-		for(int i=0;i<k;++i)
-		{
-			mp[a[i]]++;
-		}
-		for(auto &v:mp)
-		{
-			st.insert(v.second);
-		}
-		ans.push_back(*st.rbegin());
-		j=k;
-		while(j<n)
-		{
-			st.erase(st.find(mp[a[i]]));
-			mp[a[i]]--;
-			if(mp[a[i]]!=0)
-			{
-				st.insert(mp[a[i]]);
-			}
-			else
-			{
-				mp.erase(a[i]);
-			}
-			if(mp.count(a[j]))
-			{
-				st.erase(st.find(mp[a[j]]));
-			}
-			mp[a[j]]++;
-			st.insert(mp[a[j]]);
-			ans.push_back(*st.rbegin());
-			i++;
-			j++;
-		}
-		while(q--)
-		{
-			int l,r;
-			cin>>l>>r;
-			l--;
-			r--;
-			cout<<k-ans[l]<<'\n';
-		}
+		cout<<'\n';
 	}
 	return 0;
 }
