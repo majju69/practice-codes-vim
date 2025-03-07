49907f500e124010e9a09fd80501db0d5d6838b3
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-07 21:55:59 +0530

    https://codeforces.com/contest/1582/problem/F1 accepted

--- a.cpp
+++ a.cpp
@@ -7,86 +7,42 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-const ll mod=1e9+7;
-
-inline ll add(ll a,ll b)
-{
-	return (a%mod+b%mod)%mod;
-}
-
-vector<long long> fact(1000001);
-
-void fillFact()
-{
-	long long n=fact.size();
-	fact[0]=1;
-	for(long long i=1;i<n;++i)
-	{
-		fact[i]=(i%mod*fact[i-1]%mod)%mod;
-	}
-}
-
-long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
-{
-	long long ans=1;
-	a=a%mod;
-	while(b)
-	{
-		if(b&1)
-		{
-			ans=(ans*a)%mod;
-		}
-		a=(a*a)%mod;
-		b>>=1;
-	}
-	return ans%mod;
-}
-
-long long nCr(long long n,long long r)         // Ensure that fillFact() is called before this function is used
-{
-	if(n<r||n<0||r<0)
-	{
-		return 0;
-	}
-	if(r==n||r==0)
-	{
-		return 1;
-	}
-	return (fact[n]*power(fact[r],mod-2)%mod*power(fact[n-r],mod-2)%mod)%mod;
-}
-
-bool isGood(ll x,ll a,ll b)
-{
-	while(x)
-	{
-		ll rem=x%10;
-		if(rem!=a&&rem!=b)
-		{
-			return 0;
-		}
-		x/=10;
-	}
-	return 1;
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	fillFact();
-	ll a,b,n,ans=0;
-	cin>>a>>b>>n;
-	for(ll i=0;i<=n;++i)	// i  a's
+	int n;
+	vector<int> a(512,512);
+	set<int> ans;
+	cin>>n;
+	for(int i=0;i<n;++i)
 	{
-		ll sum=i*a+(n-i)*b;
-		if(isGood(sum,a,b))
+		int x;
+		cin>>x;
+		a[x]=min(x,a[x]);
+		for(int j=0;j<512;++j)
 		{
-			ans=add(ans,nCr(n,i));
+			int req=(j^x);
+			if(a[req]<x)
+			{
+				a[j]=min(a[j],x);
+			}
 		}
 	}
-	cout<<ans<<'\n';
+	for(int i=0;i<512;++i)
+	{
+		if(a[i]!=512)
+		{
+			ans.insert(i);
+		}
+	}
+	ans.insert(0);
+	cout<<(int)ans.size()<<'\n';
+	for(auto &v:ans)
+	{
+		cout<<v<<' ';
+	}
+	cout<<'\n';
 	return 0;
 }
