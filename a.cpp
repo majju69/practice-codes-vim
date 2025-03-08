377814c5e6e0902d7eab669458e6823abc2d549b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-08 13:35:53 +0530

    https://codeforces.com/contest/1183/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,42 +7,62 @@ using namespace std;
 	#define debug(x)
 #endif
 
+vector<string> get(string &s)
+{
+	int n=s.size();
+	vector<string> ans;
+	for(int i=0;i<n;++i)
+	{
+		string cur;
+		for(int j=0;j<n;++j)
+		{
+			if(j!=i)
+			{
+				cur.push_back(s[j]);
+			}
+		}
+		ans.push_back(cur);
+	}
+	return ans;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n;
-	vector<int> a(512,512);
-	set<int> ans;
-	cin>>n;
-	for(int i=0;i<n;++i)
+	int n,k,ans=0,cnt=0;
+	string s;
+	queue<pair<string,int>> q;
+	map<string,bool> vis;
+	cin>>n>>k>>s;
+	q.push({s,0});
+	vis[s]=1;
+	while(q.size())
 	{
-		int x;
-		cin>>x;
-		a[x]=min(x,a[x]);
-		for(int j=0;j<512;++j)
+		string cur=q.front().first;
+		int cost=q.front().second;
+		q.pop();
+		cnt++;
+		ans+=cost;
+		if(cnt>=k)
 		{
-			int req=(j^x);
-			if(a[req]<x)
+			break;
+		}
+		vector<string> nbrs=get(cur);
+		for(auto &v:nbrs)
+		{
+			if(!vis[v])
 			{
-				a[j]=min(a[j],x);
+				q.push({v,cost+1});
+				vis[v]=1;
 			}
 		}
 	}
-	for(int i=0;i<512;++i)
+	if(cnt<k)
 	{
-		if(a[i]!=512)
-		{
-			ans.insert(i);
-		}
+		ans=-1;
 	}
-	ans.insert(0);
-	cout<<(int)ans.size()<<'\n';
-	for(auto &v:ans)
-	{
-		cout<<v<<' ';
-	}
-	cout<<'\n';
+	cout<<ans<<'\n';
 	return 0;
 }
