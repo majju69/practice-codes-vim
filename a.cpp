148aad006e99a72d6653aeccf1c6b8b58c18b432
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-11 14:14:01 +0530

    https://codeforces.com/contest/1486/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,23 +7,38 @@ using namespace std;
 	#define debug(x)
 #endif
 
-vector<string> get(string &s)
+bool check(int mid,int k,vector<int> &arr)
 {
-	int n=s.size();
-	vector<string> ans;
+	int n=arr.size(),sum=0;
+	vector<int> a,pre,dp(n,0);
+	for(auto &v:arr)
+	{
+		if(v>=mid)
+		{
+			a.push_back(1);
+		}
+		else
+		{
+			a.push_back(-1);
+		}
+		sum+=a.back();
+		pre.push_back(sum);
+	}
 	for(int i=0;i<n;++i)
 	{
-		string cur;
-		for(int j=0;j<n;++j)
-		{
-			if(j!=i)
-			{
-				cur.push_back(s[j]);
-			}
-		}
-		ans.push_back(cur);
+		int s1=max(a[i],0),s2=a[i]+((i>0)?dp[i-1]:0);
+		dp[i]=max(s1,s2);
 	}
-	return ans;
+	for(int i=k-1;i<n;++i)
+	{
+		int s1=pre[i]-((i>=k)?pre[i-k]:0);
+		int s2=s1+((i>=k)?dp[i-k]:0);
+		if(max(s1,s2)>0)
+		{
+			return 1;
+		}
+	}
+	return 0;
 }
 
 int main()
@@ -31,37 +46,27 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,k,ans=0,cnt=0;
-	string s;
-	queue<pair<string,int>> q;
-	map<string,bool> vis;
-	cin>>n>>k>>s;
-	q.push({s,0});
-	vis[s]=1;
-	while(q.size())
+	int n,k,lo=1e9,hi=-1e9,ans=-1;
+	cin>>n>>k;
+	vector<int> a(n);
+	for(auto &v:a)
 	{
-		string cur=q.front().first;
-		int cost=q.front().second;
-		q.pop();
-		cnt++;
-		ans+=cost;
-		if(cnt>=k)
-		{
-			break;
-		}
-		vector<string> nbrs=get(cur);
-		for(auto &v:nbrs)
-		{
-			if(!vis[v])
-			{
-				q.push({v,cost+1});
-				vis[v]=1;
-			}
-		}
+		cin>>v;
+		lo=min(lo,v);
+		hi=max(hi,v);
 	}
-	if(cnt<k)
+	while(lo<=hi)
 	{
-		ans=-1;
+		int mid=lo+(hi-lo)/2;
+		if(check(mid,k,a))
+		{
+			ans=mid;
+			lo=mid+1;
+		}
+		else
+		{
+			hi=mid-1;
+		}
 	}
 	cout<<ans<<'\n';
 	return 0;
