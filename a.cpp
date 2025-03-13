9893962ccd786803b2a4f316c5aea12fa6d88bc9
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-13 22:22:21 +0530

    https://codeforces.com/contest/1304/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,17 +7,58 @@ using namespace std;
 	#define debug(x)
 #endif
 
-inline int ask(int i,int j,int k)
+const int N=1e5,LOG=18;
+int up[N][LOG],depth[N];
+
+void dfs(int node,int p,vector<int> adj[])
 {
-	cout<<"? "<<i+1<<' '<<j+1<<' '<<k+1<<endl;
-	int x;
-	cin>>x;
-	return x-1;
+	for(auto &v:adj[node])
+	{
+		if(v!=p)
+		{
+			depth[v]=depth[node]+1;
+			up[v][0]=node;
+			for(int j=1;j<LOG;++j)
+			{
+				up[v][j]=up[up[v][j-1]][j-1];
+			}
+			dfs(v,node,adj);
+		}
+	}
 }
 
-inline void reply(int i,int j,int k)
+int lowestCommonAncestor(int u,int v)
 {
-	cout<<"! "<<i+1<<' '<<j+1<<' '<<k+1<<endl;
+	if(depth[u]<depth[v])
+	{
+		swap(u,v);
+	}
+	int k=depth[u]-depth[v];
+	for(int j=LOG-1;j>=0;--j)
+	{
+		if(k>>j&1)
+		{
+			u=up[u][j];
+		}
+	}
+	if(u==v)
+	{
+		return u;
+	}
+	for(int j=LOG-1;j>=0;--j)
+	{
+		if(up[u][j]!=up[v][j])
+		{
+			u=up[u][j];
+			v=up[v][j];
+		}
+	}
+	return up[u][0];
+}
+
+inline int dist(int u,int v)
+{
+	return depth[u]+depth[v]-(depth[lowestCommonAncestor(u,v)]<<1);
 }
 
 int main()
@@ -25,42 +66,30 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,q;
+	cin>>n;
+	vector<int> adj[n];
+	for(int i=1;i<n;++i)
 	{
-		int n,x=0,y=1,z=2;
-		cin>>n;
-		while(1)
-		{
-			int new_point=ask(x,y,z);
-			if(new_point==-1)
-			{
-				reply(x,y,z);
-				break;
-			}
-			int new_x=ask(new_point,y,z);
-			if(new_x==-1)
-			{
-				reply(new_point,y,z);
-				break;
-			}
-			int new_y=ask(x,new_point,z);
-			if(new_y==-1)
-			{
-				reply(x,new_point,z);
-				break;
-			}
-			int new_z=ask(x,y,new_point);
-			if(new_z==-1)
-			{
-				reply(x,y,new_point);
-				break;
-			}
-			x=new_x;
-			y=new_y;
-			z=new_z;
-		}
+		int u,v;
+		cin>>u>>v;
+		u--;
+		v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	dfs(0,0,adj);
+	cin>>q;
+	while(q--)
+	{
+		int x,y,u,v,k;
+		cin>>x>>y>>u>>v>>k;
+		x--;
+		y--;
+		u--;
+		v--;
+		int d1=k-dist(u,v),d2=k-(dist(u,x)+dist(y,v)+1),d3=k-(dist(u,y)+dist(x,v)+1);
+		cout<<(((d1>=0&&d1%2==0)||(d2>=0&&d2%2==0)||(d3>=0&&d3%2==0))?"YES":"NO")<<'\n';
 	}
 	return 0;
 }
