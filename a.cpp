6c0061c62a238832da68c9e65db9172ba62371ed
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-15 04:10:15 +0530

    https://codeforces.com/contest/1473/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,59 +7,51 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-ll dp[300001][11];
-
-ll solve(ll i,ll k,vector<ll> &a)
-{
-	if(i>=(ll)a.size())
-	{
-		return 0;
-	}
-	if(dp[i][k]!=-1)
-	{
-		return dp[i][k];
-	}
-	ll ans=1e18,mn=1e18;
-	for(int j=i;j<=min(i+k,(ll)a.size()-1);++j)
-	{
-		mn=min(mn,a[j]);
-		ans=min(ans,solve(j+1,k+i-j,a)+(j-i+1)*mn);
-	}
-	return dp[i][k]=ans;
-}
-
-ll minSum(vector<ll> &a,ll k)
-{
-	ll n=a.size();
-	for(ll i=0;i<=n;++i)
-	{
-		for(ll j=0;j<=k;++j)
-		{
-			dp[i][j]=-1;
-		}
-	}
-	return solve(0,k,a);
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll tc;
+	int tc;
 	cin>>tc;
 	while(tc--)
 	{
-		ll n,k;
-		cin>>n>>k;
-		vector<ll> a(n);
-		for(auto &v:a)
+		int n,q,sum=0;
+		string s;
+		cin>>n>>q>>s;
+		vector<int> a(n),pre(n),pre_mx(n),suf_mx(n),pre_mn(n),suf_mn(n);
+		for(int i=0;i<n;++i)
 		{
-			cin>>v;
+			a[i]=((s[i]=='+')?1:-1);
+			sum+=a[i];
+			pre[i]=sum;
+			pre_mn[i]=((i==0)?pre[i]:min(pre_mn[i-1],pre[i]));
+			pre_mx[i]=((i==0)?pre[i]:max(pre_mx[i-1],pre[i]));
 		}
-		cout<<minSum(a,k)<<'\n';
+		suf_mn[n-1]=suf_mx[n-1]=pre[n-1];
+		for(int i=n-2;i>=0;--i)
+		{
+			suf_mn[i]=min(suf_mn[i+1],pre[i]);
+			suf_mx[i]=max(suf_mx[i+1],pre[i]);
+		}
+		while(q--)
+		{
+			int l,r,mn=0,mx=0;
+			cin>>l>>r;
+			l--;
+			r--;
+			if(l!=0)
+			{
+				mn=min(mn,pre_mn[l-1]);
+				mx=max(mx,pre_mx[l-1]);
+			}
+			if(r!=n-1)
+			{
+				mn=min(mn,suf_mn[r+1]-(pre[r]-((l==0)?0:pre[l-1])));
+				mx=max(mx,suf_mx[r+1]-(pre[r]-((l==0)?0:pre[l-1])));
+			}
+			cout<<mx-mn+1<<'\n';
+		}
 	}
 	return 0;
 }
