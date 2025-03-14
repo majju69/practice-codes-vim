4afcd107167bc9bcbd41d4609c6827ff8554c464
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-14 21:58:59 +0530

    https://codeforces.com/contest/1969/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,88 +9,57 @@ using namespace std;
 
 typedef long long ll;
 
-class SegmentTree
+ll dp[300001][11];
+
+ll solve(ll i,ll k,vector<ll> &a)
 {
-
-private:
-
-	vector<ll> seg;
-
-public:
-
-	SegmentTree(ll n)
+	if(i>=(ll)a.size())
 	{
-		seg.resize(4*n+1);
+		return 0;
 	}
-
-	void update(ll ind,ll lo,ll hi,ll i)
+	if(dp[i][k]!=-1)
 	{
-		if(lo==hi)
-		{
-			seg[ind]++;
-			return;
-		}
-		ll mid=lo+(hi-lo)/2;
-		if(i<=mid)
-		{
-			update(2*ind+1,lo,mid,i);
-		}
-		else
-		{
-			update(2*ind+2,mid+1,hi,i);
-		}
-		seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+		return dp[i][k];
 	}
-
-	ll query(ll ind,ll lo,ll hi,ll l,ll r)
+	ll ans=1e18,mn=1e18;
+	for(int j=i;j<=min(i+k,(ll)a.size()-1);++j)
 	{
-		if(l>hi||lo>r)
-		{
-			return 0;
-		}
-		if(l<=lo&&hi<=r)
-		{
-			return seg[ind];
-		}
-		ll mid=lo+(hi-lo)/2;
-		return query(2*ind+1,lo,mid,l,r)+query(2*ind+2,mid+1,hi,l,r);
+		mn=min(mn,a[j]);
+		ans=min(ans,solve(j+1,k+i-j,a)+(j-i+1)*mn);
 	}
+	return dp[i][k]=ans;
+}
 
-};
+ll minSum(vector<ll> &a,ll k)
+{
+	ll n=a.size();
+	for(ll i=0;i<=n;++i)
+	{
+		for(ll j=0;j<=k;++j)
+		{
+			dp[i][j]=-1;
+		}
+	}
+	return solve(0,k,a);
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,ans=0;
-	map<ll,ll> mp;
-	cin>>n;
-	vector<ll> a(n),pre(n),suf(n);
-	SegmentTree st(n+1);
-	for(auto &v:a)
+	ll tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cin>>v;
-	}
-	for(ll i=0;i<n;++i)
-	{
-		mp[a[i]]++;
-		pre[i]=mp[a[i]];
-	}
-	mp.clear();
-	for(ll i=n-1;i>=0;--i)
-	{
-		mp[a[i]]++;
-		suf[i]=mp[a[i]];
-	}
-	for(ll i=n-1;i>=0;--i)
-	{
-		if(pre[i]!=0)
+		ll n,k;
+		cin>>n>>k;
+		vector<ll> a(n);
+		for(auto &v:a)
 		{
-			ans+=st.query(0,0,n,0,pre[i]-1);
+			cin>>v;
 		}
-		st.update(0,0,n,suf[i]);
+		cout<<minSum(a,k)<<'\n';
 	}
-	cout<<ans<<'\n';
 	return 0;
 }
