72bda7f93d5b4a02dc3b80c670d0e33f989c702a
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-15 15:53:30 +0530

    https://codeforces.com/contest/1061/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,51 +7,67 @@ using namespace std;
 	#define debug(x)
 #endif
 
+typedef long long ll;
+
+const ll mod=1e9+7;
+
+inline ll add(ll a,ll b)
+{
+	return (a%mod+b%mod)%mod;
+}
+
+vector<vector<long long>> divisors(1000001);
+
+void fillDivisors()
+{
+	long long n=divisors.size();
+	divisors[0].push_back(0);
+	divisors[1].push_back(1);
+	for(long long i=2;i<n;i++)
+	{
+		divisors[i].push_back(1);
+	}
+	for(long long i=2;i<n;i++)
+	{
+		for(long long j=i;j<n;j+=i)
+		{
+			divisors[j].push_back(i);
+		}
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	fillDivisors();
+	for(auto &vec:divisors)
 	{
-		int n,q,sum=0;
-		string s;
-		cin>>n>>q>>s;
-		vector<int> a(n),pre(n),pre_mx(n),suf_mx(n),pre_mn(n),suf_mn(n);
-		for(int i=0;i<n;++i)
+		sort(vec.rbegin(),vec.rend());
+	}
+	ll n,ans=0,dp[1000001]={0};
+	cin>>n;
+	for(ll i=0;i<n;++i)
+	{
+		ll x;
+		cin>>x;
+		for(auto &d:divisors[x])
 		{
-			a[i]=((s[i]=='+')?1:-1);
-			sum+=a[i];
-			pre[i]=sum;
-			pre_mn[i]=((i==0)?pre[i]:min(pre_mn[i-1],pre[i]));
-			pre_mx[i]=((i==0)?pre[i]:max(pre_mx[i-1],pre[i]));
-		}
-		suf_mn[n-1]=suf_mx[n-1]=pre[n-1];
-		for(int i=n-2;i>=0;--i)
-		{
-			suf_mn[i]=min(suf_mn[i+1],pre[i]);
-			suf_mx[i]=max(suf_mx[i+1],pre[i]);
-		}
-		while(q--)
-		{
-			int l,r,mn=0,mx=0;
-			cin>>l>>r;
-			l--;
-			r--;
-			if(l!=0)
+			if(d==1)
 			{
-				mn=min(mn,pre_mn[l-1]);
-				mx=max(mx,pre_mx[l-1]);
+				dp[d]++;
 			}
-			if(r!=n-1)
+			else
 			{
-				mn=min(mn,suf_mn[r+1]-(pre[r]-((l==0)?0:pre[l-1])));
-				mx=max(mx,suf_mx[r+1]-(pre[r]-((l==0)?0:pre[l-1])));
+				dp[d]=add(dp[d],dp[d-1]);
 			}
-			cout<<mx-mn+1<<'\n';
 		}
 	}
+	for(ll i=1;i<=1000000;++i)
+	{
+		ans=add(ans,dp[i]);
+	}
+	cout<<ans<<'\n';
 	return 0;
 }
