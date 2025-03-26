8e910aa7aa0aa6fd7615bc44da481bc8259c6f0b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-27 01:26:14 +0530

    https://codeforces.com/contest/1255/problem/E1 accepted

--- a.cpp
+++ a.cpp
@@ -9,16 +9,46 @@ using namespace std;
 
 typedef long long ll;
 
-const ll mod=998244353;
-
-inline ll add(ll a,ll b)
+ll get(deque<ll> &a)
 {
-	return (a%mod+b%mod)%mod;
+	ll total_cnt=a.size(),cur_cnt=0,total_sum=0,cur_sum=0,lb=a[0],ub=a.back(),ans=1e18;
+	for(auto &v:a)
+	{
+		total_sum+=v;
+	}
+	for(ll i=lb;i<=ub;++i)
+	{
+		if(i==a[0])
+		{
+			cur_cnt++;
+			cur_sum+=i;
+			a.pop_front();
+		}
+		ll left_cnt=cur_cnt,left_sum=cur_sum,right_cnt=total_cnt-left_cnt,right_sum=total_sum-cur_sum;
+		ll left=left_cnt*i-left_sum,right=right_sum-i*right_cnt;
+		ans=min(ans,left+right);
+	}
+	return ans;
 }
 
-inline ll sub(ll a,ll b)
+ll get(deque<ll> a,ll d)
 {
-	return (a%mod-b%mod+mod)%mod;
+	if(d==1)
+	{
+		return 1e18;
+	}
+	ll ans=0;
+	while((ll)a.size())
+	{
+		deque<ll> tmp;
+		for(ll i=0;i<d;++i)
+		{
+			tmp.push_back(a[0]);
+			a.pop_front();
+		}
+		ans+=get(tmp);
+	}
+	return ans;
 }
 
 int main()
@@ -26,80 +56,33 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll tc;
-	cin>>tc;
-	while(tc--)
+	ll n,sum=0;
+	deque<ll> a;
+	cin>>n;
+	for(ll i=0;i<n;++i)
 	{
-		ll n,m,d_same,d_up=-1,ans=0;
-		cin>>n>>m>>d_same;
-		d_up+=d_same;
-		vector<string> a(n);
-		vector<vector<ll>> dp(n,vector<ll>(m,0));
-		for(auto &v:a)
+		ll x;
+		cin>>x;
+		sum+=x;
+		if(x==1)
 		{
-			cin>>v;
+			a.push_back(i);
 		}
-		for(ll i=0;i<n;++i)
+	}
+	if(sum==1)
+	{
+		cout<<-1<<'\n';
+	}
+	else
+	{
+		ll ans=1e18;
+		for(ll i=1;i*i<=sum;++i)
 		{
-			if(i==0)
+			if(sum%i==0)
 			{
-				ll s=0;
-				vector<ll> pre(m,0);
-				for(ll j=0;j<m;++j)
-				{
-					s+=(a[i][j]=='X');
-					pre[j]=s;
-				}
-				for(ll j=0;j<m;++j)
-				{
-					if(a[i][j]!='X')
-					{
-						continue;
-					}
-					ll lb=max(j-d_same,0LL),ub=min(m-1,j+d_same);
-					dp[i][j]=sub(pre[ub],((lb==0)?0LL:pre[lb-1]));
-				}
+				ans=min(ans,get(a,i));
+				ans=min(ans,get(a,sum/i));
 			}
-			else
-			{
-				ll s=0;
-				vector<ll> pre_pvs(m,0),pre_cur(m,0);
-				for(ll j=0;j<m;++j)
-				{
-					s=add(s,dp[i-1][j]);
-					pre_pvs[j]=s;
-				}
-				for(ll j=0;j<m;++j)
-				{
-					if(a[i][j]!='X')
-					{
-						continue;
-					}
-					ll lb=max(j-d_up,0LL),ub=min(m-1,j+d_up);
-					pre_cur[j]=sub(pre_pvs[ub],((lb==0)?0LL:pre_pvs[lb-1]));
-				}
-				for(ll j=1;j<m;++j)
-				{
-					pre_cur[j]=add(pre_cur[j],pre_cur[j-1]);
-				}
-				for(ll j=0;j<m;++j)
-				{
-					if(a[i][j]!='X')
-					{
-						continue;
-					}
-					ll lb=max(j-d_same,0LL),ub=min(m-1,j+d_same);
-					dp[i][j]=sub(pre_cur[ub],((lb==0)?0LL:pre_cur[lb-1]));
-				}
-			}
-		}
-		for(ll j=0;j<m;++j)
-		{
-			if(a[n-1][j]!='X')
-			{
-				continue;
-			}
-			ans=add(ans,dp[n-1][j]);
 		}
 		cout<<ans<<'\n';
 	}
