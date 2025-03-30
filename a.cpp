e9e2c7b7c6f1399272f18693df7528af1b85becc
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-03-30 17:44:23 +0530

    https://codeforces.com/contest/688/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,92 +7,57 @@ using namespace std;
 	#define debug(x)
 #endif
 
-int dp[200001][4];
-
-int solve(int i,int pvs,vector<int> &a)		// 0 -> pos    1 -> neg    2 -> no op
-{
-	if(i>=(int)a.size())
-	{
-		return 0;
-	}
-	if(dp[i][pvs]!=-1)
-	{
-		return dp[i][pvs];
-	}
-	if(a[i-1]>a[i])
-	{
-		if(pvs==1)
-		{
-			return dp[i][pvs]=min({solve(i+1,1,a),solve(i+1,0,a)+1,solve(i+1,2,a)});
-		}
-		else if(pvs==0)
-		{
-			return dp[i][pvs]=solve(i+1,0,a)+1;
-		}
-		else
-		{
-			return dp[i][pvs]=solve(i+1,0,a)+1;
-		}
-	}
-	else if(a[i-1]<a[i])
-	{
-		if(pvs==0)
-		{
-			return dp[i][pvs]=solve(i+1,0,a);
-		}
-		else if(pvs==1)
-		{
-			return dp[i][pvs]=min({solve(i+1,0,a),solve(i+1,1,a)+1,solve(i+1,2,a)});
-		}
-		else
-		{
-			return dp[i][pvs]=min(solve(i+1,2,a),solve(i+1,0,a)+1);
-		}
-	}
-	else
-	{
-		if(pvs==0)
-		{
-			return dp[i][pvs]=solve(i+1,0,a)+1;
-		}
-		else if(pvs==1)
-		{
-			return dp[i][pvs]=min({solve(i+1,0,a),solve(i+1,1,a)+1,solve(i+1,2,a)});
-		}
-		else
-		{
-			return dp[i][pvs]=solve(i+1,0,a)+1;
-		}
-	}
-}
-
-int countOperations(vector<int> &a)
-{
-	int n=a.size();
-	for(int i=0;i<=n;++i)
-	{
-		dp[i][0]=dp[i][1]=dp[i][2]=dp[i][3]=-1;
-	}
-	return min({solve(1,1,a)+1,solve(1,0,a)+1,solve(1,2,a)});
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,k;
+	cin>>n>>k;
+	vector<int> a(n),ans;
+	vector<vector<bool>> dp(k+1,vector<bool>(k+1,0));
+	for(auto &v:a)
 	{
-		int n;
-		cin>>n;
-		vector<int> a(n);
-		for(auto &v:a)
-		{
-			cin>>v;
-		}
-		cout<<countOperations(a)<<'\n';
+		cin>>v;
 	}
+	dp[0][0]=1;
+	for(int i=0;i<n;++i)
+	{
+		vector<vector<bool>> ndp=dp;
+		for(int x=0;x<=k;++x)
+		{
+			for(int y=0;y<=k;++y)
+			{
+				if(x>=a[i]&&dp[x-a[i]][y])
+				{
+					ndp[x][y]=1;
+				}
+				if(y>=a[i]&&dp[x][y-a[i]])
+				{
+					ndp[x][y]=1;
+				}
+			}
+		}
+		dp=ndp;
+	}
+	for(int i=0;i<=k/2;++i)
+	{
+		int x=i,y=k-i;
+		if(dp[x][y])
+		{
+			ans.push_back(x);
+			if(x!=y)
+			{
+				ans.push_back(y);
+			}
+		}
+	}
+	sort(ans.begin(),ans.end());
+	cout<<(int)ans.size()<<'\n';
+	for(auto &v:ans)
+	{
+		cout<<v<<' ';
+	}
+	cout<<'\n';
 	return 0;
 }
