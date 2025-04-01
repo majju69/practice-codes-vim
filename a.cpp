dd8f0ff788f5febc3ea26fd2a7c3843cce42970b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-04-01 15:33:29 +0530

    https://codeforces.com/contest/548/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,45 +7,112 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-inline ll bit(ll a,ll i)
+class SegmentTree
 {
-	return a>>i&1;
-}
 
-ll gcd(ll a,ll b)
-{
-	return ((b==0)?a:gcd(b,a%b));
-}
+private:
+
+	vector<int> seg;
+
+public:
+
+	SegmentTree(int n)
+	{
+		seg.resize(4*n+1);
+	}
+
+	void build(int ind,int lo,int hi,vector<int> &a)
+	{
+		if(lo==hi)
+		{
+			seg[ind]=a[lo];
+			return;
+		}
+		int mid=lo+(hi-lo)/2;
+		build(2*ind+1,lo,mid,a);
+		build(2*ind+2,mid+1,hi,a);
+		seg[ind]=min(seg[2*ind+1],seg[2*ind+2]);
+	}
+
+	int firstBelow(int ind,int lo,int hi,int l,int r,int x)
+	{
+		if(l>hi||lo>r||seg[ind]>=x)
+		{
+			return -1;
+		}
+		if(lo==hi)
+		{
+			return lo;
+		}
+		int mid=lo+(hi-lo)/2;
+		int ans=firstBelow(2*ind+1,lo,mid,l,r,x);
+		if(ans==-1)
+		{
+			ans=firstBelow(2*ind+2,mid+1,hi,l,r,x);
+		}
+		return ans;
+	}
+
+	int lastBelow(int ind,int lo,int hi,int l,int r,int x)
+	{
+		if(l>hi||lo>r||seg[ind]>=x)
+		{
+			return -1;
+		}
+		if(lo==hi)
+		{
+			return lo;
+		}
+		int mid=lo+(hi-lo)/2;
+		int ans=lastBelow(2*ind+2,mid+1,hi,l,r,x);
+		if(ans==-1)
+		{
+			ans=lastBelow(2*ind+1,lo,mid,l,r,x);
+		}
+		return ans;
+	}
+
+
+};
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n;
+	int n;
 	cin>>n;
-	vector<ll> a(n),g((1LL<<n),0),dp((1LL<<n),0);
+	vector<int> a(n),ans(n,-1);
 	for(auto &v:a)
 	{
 		cin>>v;
 	}
-	for(ll mask=1;mask<(1LL<<n);++mask)
+	SegmentTree st(n);
+	st.build(0,0,n-1,a);
+	for(int i=0;i<n;++i)
 	{
-		ll lsb=__builtin_ctzll(mask);
-		g[mask]=gcd(g[mask^(1LL<<lsb)],a[lsb]);
-	}
-	for(ll mask=1;mask<(1LL<<n);++mask)
-	{
-		for(ll i=0;i<n;++i)
+		int left=st.lastBelow(0,0,n-1,0,i,a[i]),right=st.firstBelow(0,0,n-1,i,n-1,a[i]);
+		if(right==-1)
 		{
-			if(bit(mask,i))
-			{
-				dp[mask]=max(dp[mask],g[mask]+dp[mask^(1LL<<i)]);
-			}
+			right=n;
+		}
+		ans[right-left-2]=max(ans[right-left-2],a[i]);
+	}
+	for(int i=n-1;i>=0;--i)
+	{
+		if(ans[i]==-1)
+		{
+			ans[i]=ans[i+1];
 		}
 	}
-	cout<<dp[(1LL<<n)-1]<<'\n';
+	for(int i=n-2;i>=0;--i)
+	{
+		ans[i]=max(ans[i],ans[i+1]);
+	}
+	for(auto &v:ans)
+	{
+		cout<<v<<' ';
+	}
+	cout<<'\n';
 	return 0;
 }
