62a1634df6b918cf502ad9458778035a576aaea7
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-04-06 03:34:01 +0530

    https://codeforces.com/contest/2020/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,73 +7,143 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef unsigned long long ll;
-
-inline ll bit(ll a,ll i)
+class DisjointSet
 {
-	return a>>i&1;
-}
+
+private:
+	
+	vector<int> ultimateParent,rank,size;
+
+public:
+	
+	DisjointSet(int n)
+	{
+		ultimateParent.resize(n+1);
+		rank.resize(n+1,0);
+		size.resize(n+1,1);
+		for(int i=0;i<=n;++i)
+		{
+			ultimateParent[i]=i;
+		}
+	}
+
+	int findUltimateParent(int node)
+	{
+		if(ultimateParent[node]==node)
+		{
+			return node;
+		}
+		return ultimateParent[node]=findUltimateParent(ultimateParent[node]);
+	}
+
+	int getSize(int node)
+	{
+		return size[node];
+	}
+
+	int getRank(int node)
+	{
+		return rank[node];
+	}
+
+	void unionByRank(int u,int v)
+	{
+		int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+		if(ultimateParentOfU==ultimateParentOfV)
+		{
+			return;
+		}
+		if(rank[ultimateParentOfU]<rank[ultimateParentOfV])
+		{
+			ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+		}
+		else if(rank[ultimateParentOfU]>rank[ultimateParentOfV])
+		{
+			ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+		}
+		else
+		{
+			ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+			rank[ultimateParentOfU]++;
+		}
+	}
+
+	void unionBySize(int u,int v)
+	{
+		int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+		if(ultimateParentOfU==ultimateParentOfV)
+		{
+			return;
+		}
+		if(size[ultimateParentOfU]<size[ultimateParentOfV])
+		{
+			ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+			size[ultimateParentOfV]+=size[ultimateParentOfU];
+		}
+		else
+		{
+			ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+			size[ultimateParentOfU]+=size[ultimateParentOfV];
+		}
+	}
+
+};
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll sum,_xor;
-	cin>>sum>>_xor;
-	if(sum<_xor||(sum-_xor)%2!=0)
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cout<<-1<<'\n';
-	}
-	else
-	{
-		ll _and=(sum-_xor)/2,x=0,y=0;
-		for(ll i=0;i<64;++i)
+		int n,m,ans=0;
+		vector<vector<pair<int,int>>> a(10);
+		cin>>n>>m;
+		DisjointSet ds(n);
+		for(int i=0;i<m;++i)
 		{
-			if(bit(_and,i))
+			int _a,d,k;
+			cin>>_a>>d>>k;
+			_a--;
+			d--;
+			if(k>0)
 			{
-				if(bit(_xor,i))
-				{
-					x=-1;
-					y=-1;
-					break;
-				}
-				else
-				{
-					x+=(1LL<<i);
-					y+=(1LL<<i);
-				}
-			}
-			else
-			{
-				if(bit(_xor,i))
-				{
-					y+=(1LL<<i);
-				}
+				a[d].push_back({_a,k});
 			}
 		}
-		if(x==-1)
+		for(int d=0;d<10;++d)
 		{
-			cout<<-1<<'\n';
+			if((int)a[d].size()==0)
+			{
+				continue;
+			}
+			vector<pair<int,int>> ranges(n);
+			DisjointSet ds_cur(n);
+			for(int i=0;i<n;++i)
+			{
+				ranges[i].first=ranges[i].second=i;
+			}
+			for(auto &v:a[d])
+			{
+				int lb=v.first,ub=v.first+(d+1)*v.second;
+				int cur=lb;
+				while(cur+d+1<=ub)
+				{
+					ds.unionByRank(cur,cur+d+1);
+					ds_cur.unionByRank(cur,cur+d+1);
+					ranges[ds_cur.findUltimateParent(cur)].second=max(ranges[ds_cur.findUltimateParent(cur)].second,cur+d+1);
+					ranges[ds_cur.findUltimateParent(cur)].first=min(ranges[ds_cur.findUltimateParent(cur)].first,cur);
+					cur=ranges[ds_cur.findUltimateParent(cur)].second;
+				}
+			}
 		}
-		else
+		for(int i=0;i<n;++i)
 		{
-			cout<<x<<' '<<y<<'\n';
+			ans+=(ds.findUltimateParent(i)==i);
 		}
+		cout<<ans<<'\n';
 	}
 	return 0;
 }
-
-/*
-
-a=x+y
-b=x^y
-
-x+y=x^y+2(x&y)
-
-a=b+2(x&y)
-
-(a-b)/2=x&y
-b=x^y
-
-*/
