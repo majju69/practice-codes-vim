5591e43c09098175b514435c4fe3b454336a208d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-04-08 04:16:17 +0530

    https://codeforces.com/contest/1932/problem/F accepted

--- a.cpp
+++ a.cpp
@@ -7,11 +7,96 @@ using namespace std;
 	#define debug(x)
 #endif
 
-const int mod=998244353;
-
-inline int add(int a,int b)
+class SegmentTree
 {
-	return (a%mod+b%mod)%mod;
+
+private:
+	
+	vector<int> seg;
+
+public:
+
+	SegmentTree(int n)
+	{
+		seg.resize(4*n+1);
+	}
+
+	void build(int ind,int lo,int hi,vector<pair<int,int>> &a)
+	{
+		if(lo==hi)
+		{
+			seg[ind]=a[lo].second;
+			return;
+		}
+		int mid=lo+(hi-lo)/2;
+		build(2*ind+1,lo,mid,a);
+		build(2*ind+2,mid+1,hi,a);
+		seg[ind]=max(seg[2*ind+1],seg[2*ind+2]);
+	}
+
+	int query(int ind,int lo,int hi,int l,int r)
+	{
+		if(l>hi||lo>r)
+		{
+			return -1e9;
+		}
+		if(l<=lo&&hi<=r)
+		{
+			return seg[ind];
+		}
+		int mid=lo+(hi-lo)/2;
+		return max(query(2*ind+1,lo,mid,l,r),query(2*ind+2,mid+1,hi,l,r));
+	}
+
+};
+
+int dp[1000001];
+
+int solve(int i,vector<int> &cost,vector<int> &value)
+{
+	if(i>=(int)cost.size())
+	{
+		return 0;
+	}
+	if(dp[i]!=-1)
+	{
+		return dp[i];
+	}
+	int take=solve(i+1,cost,value),skip=solve(cost[i]+1,cost,value)+value[i];
+	return dp[i]=max(take,skip);
+}
+
+int maxCats(int n,vector<pair<int,int>> &intervals)
+{
+	int m=intervals.size();
+	vector<int> left(m),right(m),cost(n),value(n,0);
+	sort(intervals.begin(),intervals.end());
+	iota(cost.begin(),cost.end(),0);
+	for(int i=0;i<m;++i)
+	{
+		left[i]=intervals[i].first;
+		right[i]=intervals[i].second;
+	}
+	sort(left.begin(),left.end());
+	sort(right.begin(),right.end());
+	SegmentTree st(m);
+	st.build(0,0,m-1,intervals);
+	for(int i=0;i<n;++i)
+	{
+		int v=m-((upper_bound(left.begin(),left.end(),n)-lower_bound(left.begin(),left.end(),i+1))+(upper_bound(right.begin(),right.end(),i-1)-lower_bound(right.begin(),right.end(),0))),c=i;
+		int idx=upper_bound(intervals.begin(),intervals.end(),make_pair(i,(int)1e9))-intervals.begin()-1;
+		if(idx>=0)
+		{
+			c=st.query(0,0,m-1,0,idx);
+		}
+		value[i]=v;
+		cost[i]=max(cost[i],c);
+	}
+	for(int i=0;i<=n;++i)
+	{
+		dp[i]=-1;
+	}
+	return solve(0,cost,value);
 }
 
 int main()
@@ -19,38 +104,20 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,cnt3[4]={0},cnt2[4]={0},cnt1[2]={0},ans=0;
-	cin>>n;
-	for(int i=0;i<n;++i)
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		int x;
-		cin>>x;
-		x&=1;
-		if(x==0)
+		int n,m;
+		cin>>n>>m;
+		vector<pair<int,int>> intervals(m);
+		for(auto &v:intervals)
 		{
-			cnt3[2]=add(cnt3[2],cnt3[3]);
-			cnt3[2]=add(cnt3[2],cnt2[3]);
-			cnt3[0]=add(cnt3[0],cnt3[0]);
-			cnt3[0]=add(cnt3[0],cnt2[0]);
-			cnt2[0]=add(cnt2[0],cnt1[0]);
-			cnt2[2]=add(cnt2[2],cnt1[1]);
-			cnt1[0]++;
+			cin>>v.first>>v.second;
+			v.first--;
+			v.second--;
 		}
-		else
-		{
-			cnt3[3]=add(cnt3[3],cnt3[1]);
-			cnt3[3]=add(cnt3[3],cnt2[1]);
-			cnt3[1]=add(cnt3[1],cnt3[2]);
-			cnt3[1]=add(cnt3[1],cnt2[2]);
-			cnt2[1]=add(cnt2[1],cnt1[0]);
-			cnt2[3]=add(cnt2[3],cnt1[1]);
-			cnt1[1]++;
-		}
+		cout<<maxCats(n,intervals)<<'\n';
 	}
-	for(int i=0;i<4;++i)
-	{
-		ans=add(ans,cnt3[i]);
-	}
-	cout<<ans<<'\n';
 	return 0;
 }
