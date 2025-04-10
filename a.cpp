7196ebdff460e6a88b23c7aecdbb39e265b426d6
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-04-10 21:33:31 +0530

    https://codeforces.com/contest/1918/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,96 +7,94 @@ using namespace std;
 	#define debug(x)
 #endif
 
+typedef long long ll;
+
 class SegmentTree
 {
 
 private:
-	
-	vector<int> seg;
+
+	vector<ll> seg;
 
 public:
 
-	SegmentTree(int n)
+	SegmentTree(ll n)
 	{
 		seg.resize(4*n+1);
 	}
 
-	void build(int ind,int lo,int hi,vector<pair<int,int>> &a)
+	void update(ll ind,ll lo,ll hi,ll i,ll val)
 	{
 		if(lo==hi)
 		{
-			seg[ind]=a[lo].second;
+			seg[ind]=val;
 			return;
 		}
-		int mid=lo+(hi-lo)/2;
-		build(2*ind+1,lo,mid,a);
-		build(2*ind+2,mid+1,hi,a);
-		seg[ind]=max(seg[2*ind+1],seg[2*ind+2]);
+		ll mid=lo+(hi-lo)/2;
+		if(i<=mid)
+		{
+			update(2*ind+1,lo,mid,i,val);
+		}
+		else
+		{
+			update(2*ind+2,mid+1,hi,i,val);
+		}
+		seg[ind]=min(seg[2*ind+1],seg[2*ind+2]);
 	}
 
-	int query(int ind,int lo,int hi,int l,int r)
+	ll query(ll ind,ll lo,ll hi,ll l,ll r)
 	{
 		if(l>hi||lo>r)
 		{
-			return -1e9;
+			return 1e18;
 		}
 		if(l<=lo&&hi<=r)
 		{
 			return seg[ind];
 		}
-		int mid=lo+(hi-lo)/2;
-		return max(query(2*ind+1,lo,mid,l,r),query(2*ind+2,mid+1,hi,l,r));
+		ll mid=lo+(hi-lo)/2;
+		return min(query(2*ind+1,lo,mid,l,r),query(2*ind+2,mid+1,hi,l,r));
 	}
 
 };
 
-int dp[1000001];
-
-int solve(int i,vector<int> &cost,vector<int> &value)
+bool check(ll mid,vector<ll> &a)
 {
-	if(i>=(int)cost.size())
+	ll n=a.size(),sum=0,ans=1e18;
+	vector<ll> pre(n),jump(n,n),dp(n);
+	pre[0]=a[0];
+	for(ll i=1;i<n;++i)
 	{
-		return 0;
+		pre[i]=pre[i-1]+a[i];
 	}
-	if(dp[i]!=-1)
+	for(ll i=0;i<n-1;++i)
 	{
-		return dp[i];
+		ll idx=upper_bound(pre.begin()+i+1,pre.end(),mid+pre[i])-pre.begin();
+		jump[i]=idx;
 	}
-	int take=solve(i+1,cost,value),skip=solve(cost[i]+1,cost,value)+value[i];
-	return dp[i]=max(take,skip);
-}
-
-int maxCats(int n,vector<pair<int,int>> &intervals)
-{
-	int m=intervals.size();
-	vector<int> left(m),right(m),cost(n),value(n,0);
-	sort(intervals.begin(),intervals.end());
-	iota(cost.begin(),cost.end(),0);
-	for(int i=0;i<m;++i)
+	SegmentTree st(n);
+	for(ll i=n-1;i>=0;--i)
 	{
-		left[i]=intervals[i].first;
-		right[i]=intervals[i].second;
-	}
-	sort(left.begin(),left.end());
-	sort(right.begin(),right.end());
-	SegmentTree st(m);
-	st.build(0,0,m-1,intervals);
-	for(int i=0;i<n;++i)
-	{
-		int v=m-((upper_bound(left.begin(),left.end(),n)-lower_bound(left.begin(),left.end(),i+1))+(upper_bound(right.begin(),right.end(),i-1)-lower_bound(right.begin(),right.end(),0))),c=i;
-		int idx=upper_bound(intervals.begin(),intervals.end(),make_pair(i,(int)1e9))-intervals.begin()-1;
-		if(idx>=0)
+		if(jump[i]==n)
 		{
-			c=st.query(0,0,m-1,0,idx);
+			dp[i]=a[i];
 		}
-		value[i]=v;
-		cost[i]=max(cost[i],c);
+		else
+		{
+			dp[i]=a[i]+st.query(0,0,n-1,i+1,jump[i]);
+		}
+		st.update(0,0,n-1,i,dp[i]);
 	}
-	for(int i=0;i<=n;++i)
+	for(ll i=0;i<n;++i)
 	{
-		dp[i]=-1;
+		ans=min(ans,dp[i]);
+		sum+=a[i];
+		if(sum>mid)
+		{
+			break;
+		}
 	}
-	return solve(0,cost,value);
+	return (ans<=mid);
 }
 
 int main()
@@ -104,20 +102,33 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
+	ll tc;
 	cin>>tc;
 	while(tc--)
 	{
-		int n,m;
-		cin>>n>>m;
-		vector<pair<int,int>> intervals(m);
-		for(auto &v:intervals)
+		ll n,lo=-1e18,hi=0,ans=-1;
+		cin>>n;
+		vector<ll> a(n);
+		for(auto &v:a)
 		{
-			cin>>v.first>>v.second;
-			v.first--;
-			v.second--;
+			cin>>v;
+			hi+=v;
+			lo=max(lo,v);
 		}
-		cout<<maxCats(n,intervals)<<'\n';
+		while(lo<=hi)
+		{
+			ll mid=lo+(hi-lo)/2;
+			if(check(mid,a))
+			{
+				ans=mid;
+				hi=mid-1;
+			}
+			else
+			{
+				lo=mid+1;
+			}
+		}
+		cout<<ans<<'\n';
 	}
 	return 0;
 }
