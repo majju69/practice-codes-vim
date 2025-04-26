4a7103523a16f8c2f53f916a312020c53a7f08ed
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-04-26 22:47:52 +0530

    https://leetcode.com/problems/maximum-profit-from-valid-topological-order-in-dag/description/ accepted

--- a.cpp
+++ a.cpp
@@ -7,10 +7,48 @@ using namespace std;
 	#define debug(x)
 #endif
 
-bool cmp(string &s1,string &s2)
+inline int bit(int a,int i)
 {
-	string f=s1+s2,s=s2+s1;
-	return f<s;
+	return a>>i&1;
+}
+
+int maxProfit(int n,vector<vector<int>> &edges,vector<int> &score)
+{
+	vector<int> badMask(n,0),dp((1<<n),0);
+	for(auto &edge:edges)
+	{
+		badMask[edge[0]]|=(1<<edge[1]);
+	}
+	for(int mask=1;mask<(1<<n);++mask)
+	{
+		int cnt=__builtin_popcount(mask);
+		if(mask==1)
+		{
+			for(int i=0;i<n;++i)
+			{
+				if(bit(mask,i))
+				{
+					dp[mask]=score[i];
+					break;
+				}
+			}
+		}
+		else
+		{
+			for(int i=0;i<n;++i)
+			{
+				if(bit(mask,i))
+				{
+					int curMask=(mask^(1<<i));
+					if((curMask&badMask[i])==0)
+					{
+						dp[mask]=max(dp[mask],dp[curMask]+cnt*score[i]);
+					}
+				}
+			}
+		}
+	}
+	return dp.back();
 }
 
 int main()
@@ -18,18 +56,18 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n;
-	cin>>n;
-	vector<string> a(n);
-	for(auto &v:a)
+	int n,m;
+	cin>>n>>m;
+	vector<vector<int>> edges(m,vector<int>(2,0));
+	vector<int> score(n);
+	for(auto &edge:edges)
+	{
+		cin>>edge[0]>>edge[1];
+	}
+	for(auto &v:score)
 	{
 		cin>>v;
 	}
-	sort(a.begin(),a.end(),cmp);
-	for(auto &v:a)
-	{
-		cout<<v;
-	}
-	cout<<'\n';
+	cout<<maxProfit(n,edges,score)<<'\n';
 	return 0;
 }
