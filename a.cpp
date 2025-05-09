b083effb67ad43aab25bdd22e17a8dd9d8ab8f60
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-05-09 16:11:40 +0530

    https://codeforces.com/contest/1433/problem/F accepted

--- a.cpp
+++ a.cpp
@@ -7,87 +7,76 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-/*ll dp[200001][3];
-
-ll solve(ll i,bool toAdd,vector<ll> &a)
+vector<int> get(vector<int> &a,int k)
 {
-	if(i>=(ll)a.size())
+	int n=a.size();
+	vector<int> ans(k,-1e9);
+	vector<vector<int>> dp(n/2+1,vector<int>(k,-1e9));
+	dp[0][0]=0;
+	dp[1][a[0]%k]=a[0];
+	for(int i1=0;i1<n-1;++i1)
 	{
-		if(!toAdd)
+		vector<vector<int>> ndp=dp;
+		for(int i2=1;i2<=n/2;++i2)
 		{
-			return (ll)1e18;
+			for(int i3=0;i3<k;++i3)
+			{
+				ndp[i2][i3]=max({ndp[i2][i3],dp[i2][i3],dp[i2-1][((i3-a[i1+1])%k+k)%k]+a[i1+1]});
+			}
 		}
-		return 0;
+		dp=ndp;
 	}
-	if(dp[i][toAdd]!=(ll)-1e18)
+	for(int i=0;i<k;++i)
 	{
-		return dp[i][toAdd];
+		for(int j=0;j<=n/2;++j)
+		{
+			ans[i]=max(ans[i],dp[j][i]);
+		}
 	}
-	if(toAdd)
-	{
-		return dp[i][toAdd]=solve(i+2,0,a)+a[i];
-	}
-	return dp[i][toAdd]=min(solve(i+1,0,a),solve(i+1,1,a)-a[i]);
+	return ans;
 }
-
-ll minDiversity(vector<ll> &a)
-{
-	ll n=a.size();
-	sort(a.rbegin(),a.rend());
-	for(ll i=0;i<=n;++i)
-	{
-		dp[i][0]=dp[i][1]=dp[i][2]=(ll)-1e18;
-	}
-	return solve(0,1,a);
-}*/
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,pvs_idx=0,cur_idx=3,cur=1;
-	cin>>n;
-	vector<pair<ll,ll>> a(n);
-	vector<vector<ll>> dp(n+2,vector<ll>(2,0));
-	vector<ll> ans(n,1);
-	for(ll i=0;i<n;++i)
+	int n,m,k;
+	cin>>n>>m>>k;
+	vector<vector<int>> a(n,vector<int>(m,0));
+	for(auto &vec:a)
 	{
-		cin>>a[i].first;;
-		a[i].second=i;
-	}
-	sort(a.rbegin(),a.rend());
-	dp[n][0]=1e18;
-	dp[n+1][0]=1e18;
-	for(ll i=n-1;i>=0;--i)
-	{
-		dp[i][1]=dp[i+2][0]+a[i].first;
-		dp[i][0]=min(dp[i+1][0],dp[i+1][1]-a[i].first);
-	}
-	while(cur_idx<n)
-	{
-		if(a[cur_idx-1].first-a[pvs_idx].first+dp[pvs_idx][1]==dp[cur_idx][1])
+		for(auto &v:vec)
 		{
-			cur++;
-			ans[a[cur_idx].second]=cur;
-			ans[a[cur_idx+1].second]=cur;
-			ans[a[cur_idx+2].second]=cur;
-			pvs_idx=cur_idx;
-			cur_idx+=3;
-		}
-		else
-		{
-			ans[a[cur_idx].second]=cur;
-			cur_idx++;
+			cin>>v;
 		}
 	}
-	cout<<dp[0][1]<<' '<<cur<<'\n';
-	for(auto &v:ans)
+	if(m==1)
 	{
-		cout<<v<<' ';
+		cout<<0<<'\n';
 	}
-	cout<<'\n';
+	else
+	{
+		vector<vector<int>> mxSum;
+		vector<int> dp(k,0);
+		for(int i=0;i<n;++i)
+		{
+			mxSum.push_back(get(a[i],k));
+		}
+		dp=mxSum[0];
+		for(int i1=0;i1<n-1;++i1)
+		{
+			vector<int> ndp=dp;
+			for(int i2=0;i2<k;++i2)
+			{
+				for(int i3=0;i3<k;++i3)
+				{
+					ndp[i2]=max({ndp[i2],dp[i2],dp[((i2-i3)%k+k)%k]+mxSum[i1+1][i3]});
+				}
+			}
+			dp=ndp;
+		}
+		cout<<dp[0]<<'\n';
+	}
 	return 0;
 }
