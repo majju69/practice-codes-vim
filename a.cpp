d91adfb58a159a0bb109b0f7cc93d01721654d06
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-05-17 02:14:22 +0530

    https://cses.fi/problemset/task/1748/ accepted

--- a.cpp
+++ a.cpp
@@ -7,62 +7,99 @@ using namespace std;
 	#define debug(x)
 #endif
 
-inline long long get(int n)
+const int mod=1e9+7;
+
+inline int add(int a,int b)
 {
-	return 1ll*n*(n-1)/2;
+	return ((a%mod)+(b%mod))%mod;
 }
+
+class SegmentTree
+{
+
+private:
+
+	vector<int> seg;
+
+public:
+
+	SegmentTree(int n)
+	{
+		seg.resize(4*n+1);
+	}
+
+	void update(int ind,int lo,int hi,int i,int val)
+	{
+		if(lo==hi)
+		{
+			seg[ind]=add(seg[ind],val);
+			return;
+		}
+		int mid=lo+(hi-lo)/2;
+		if(i<=mid)
+		{
+			update(2*ind+1,lo,mid,i,val);
+		}
+		else
+		{
+			update(2*ind+2,mid+1,hi,i,val);
+		}
+		seg[ind]=add(seg[2*ind+1],seg[2*ind+2]);
+	}
+
+	int query(int ind,int lo,int hi,int l,int r)
+	{
+		if(l>hi||lo>r)
+		{
+			return 0;
+		}
+		if(l<=lo&&hi<=r)
+		{
+			return seg[ind];
+		}
+		int mid=lo+(hi-lo)/2;
+		return add(query(2*ind+1,lo,mid,l,r),query(2*ind+2,mid+1,hi,l,r));
+	}
+
+};
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	int n,ind=0,ans=0;
+	map<int,int> mp;
+	cin>>n;
+	vector<int> a(n),dp(n);
+	SegmentTree st(n);
+	for(auto &v:a)
 	{
-		int n;
-		long long m,ans=0;
-		cin>>n>>m;
-		vector<long long> cnt(n+1,0),dp(n+1,0);
-		for(int i=1;i<=n;++i)
-		{
-			cnt[i]=get(n/i);
-		}
-		for(int i=n;i>=1;--i)
-		{
-			if(cnt[i]==0)
-			{
-				continue;
-			}
-			dp[i]=cnt[i];
-			for(int j=2*i;j<=n;j+=i)
-			{
-				dp[i]-=dp[j];
-			}
-		}
-		for(int i=n;i>=2;--i)
-		{
-			int cost=i,batchSize=i-1;
-			long long reqBatches=m/batchSize,maxBatches=dp[i]/batchSize;
-			if(reqBatches<=maxBatches)
-			{
-				long long takenBatches=reqBatches;
-				ans+=takenBatches*cost;
-				m-=takenBatches*batchSize;
-			}
-			else
-			{
-				long long takenBatches=maxBatches;
-				ans+=takenBatches*cost;
-				m-=takenBatches*batchSize;
-			}
-		}
-		if(m>0)
-		{
-			ans=-1;
-		}
-		cout<<ans<<'\n';
+		cin>>v;
+		mp[v]=0;
 	}
+	for(auto &v:mp)
+	{
+		v.second=ind++;
+	}
+	for(auto &v:a)
+	{
+		v=mp[v];
+	}
+	for(int i=0;i<n;++i)
+	{
+		if(a[i]==0)
+		{
+			dp[i]=1;
+		}
+		else
+		{
+			int cur=st.query(0,0,ind-1,0,a[i]-1);
+			dp[i]=add(cur,1);
+		}
+		st.update(0,0,ind-1,a[i],dp[i]);
+		ans=add(ans,dp[i]);
+	}
+	cout<<ans<<'\n';
 	return 0;
-}
+}	
