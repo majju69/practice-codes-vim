3ef73607d0c8820b96a3c0c7df74a6358fd83c7f
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-06-06 06:12:44 +0530

    https://codeforces.com/contest/1593/problem/D2 accepted

--- a.cpp
+++ a.cpp
@@ -7,6 +7,15 @@ using namespace std;
 	#define debug(x)
 #endif
 
+inline int get(int x,int m)
+{
+	if(x<0)
+	{
+		return ((x%m)+m)%m;
+	}
+	return x%m;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -16,47 +25,79 @@ int main()
 	cin>>tc;
 	while(tc--)
 	{
-		int n,ans=1;
-		map<long long,vector<pair<int,int>>> mp;
+		int n;
+		bool ok=0;
+		map<int,int> mp;
 		cin>>n;
-		vector<int> a(n);
-		for(auto &v:a)
+		for(int i=0;i<n;++i)
 		{
-			cin>>v;
-		}
-		for(int i=0;i<n-1;++i)
-		{
-			for(int j=i+1;j<n;++j)
+			int x;
+			cin>>x;
+			mp[x]++;
+			if(mp[x]>=n/2)
 			{
-				int x=a[j]-a[i];
-				for(int d=1;d*d<=x;++d)
+				ok=1;
+			}
+		}
+		if(ok)
+		{
+			cout<<-1<<'\n';
+		}
+		else
+		{
+			int ans=1;
+			vector<int> a;
+			map<int,vector<pair<int,int>>> mp1;
+			for(auto &v:mp)
+			{
+				a.push_back(v.first);
+			}
+			for(int i=0;i<(int)a.size()-1;++i)
+			{
+				for(int j=i+1;j<(int)a.size();++j)
 				{
-					if(x%d==0)
+					int x=a[j]-a[i];
+					for(int d=1;d*d<=x;++d)
 					{
-						int d1=x/d,d2=d;
-						if((d1&1)==(d2&1))
+						if(x%d==0)
 						{
-							long long u=(d1+d2)>>1;
-							if(u*u-a[j]>=0)
+							mp1[x/d].push_back({i,j});
+							if(d*d!=x)
 							{
-								mp[u*u-a[j]].push_back({i,j});
+								mp1[d].push_back({i,j});
 							}
 						}
 					}
 				}
 			}
-		}
-		for(auto &v:mp)
-		{
-			set<int> st;
-			for(auto &p:v.second)
+			for(auto &v:mp1)
 			{
-				st.insert(p.first);
-				st.insert(p.second);
+				set<int> idx;
+				map<int,vector<int>> mod_class;
+				for(auto &p:v.second)
+				{
+					idx.insert(p.first);
+					idx.insert(p.second);
+				}
+				for(auto &i:idx)
+				{
+					mod_class[get(a[i],v.first)].push_back(a[i]);
+				}
+				for(auto &c:mod_class)
+				{
+					int cnt=0;
+					for(auto &v1:c.second)
+					{
+						cnt+=mp[v1];
+					}
+					if(cnt>=n/2)
+					{
+						ans=max(ans,v.first);
+					}
+				}
 			}
-			ans=max(ans,(int)st.size());
+			cout<<ans<<'\n';
 		}
-		cout<<ans<<'\n';
 	}
 	return 0;
 }
