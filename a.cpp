1954207a08e00ed32566294eeacb23ff3a16b0a9
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-06-09 08:06:04 +0530

    https://codeforces.com/contest/1608/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,57 +7,56 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
-
-ll solve(vector<ll> &a)
-{
-	ll n=a.size();
-	if(n==0)
-	{
-		return 0;
-	}
-	ll mn=*min_element(a.begin(),a.end());
-	if(n<=mn)
-	{
-		return n;
-	}
-	ll sum=mn;
-	vector<ll> tmp;
-	for(auto &v:a)
-	{
-		if(v>mn)
-		{
-			tmp.push_back(v-mn);
-		}
-		else
-		{
-			sum+=solve(tmp);
-			if(sum>n)
-			{
-				break;
-			}
-			tmp.clear();
-		}
-	}
-	if(sum<n)
-	{
-		sum+=solve(tmp);
-	}
-	return min(n,sum);
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n;
-	cin>>n;
-	vector<ll> a(n);
-	for(auto &v:a)
+	int tc;
+	cin>>tc;
+	while(tc--)
 	{
-		cin>>v;
+		int n;
+		set<int> win_a,win_b;
+		cin>>n;
+		vector<int> a(n),b(n);
+		vector<pair<int,int>> _a(n),_b(n);
+		vector<bool> ans(n,0);
+		for(int i=0;i<n;++i)
+		{
+			cin>>a[i];
+			_a[i]={a[i],i};
+		}
+		for(int i=0;i<n;++i)
+		{
+			cin>>b[i];
+			_b[i]={b[i],i};
+		}
+		sort(_a.begin(),_a.end());
+		sort(_b.begin(),_b.end());
+		ans[_a[n-1].second]=1;
+		ans[_b[n-1].second]=1;
+		win_b.insert(b[_a[n-1].second]);
+		win_a.insert(a[_b[n-1].second]);
+		for(int i=n-2;i>=0;--i)
+		{
+			bool c1=(*win_a.begin()<_a[i].first),c2=(*win_b.begin()<_b[i].first);
+			if(c1)
+			{
+				ans[_a[i].second]=1;
+				win_b.insert(b[_a[i].second]);
+			}
+			if(c2)
+			{
+				ans[_b[i].second]=1;
+				win_a.insert(a[_b[i].second]);
+			}
+		}
+		for(auto v:ans)
+		{
+			cout<<v;
+		}
+		cout<<'\n';
 	}
-	cout<<solve(a)<<'\n';
 	return 0;
 }
