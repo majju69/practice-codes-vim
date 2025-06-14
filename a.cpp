44264b7553639ea41c55521f2b47965fac2e21c5
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-06-14 12:55:18 +0530

    https://codeforces.com/contest/988/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -9,76 +9,64 @@ using namespace std;
 
 typedef long long ll;
 
-ll dp[5002][5002];
-
-ll solve(int i,int j,int n,vector<int> &a,vector<int> &cold,vector<int> &hot)
-{
-	if(max(i,j)+1>n)
-	{
-		return 0;
-	}
-	if(dp[i][j]!=-1)
-	{
-		return dp[i][j];
-	}
-	int to_take=max(i,j)+1;
-	ll cpu1=1e18,cpu2=1e18;
-	if(i==0||a[i]!=a[to_take])
-	{
-		cpu1=solve(to_take,j,n,a,cold,hot)+cold[a[to_take]];
-	}
-	else
-	{
-		cpu1=solve(to_take,j,n,a,cold,hot)+hot[a[to_take]];
-	}
-	if(j==0||a[j]!=a[to_take])
-	{
-		cpu2=solve(i,to_take,n,a,cold,hot)+cold[a[to_take]];
-	}
-	else
-	{
-		cpu2=solve(i,to_take,n,a,cold,hot)+hot[a[to_take]];
-	}
-	return dp[i][j]=min(cpu1,cpu2);
-}
-
-ll minTime(int n,vector<int> &a,vector<int> &hot,vector<int> &cold)
-{
-	for(int i=0;i<=n+1;++i)
-	{
-		for(int j=0;j<=n+1;++j)
-		{
-			dp[i][j]=-1;
-		}
-	}
-	return solve(0,0,n,a,cold,hot);
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
+	ll n,ans=1;
+	vector<ll> a;
+	set<ll> st;
+	cin>>n;
+	while(n--)
 	{
-		int n,k;
-		cin>>n>>k;
-		vector<int> a(n+1),cold(k+1),hot(k+1);
-		for(int i=1;i<=n;++i)
+		ll x;
+		cin>>x;
+		st.insert(x);
+	}
+	for(auto &v:st)
+	{
+		if(ans==3)
 		{
-			cin>>a[i];
+			break;
 		}
-		for(int i=1;i<=k;++i)
+		for(ll cur=1;cur<=(1LL<<31);cur<<=1)
 		{
-			cin>>cold[i];
+			if(ans==3)
+			{
+				break;
+			}
+			if(st.count(v+cur))
+			{
+				if(st.count(v+2*cur))
+				{
+					ans=3;
+					a={v,v+cur,v+2*cur};
+					break;
+				}
+				else
+				{
+					if(ans==1)
+					{
+						ans=2;
+						a={v,v+cur};
+					}
+				}
+			}
 		}
-		for(int i=1;i<=k;++i)
+	}
+	if(ans==1)
+	{
+		cout<<1<<'\n'<<*st.begin()<<'\n';
+	}
+	else
+	{
+		cout<<ans<<'\n';
+		for(auto &v:a)
 		{
-			cin>>hot[i];
+			cout<<v<<' ';
 		}
-		cout<<minTime(n,a,hot,cold)<<'\n';
+		cout<<'\n';
 	}
 	return 0;
 }
