c89d24191938b6b5b7b8e4aeed8e96e1a5697b90
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-06-16 05:25:01 +0530

    https://codeforces.com/contest/1294/problem/F accepted

--- a.cpp
+++ a.cpp
@@ -7,66 +7,111 @@ using namespace std;
 	#define debug(x)
 #endif
 
-typedef long long ll;
+void bfs(vector<int> &src,vector<int> &dist,vector<int> adj[])
+{
+	queue<pair<int,int>> q;
+	for(auto &v:src)
+	{
+		dist[v]=0;
+		q.push({v,0});
+	}
+	while(q.size())
+	{
+		int node=q.front().first,dis=q.front().second;
+		q.pop();
+		for(auto &v:adj[node])
+		{
+			if(dist[v]>1+dis)
+			{
+				dist[v]=1+dis;
+				q.push({v,1+dis});
+			}
+		}
+	}
+}
+
+void dfs(int node,int dest,int p,vector<int> adj[],vector<int> &path)
+{
+	path.push_back(node);
+	if(path.back()==dest)
+	{
+		return;
+	}
+	for(auto &v:adj[node])
+	{
+		if(v!=p)
+		{
+			dfs(v,dest,node,adj,path);
+		}
+	}
+	if(path.back()!=dest)
+	{
+		path.pop_back();
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	ll n,ans=1;
-	vector<ll> a;
-	set<ll> st;
+	int n,node1=0,node2=0,node3=-1;
 	cin>>n;
-	while(n--)
+	vector<int> *adj=new vector<int>[n],src={0},dist1(n,1e9),dist2(n,1e9),dist3(n,1e9),path;
+	for(int i=1;i<n;++i)
 	{
-		ll x;
-		cin>>x;
-		st.insert(x);
+		int u,v;
+		cin>>u>>v;
+		u--;
+		v--;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
-	for(auto &v:st)
+	bfs(src,dist1,adj);
+	for(int i=0;i<n;++i)
 	{
-		if(ans==3)
+		if(dist1[i]>dist1[node1])
 		{
-			break;
+			node1=i;
 		}
-		for(ll cur=1;cur<=(1LL<<31);cur<<=1)
+	}
+	src={node1};
+	bfs(src,dist2,adj);
+	for(int i=0;i<n;++i)
+	{
+		if(dist2[i]>dist2[node2])
 		{
-			if(ans==3)
+			node2=i;
+		}
+	}
+	dfs(node1,node2,-1,adj,path);
+	src.clear();
+	for(auto &v:path)
+	{
+		if(v!=node1&&v!=node2)
+		{
+			src.push_back(v);
+		}
+	}
+	bfs(src,dist3,adj);
+	for(int i=0;i<n;++i)
+	{
+		if(i!=node1&&i!=node2)
+		{
+			if(node3==-1)
 			{
-				break;
+				node3=i;
 			}
-			if(st.count(v+cur))
+			else
 			{
-				if(st.count(v+2*cur))
+				if(dist3[node3]<dist3[i])
 				{
-					ans=3;
-					a={v,v+cur,v+2*cur};
-					break;
-				}
-				else
-				{
-					if(ans==1)
-					{
-						ans=2;
-						a={v,v+cur};
-					}
+					node3=i;
 				}
 			}
 		}
 	}
-	if(ans==1)
-	{
-		cout<<1<<'\n'<<*st.begin()<<'\n';
-	}
-	else
-	{
-		cout<<ans<<'\n';
-		for(auto &v:a)
-		{
-			cout<<v<<' ';
-		}
-		cout<<'\n';
-	}
+	cout<<dist2[node2]+dist3[node3]<<'\n';
+	cout<<node1+1<<' '<<node2+1<<' '<<node3+1<<'\n';
 	return 0;
 }
