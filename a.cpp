c2ac4c95590e8e23b6d01993980c147a93931401
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-13 03:00:52 +0530

    https://codeforces.com/contest/934/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,89 +7,39 @@ using namespace std;
 	#define debug(x)
 #endif
 
-const int N=1e5+10,BLK=318;
-int a[N];
-pair<pair<int,int>,int> pre[N];
-
-inline int get_blk(int i)
-{
-	return i/BLK;
-}
-
-void build(int n)
-{
-	for(int i=n-1;i>=0;--i)
-	{
-		if(i+a[i]>=n||get_blk(i+a[i])>get_blk(i))
-		{
-			pre[i]={{1,min(n,i+a[i])},i};
-		}
-		else
-		{
-			pre[i]={{pre[i+a[i]].first.first+1,pre[i+a[i]].first.second},pre[i+a[i]].second};
-		}
-	}
-}
-
-void update(int idx,int val,int n)
-{
-	a[idx]=val;
-	for(int i=idx;i>=get_blk(idx)*BLK;--i)
-	{
-		if(i+a[i]>=n||get_blk(i+a[i])>get_blk(i))
-		{
-			pre[i]={{1,min(n,i+a[i])},i};
-		}
-		else
-		{
-			pre[i]={{pre[i+a[i]].first.first+1,pre[i+a[i]].first.second},pre[i+a[i]].second};
-		}
-	}
-}
-
-pair<int,int> query(int idx,int n)
-{
-	int cur=idx,num_jumps=0,last=-1;
-	while(cur<n)
-	{
-		num_jumps+=pre[cur].first.first;
-		last=pre[cur].second;
-		cur=pre[cur].first.second;
-	}
-	return {last+1,num_jumps};
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,q;
-	cin>>n>>q;
-	for(int i=0;i<n;++i)
+	long long p;
+	int k;
+	bool sub=1;
+	vector<int> ans;
+	cin>>p>>k;
+	while(p>0)
 	{
-		cin>>a[i];
-	}
-	build(n);
-	while(q--)
-	{
-		int type;
-		cin>>type;
-		if(type==0)
+		if(sub)
 		{
-			int idx,val;
-			cin>>idx>>val;
-			idx--;
-			update(idx,val,n);
+			int x=p%k;
+			ans.push_back(x);
+			p-=x;
+			p/=k;
 		}
 		else
 		{
-			int idx;
-			cin>>idx;
-			idx--;
-			pair<int,int> p=query(idx,n);
-			cout<<p.first<<' '<<p.second<<'\n';
+			int x=(k-p%k)%k;
+			ans.push_back(x);
+			p+=x;
+			p/=k;
 		}
+		sub=!sub;
 	}
+	cout<<(int)ans.size()<<'\n';
+	for(auto &v:ans)
+	{
+		cout<<v<<' ';
+	}
+	cout<<'\n';
 	return 0;
 }
