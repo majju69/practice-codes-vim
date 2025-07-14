84480cbf688c351c698a83de65254b1298699cbd
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-15 03:29:21 +0530

    https://codeforces.com/contest/1000/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,39 +7,91 @@ using namespace std;
 	#define debug(x)
 #endif
 
+const int mod=998244353;
+
+inline int add(int a,int b)
+{
+	return ((a%mod)+(b%mod))%mod;
+}
+
+inline int mul(int a,int b)
+{
+	return (1ll*(a%mod)*(b%mod))%mod;
+}
+
+vector<int> fact(1001);
+
+void fillFact()
+{
+	int n=fact.size();
+	fact[0]=1;
+	for(int i=1;i<n;++i)
+	{
+		fact[i]=(1ll*(i%mod)*(fact[i-1]%mod))%mod;
+	}
+}
+
+int power(int a,int b)        // Use when mod is of order 10^9 or less
+{
+	int ans=1;
+	a=a%mod;
+	while(b)
+	{
+		if(b&1)
+		{
+			ans=(1ll*(ans%mod)*(a%mod))%mod;
+		}
+		a=(1ll*(a%mod)*(a%mod))%mod;
+		b>>=1;
+	}
+	return ans%mod;
+}
+
+int nCr(int n,int r)         // Ensure that fillFact() is called before this function is used
+{
+	if(n<r||n<0||r<0)
+	{
+		return 0;
+	}
+	if(r==n||r==0)
+	{
+		return 1;
+	}
+	int ans=fact[n];
+	ans=(1ll*ans*power(fact[r],mod-2))%mod;
+	ans=(1ll*ans*power(fact[n-r],mod-2))%mod;
+	return ans;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	long long p;
-	int k;
-	bool sub=1;
-	vector<int> ans;
-	cin>>p>>k;
-	while(p>0)
+	fillFact();
+	int n,ans=0;
+	cin>>n;
+	int *a=new int[n],*dp=new int[n];
+	for(int i=0;i<n;++i)
 	{
-		if(sub)
-		{
-			int x=p%k;
-			ans.push_back(x);
-			p-=x;
-			p/=k;
-		}
-		else
-		{
-			int x=(k-p%k)%k;
-			ans.push_back(x);
-			p+=x;
-			p/=k;
-		}
-		sub=!sub;
+		cin>>a[i];
+		dp[i]=0;
 	}
-	cout<<(int)ans.size()<<'\n';
-	for(auto &v:ans)
+	for(int i=n-1;i>=0;--i)
 	{
-		cout<<v<<' ';
+		if(a[i]>0&&i+a[i]<n)
+		{
+			for(int j=i+a[i]+1;j<n;++j)
+			{
+				// i+1.....j-1 choose a[i]-1
+				dp[i]=add(dp[i],mul(nCr(j-i-1,a[i]),dp[j]));
+			}
+			dp[i]=add(dp[i],nCr(n-i-1,a[i]));
+		}
+		ans=add(ans,dp[i]);
 	}
-	cout<<'\n';
+	cout<<ans<<'\n';
+	delete []a;
+	delete []dp;
 	return 0;
 }
