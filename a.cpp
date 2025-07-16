b2967ccd68f77583941adc59f1bdd577f7efbf19
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-16 05:47:35 +0530

    https://codeforces.com/contest/1902/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,60 +7,9 @@ using namespace std;
 	#define debug(x)
 #endif
 
-const int mod=998244353;
-
-inline int add(int a,int b)
+inline int get(int l,int r,vector<int> &a)
 {
-	return ((a%mod)+(b%mod))%mod;
-}
-
-inline int mul(int a,int b)
-{
-	return (1ll*(a%mod)*(b%mod))%mod;
-}
-
-vector<int> fact(1001);
-
-void fillFact()
-{
-	int n=fact.size();
-	fact[0]=1;
-	for(int i=1;i<n;++i)
-	{
-		fact[i]=(1ll*(i%mod)*(fact[i-1]%mod))%mod;
-	}
-}
-
-int power(int a,int b)        // Use when mod is of order 10^9 or less
-{
-	int ans=1;
-	a=a%mod;
-	while(b)
-	{
-		if(b&1)
-		{
-			ans=(1ll*(ans%mod)*(a%mod))%mod;
-		}
-		a=(1ll*(a%mod)*(a%mod))%mod;
-		b>>=1;
-	}
-	return ans%mod;
-}
-
-int nCr(int n,int r)         // Ensure that fillFact() is called before this function is used
-{
-	if(n<r||n<0||r<0)
-	{
-		return 0;
-	}
-	if(r==n||r==0)
-	{
-		return 1;
-	}
-	int ans=fact[n];
-	ans=(1ll*ans*power(fact[r],mod-2))%mod;
-	ans=(1ll*ans*power(fact[n-r],mod-2))%mod;
-	return ans;
+	return upper_bound(a.begin(),a.end(),r)-lower_bound(a.begin(),a.end(),l);
 }
 
 int main()
@@ -68,30 +17,86 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	fillFact();
-	int n,ans=0;
-	cin>>n;
-	int *a=new int[n],*dp=new int[n];
+	int n,q,_x=0,_y=0;
+	map<pair<int,int>,vector<int>> mp_pre,mp_suf;
+	vector<int> pre_x,pre_y,suf_x,suf_y;
+	string s;
+	cin>>n>>q>>s;
 	for(int i=0;i<n;++i)
 	{
-		cin>>a[i];
-		dp[i]=0;
+		if(s[i]=='U')
+		{
+			_y++;
+		}
+		else if(s[i]=='D')
+		{
+			_y--;
+		}
+		else if(s[i]=='R')
+		{
+			_x++;
+		}
+		else
+		{
+			_x--;
+		}
+		mp_pre[{_x,_y}].push_back(i);
+		pre_x.push_back(_x);
+		pre_y.push_back(_y);
 	}
+	_x=0;
+	_y=0;
 	for(int i=n-1;i>=0;--i)
 	{
-		if(a[i]>0&&i+a[i]<n)
+		if(s[i]=='U')
 		{
-			for(int j=i+a[i]+1;j<n;++j)
-			{
-				// i+1.....j-1 choose a[i]-1
-				dp[i]=add(dp[i],mul(nCr(j-i-1,a[i]),dp[j]));
-			}
-			dp[i]=add(dp[i],nCr(n-i-1,a[i]));
+			_y++;
 		}
-		ans=add(ans,dp[i]);
+		else if(s[i]=='D')
+		{
+			_y--;
+		}
+		else if(s[i]=='R')
+		{
+			_x++;
+		}
+		else
+		{
+			_x--;
+		}
+		mp_suf[{_x,_y}].push_back(i);
+		suf_x.push_back(_x);
+		suf_y.push_back(_y);
 	}
-	cout<<ans<<'\n';
-	delete []a;
-	delete []dp;
+	reverse(suf_x.begin(),suf_x.end());
+	reverse(suf_y.begin(),suf_y.end());
+	for(auto &v:mp_suf)
+	{
+		reverse(v.second.begin(),v.second.end());
+	}
+	while(q--)
+	{
+		int x,y,l,r;
+		cin>>x>>y>>l>>r;
+		l--;
+		r--;
+		if(x==0&&y==0)
+		{
+			cout<<"YES\n";
+			continue;
+		}
+		if((l>0&&mp_pre.count({x,y})&&get(0,l-1,mp_pre[{x,y}])>0)||(r+1<n&&mp_pre.count({x,y})&&get(r+1,n-1,mp_pre[{x,y}])>0))
+		{
+			cout<<"YES\n";
+			continue;
+		}
+		int res_x=x-((l>=1)?pre_x[l-1]:0)+((r+1<n)?suf_x[r+1]:0),res_y=y-((l>=1)?pre_y[l-1]:0)+((r+1<n)?suf_y[r+1]:0);
+		if(mp_suf.count({res_x,res_y})&&get(l,r,mp_suf[{res_x,res_y}])>0)
+		{
+			cout<<"YES\n";
+			continue;
+		}
+		cout<<"NO\n";
+	}
 	return 0;
 }
