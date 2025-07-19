84ad327379b43c7b49b970f3e333c14ae2a40668
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:42:26 +0530

    https://codeforces.com/contest/1102/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -9,42 +9,69 @@ using namespace std;
 
 typedef long long ll;
 
+const ll mod=998244353;
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+{
+    long long ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(ans*a)%mod;
+        }
+        a=(a*a)%mod;       
+        b>>=1;
+    }
+    return ans%mod;
+}
+
+ll mergeOverlap(vector<pair<ll,ll>> &a)
+{
+    ll ans=0;
+    sort(a.begin(),a.end());
+    for(ll i=1;i<(ll)a.size();++i)
+    {
+        if(a[ans].second>=a[i].first)
+        {
+            a[ans].second=max(a[ans].second,a[i].second);
+        }           
+        else
+        {            
+            ans++;
+            a[ans]=a[i];
+        }
+    }
+    return (ans+1);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n,k,ans=0,cnt=0;
-    map<ll,ll> mp;
-    cin>>n>>k;
-    vector<pair<ll,ll>> a(n);   //{beauty,length}
+    ll n;
+    vector<pair<ll,ll>> a;
+    map<ll,pair<ll,ll>> mp;
+    cin>>n;
     for(ll i=0;i<n;++i)
     {
-        cin>>a[i].second>>a[i].first;
-    }
-    sort(a.rbegin(),a.rend());
-    for(ll i=0;i<k;++i)
-    {
-        cnt+=a[i].second;
-        ans=max(ans,cnt*a[i].first);
-        mp[a[i].second]++;
-    }
-    for(ll i=k;i<n;++i)
-    {
-        ll x=(*mp.begin()).first;
-        if(a[i].second>x)
+        ll x;
+        cin>>x;
+        if(mp.find(x)==mp.end())
         {
-            mp[x]--;
-            mp[a[i].second]++;
-            cnt-=x;
-            cnt+=a[i].second;
-            if(mp[x]==0)
-            {
-                mp.erase(x);
-            }
-            ans=max(ans,cnt*a[i].first);
+            mp[x]={i,i};
+        }
+        else
+        {
+            mp[x].second=i;
         }
     }
-    cout<<ans<<'\n';
+    for(auto &v:mp)
+    {
+        a.push_back(v.second);
+    }
+    cout<<power(2,mergeOverlap(a)-1)<<'\n';
     return 0;
 }
