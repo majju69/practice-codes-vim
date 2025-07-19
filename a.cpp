10040d31dde91d0a4aacf0233584358ed0ab9d85
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:22:29 +0530

    https://codeforces.com/contest/1475/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -18,14 +18,81 @@ int main()
     cin>>tc;
     while(tc--)
     {
-        ll a,b,ans=0;
-        cin>>a>>b;
-        for(ll i=1;i<32000;++i)
+        ll n,m,sum=0;
+        cin>>n>>m;
+        vector<ll> arr(n),a[2];
+        for(auto &v:arr)
         {
-            ll l=i+2,r=min(a/i,b+1);
-            ans+=max(0LL,r-l+1);
+            cin>>v;
+            sum+=v;
         }
-        cout<<ans<<'\n';
+        for(ll i=0;i<n;++i)
+        {
+            ll x;
+            cin>>x;
+            x--;
+            a[x].push_back(arr[i]);
+        }
+        if(sum<m)
+        {
+            cout<<-1<<'\n';
+            continue;
+        }
+        sort(a[0].rbegin(),a[0].rend());
+        sort(a[1].rbegin(),a[1].rend());
+        if((ll)a[0].size()>0)
+        {
+            ll s=0,cnt2=0,ans=1e9;
+            vector<ll> pre;
+            pre.push_back(a[0][0]);
+            for(ll i=1;i<(ll)a[0].size();++i)
+            {
+                pre.push_back(pre.back()+a[0][i]);
+            }
+            if(pre.back()>=m)
+            {
+                ans=0;
+                for(auto &v:pre)
+                {
+                    ans++;
+                    if(v>=m)
+                    {
+                        break;
+                    }
+                }
+            }
+            for(ll i=0;i<(ll)a[1].size();++i)
+            {
+                s+=a[1][i];
+                cnt2+=2;
+                if(s>=m)
+                {
+                    ans=min(ans,cnt2);
+                    break;
+                }
+                ll req=m-s;
+                ll idx=lower_bound(pre.begin(),pre.end(),req)-pre.begin();
+                if(idx<(ll)pre.size())
+                {
+                    ans=min(ans,idx+1+cnt2);
+                }
+            }
+            cout<<ans<<'\n';
+        }
+        else
+        {
+            ll s=0,ans=0;
+            for(auto &v:a[1])
+            {
+                s+=v;
+                ans+=2;
+                if(s>=m)
+                {
+                    break;
+                }
+            }
+            cout<<ans<<'\n';
+        }
     }
     return 0;
 }
