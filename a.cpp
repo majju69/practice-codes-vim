be69a85a8b86533fd4ccc50d4ebc2b041b74fdf8
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:51:20 +0530

    https://codeforces.com/contest/1095/problem/F accepted

--- a.cpp
+++ a.cpp
@@ -2,39 +2,133 @@
 using namespace std;
 
 #ifdef LOCAL
-	#include"debug.h"
+    #include"debug.h"
 #else
-	#define debug(x)
+    #define debug(x)
 #endif
 
-int gcd(int a,int b)
+typedef long long ll;
+
+class DisjointSet
 {
-	return ((b==0)?a:gcd(b,a%b));
-}
+
+private:
+
+    vector<long long> ultimateParent,rank,size;
+
+public:
+
+    DisjointSet(long long n)
+    {
+        ultimateParent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(long long i=0;i<=n;++i)
+        {
+            ultimateParent[i]=i;
+        }
+    }
+
+    long long findUltimateParent(long long node)
+    {
+        if(ultimateParent[node]==node)
+        {
+            return node;
+        }
+        return ultimateParent[node]=findUltimateParent(ultimateParent[node]);
+    }
+
+    long long getSize(long long node)
+    {
+        return size[node];
+    }
+
+    long long getRank(long long node)
+    {
+        return rank[node];
+    }
+
+    void unionByRank(long long u,long long v)
+    {
+        long long ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(rank[ultimateParentOfU]<rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+        }
+        else if(rank[ultimateParentOfU]>rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+        }
+        else
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            rank[ultimateParentOfU]++;
+        }
+    }
+
+    void unionBySize(long long u,long long v)
+    {
+        long long ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(size[ultimateParentOfU]<size[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+            size[ultimateParentOfV]+=size[ultimateParentOfU];
+        }
+        else
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            size[ultimateParentOfU]+=size[ultimateParentOfV];
+        }
+    }
+
+};
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
-	{
-		int a,g=-1,cur=-1,mul=1;
-		cin>>a;
-		g=gcd(a,180);
-		cur=a/g;
-		for(int i=1;i<=180;++i)
-		{
-			if(2*a<=cur*i*(180-a))
-			{
-				mul=i;
-				break;
-			}
-		}
-		cout<<(180*mul)/g<<'\n';
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n,m,ans=0;
+    vector<pair<ll,pair<ll,ll>>> edges;
+    cin>>n>>m;
+    DisjointSet ds(n);
+    vector<pair<ll,ll>> a(n);
+    for(ll i=0;i<n;++i)
+    {
+        cin>>a[i].first;
+        a[i].second=i;
+    }
+    sort(a.begin(),a.end());
+    for(ll i=1;i<n;++i)
+    {
+        edges.push_back({a[0].first+a[i].first,{a[0].second,a[i].second}});
+    }
+    for(ll i=0;i<m;++i)
+    {
+        ll u,v,w;
+        cin>>u>>v>>w;
+        u--;
+        v--;
+        edges.push_back({w,{u,v}});
+    }
+    sort(edges.begin(),edges.end());
+    for(auto &edge:edges)
+    {
+        ll u=edge.second.first,v=edge.second.second,w=edge.first;
+        if(ds.findUltimateParent(u)!=ds.findUltimateParent(v))
+        {
+            ds.unionByRank(u,v);
+            ans+=w;
+        }
+    }
+    cout<<ans<<'\n';
+    return 0;
 }
-
