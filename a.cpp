eec9ce42367d901bffc3376fad759f649de795e2
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:13:33 +0530

    https://codeforces.com/contest/1554/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -2,81 +2,74 @@
 using namespace std;
 
 #ifdef LOCAL
-    #include"debug.h"
+	#include"debug.h"
 #else
-    #define debug(x)
+	#define debug(x)
 #endif
+
+inline int bit(int a,int i)
+{
+	return a>>i&1;
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int tc;
-    cin>>tc;
-    while(tc--)
-    {
-        int n,ans=0;
-        queue<int> q;
-        cin>>n;
-        vector<int> adj[n],adjT[n],indegree(n,0),topoSort;
-        for(int i=0;i<n;++i)
-        {
-            int x;
-            cin>>x;
-            while(x--)
-            {
-                int u;
-                cin>>u;
-                u--;
-                adj[u].push_back(i);
-                adjT[i].push_back(u);
-                indegree[i]++;
-            }
-        }
-        for(int i=0;i<n;++i)
-        {
-            if(indegree[i]==0)
-            {
-                q.push(i);
-            }
-        }
-        while(q.size())
-        {
-            int node=q.front();
-            q.pop();
-            topoSort.push_back(node);
-            for(auto &v:adj[node])
-            {
-                indegree[v]--;
-                if(indegree[v]==0)
-                {
-                    q.push(v);
-                }
-            }
-        }
-        if((int)topoSort.size()!=n)
-        {
-            cout<<-1<<'\n';
-        }
-        else
-        {
-            vector<int> dp(n,1e9);
-            for(auto &v:topoSort)
-            {
-                int cur=1;
-                for(auto &v1:adjT[v])
-                {
-                    cur=max(cur,dp[v1]+(v1>v));
-                }
-                dp[v]=cur;
-            }
-            for(auto &v:dp)
-            {
-                ans=max(ans,v);
-            }
-            cout<<ans<<'\n';
-        }
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc;
+	cin>>tc;
+	while(tc--)
+	{
+		int n,m,ans=0;
+		cin>>n>>m;
+		if(n>m)
+		{
+			cout<<0<<'\n';
+			continue;
+		}
+		if(n==0)
+		{
+			cout<<m+1<<'\n';
+			continue;
+		}
+		for(int i=30;i>=0;--i)
+		{
+			int _n=bit(n,i),_m=bit(m,i);
+			if(_n==0&&_m==1)
+			{
+				ans+=(1<<i);
+				continue;
+			}
+			if(_n==1&&_m==1)
+			{
+				continue;
+			}
+			if(_n==0&&_m==0)
+			{
+				bool canSkip=0;
+				for(int j=i-1;j>=0;--j)
+				{
+					if((bit(n,j)==0&&bit(m,j)==0)||(bit(n,j)==1&&bit(m,j)==0))
+					{
+						canSkip=1;
+						break;
+					}
+				}
+				if(canSkip)
+				{
+					continue;
+				}
+				else
+				{
+					ans+=(1<<i);
+					break;
+				}
+			}
+			break;
+		}
+		cout<<ans<<'\n';
+	}
+	return 0;
 }
+
