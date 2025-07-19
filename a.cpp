80d9061196fb52f7c9adde1a5967d8f5e37faf9d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:39:41 +0530

    https://codeforces.com/contest/1140/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,84 +9,42 @@ using namespace std;
 
 typedef long long ll;
 
-vector<long long> spf(10000001,0);
-
-void smallestPrimeFactor()
-{
-    long long n=spf.size();
-    for(long long i=1;i<n;++i)
-    {
-        spf[i]=i;
-    }
-    for(long long i=4;i<n;i+=2)
-    {
-        spf[i]=2;
-    }
-    for(long long i=3;i*i<n;++i)
-    {
-        if(spf[i]==i)
-        {
-            for(long long j=i*i;j<n;j+=i)
-            {
-                if(spf[j]==j)
-                {
-                    spf[j]=i;
-                }
-            }   
-        }
-    }
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    smallestPrimeFactor();
-    ll n,k,ans1=0,ans2=0;
-    map<map<ll,ll>,ll> mp;
+    ll n,k,ans=0,cnt=0;
+    map<ll,ll> mp;
     cin>>n>>k;
+    vector<pair<ll,ll>> a(n);   //{beauty,length}
     for(ll i=0;i<n;++i)
     {
-        ll x;
-        map<ll,ll> cur;
-        cin>>x;
-        while(x>1)
-        {
-            ll cnt=0,p=spf[x];
-            while(x%p==0)
-            {
-                x/=p;
-                cnt++;
-            }
-            cnt%=k;
-            if(cnt>0)
-            {
-                cur[p]=cnt;
-            }
-        }
-        mp[cur]++;
+        cin>>a[i].second>>a[i].first;
     }
-    for(auto &v:mp)
+    sort(a.rbegin(),a.rend());
+    for(ll i=0;i<k;++i)
     {
-        ll cnt=v.second;
-        map<ll,ll> cur=v.first,req;
-        for(auto &v1:cur)
+        cnt+=a[i].second;
+        ans=max(ans,cnt*a[i].first);
+        mp[a[i].second]++;
+    }
+    for(ll i=k;i<n;++i)
+    {
+        ll x=(*mp.begin()).first;
+        if(a[i].second>x)
         {
-            req[v1.first]=(k-v1.second)%k;
-        }
-        if(req==v.first)
-        {
-            ans1+=((cnt*(cnt-1))>>1);
-        }
-        else
-        {
-            if(mp.find(req)!=mp.end())
+            mp[x]--;
+            mp[a[i].second]++;
+            cnt-=x;
+            cnt+=a[i].second;
+            if(mp[x]==0)
             {
-                ans2+=cnt*mp[req];
+                mp.erase(x);
             }
+            ans=max(ans,cnt*a[i].first);
         }
     }
-    cout<<(ans2>>1)+ans1<<'\n';
+    cout<<ans<<'\n';
     return 0;
 }
