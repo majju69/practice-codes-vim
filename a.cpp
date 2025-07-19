1ca27ad4824d5509208abac4594b14cd8e254247
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 02:44:40 +0530

    https://codeforces.com/contest/1101/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,43 +7,90 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-
-const ll mod=998244353;
-
-long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+int gcd(int a,int b)
 {
-    long long ans=1;
-    a=a%mod;
-    while(b)
-    {
-        if(b&1)
-        {
-            ans=(ans*a)%mod;
-        }
-        a=(a*a)%mod;       
-        b>>=1;
-    }
-    return ans%mod;
+    return ((b==0)?a:gcd(b,a%b));
 }
 
-ll mergeOverlap(vector<pair<ll,ll>> &a)
+vector<long long> spf(200001,0);
+
+void smallestPrimeFactor()
 {
-    ll ans=0;
-    sort(a.begin(),a.end());
-    for(ll i=1;i<(ll)a.size();++i)
+    long long n=spf.size();
+    for(long long i=1;i<n;++i)
     {
-        if(a[ans].second>=a[i].first)
+        spf[i]=i;
+    }
+    for(long long i=4;i<n;i+=2)
+    {
+        spf[i]=2;
+    }
+    for(long long i=3;i*i<n;++i)
+    {
+        if(spf[i]==i)
         {
-            a[ans].second=max(a[ans].second,a[i].second);
-        }           
-        else
-        {            
-            ans++;
-            a[ans]=a[i];
+            for(long long j=i*i;j<n;j+=i)
+            {
+                if(spf[j]==j)
+                {
+                    spf[j]=i;
+                }
+            }   
         }
     }
-    return (ans+1);
+}
+
+// 1 --> ending  0 --> passing
+void dfs(int node,int p,vector<int> adj[],vector<int> &a,map<pair<int,pair<int,int>>,int> &dp)
+{
+    if(node!=0&&adj[node].size()==1)
+    {
+        int x=a[node];
+        while(x>1)
+        {
+            dp[{node,{1,spf[x]}}]=1;
+            x/=spf[x];
+        }
+        return;
+    }
+    for(auto &v:adj[node])
+    {
+        if(v!=p)
+        {
+            dfs(v,node,adj,a,dp);
+        }
+    }
+    map<int,vector<int>> mp;
+    int x=a[node];
+    while(x>1)
+    {
+        dp[{node,{1,spf[x]}}]=1;
+        x/=spf[x];
+    }
+    for(auto &v:adj[node])
+    {
+        if(v!=p)
+        {
+            int g=gcd(a[node],a[v]);
+            while(g>1)
+            {
+                if(dp.find({v,{1,spf[g]}})!=dp.end())
+                {
+                    dp[{node,{1,spf[g]}}]=max(dp[{v,{1,spf[g]}}]+1,dp[{node,{1,spf[g]}}]);
+                    mp[g].push_back(dp[{v,{1,spf[g]}}]);
+                }
+                g/=spf[g];
+            }
+        }
+    }
+    for(auto &v:mp)
+    {
+        sort(v.second.rbegin(),v.second.rend());
+        if(v.second.size()>1)
+        {
+            dp[{node,{0,v.first}}]=v.second[0]+v.second[1]+1;
+        }
+    }
 }
 
 int main()
@@ -51,27 +98,41 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n;
-    vector<pair<ll,ll>> a;
-    map<ll,pair<ll,ll>> mp;
+    smallestPrimeFactor();
+    int n,ans=0;
+    map<pair<int,pair<int,int>>,int> dp;
     cin>>n;
-    for(ll i=0;i<n;++i)
+    vector<int> a(n),adj[n];
+    for(auto &v:a)
     {
-        ll x;
-        cin>>x;
-        if(mp.find(x)==mp.end())
+        cin>>v;
+        int x=1;
+        set<int> st;
+        while(v>1)
         {
-            mp[x]={i,i};
+            if(st.find(spf[v])==st.end())
+            {
+                st.insert(spf[v]);
+                x*=spf[v];
+            }
+            v/=spf[v];
         }
-        else
-        {
-            mp[x].second=i;
-        }
+        v=x;
     }
-    for(auto &v:mp)
+    for(int i=1;i<n;++i)
     {
-        a.push_back(v.second);
+        int u,v;
+        cin>>u>>v;
+        u--;
+        v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    cout<<power(2,mergeOverlap(a)-1)<<'\n';
+    dfs(0,0,adj,a,dp);
+    for(auto &v:dp)
+    {
+        ans=max(ans,v.second);
+    }
+    cout<<ans<<'\n';
     return 0;
 }
