ca5851fac386a7be74f3922a85d127a9a99632da
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 17:08:49 +0530

    https://codeforces.com/contest/979/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -1,58 +1,153 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#ifdef LOCAL
-    #include"debug.h"
-#else
-    #define debug(x)
-#endif
-
-int main()
+class DisjointSet
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n,m,maxx=-1;
-    pair<int,int> ans={-1,-1};
-    cin>>n;
-    vector<int> a(n);
-    for(auto &v:a)
+
+private:
+
+    vector<int> ultimateParent,rank,size;
+
+public:
+
+    DisjointSet(int n)
     {
-        cin>>v;
-    }
-    cin>>m;
-    vector<int> b(m);
-    for(auto &v:b)
-    {
-        cin>>v;
-    }
-    maxx=2*(n-m);
-    ans={2*n,2*m};
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    for(int i=0;i<n;++i)    // a scores 3 points from i to n-1
-    {
-        int score_a=2*i+3*(n-i),score_b=-1;
-        int d=a[i]-1;
-        int idx=upper_bound(b.begin(),b.end(),d)-b.begin(); // idx....m-1 3 points 0....idx-1 2 points
-        score_b=2*idx+3*(m-idx);
-        if(score_a-score_b>maxx)
+        ultimateParent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;++i)
         {
-            maxx=score_a-score_b;
-            ans={score_a,score_b};
+            ultimateParent[i]=i;
         }
-        else if(score_a-score_b==maxx)
+    }
+
+    int findUltimateParent(int node)
+    {
+        if(ultimateParent[node]==node)
         {
-            if(score_a>ans.first)
-            {
-                ans={score_a,score_b};
-            }
+            return node;
+        }
+        return ultimateParent[node]=findUltimateParent(ultimateParent[node]);
+    }
+
+    int getSize(int node)
+    {
+        return size[node];
+    }
+
+    int getRank(int node)
+    {
+        return rank[node];
+    }
+
+    void unionByRank(int u,int v)
+    {
+        int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(rank[ultimateParentOfU]<rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+        }
+        else if(rank[ultimateParentOfU]>rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
         }
         else
         {
-            continue;
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            rank[ultimateParentOfU]++;
         }
     }
-    cout<<ans.first<<':'<<ans.second<<'\n';
+
+    void unionBySize(int u,int v)
+    {
+        int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(size[ultimateParentOfU]<size[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+            size[ultimateParentOfV]+=size[ultimateParentOfU];
+        }
+        else
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            size[ultimateParentOfU]+=size[ultimateParentOfV];
+        }
+    }
+
+};
+
+void dfsPath(int node,int bee,vector<int> adj[],vector<bool> &vis,vector<int> &path)
+{
+    path.push_back(node);
+    vis[node]=true;
+    if(node==bee)
+    {
+        return ;
+    }
+    for(auto &v:adj[node])
+    {
+        if(!vis[v])
+        {
+            dfsPath(v,bee,adj,vis,path);
+        }
+    }
+    if(path.back()!=bee)
+    {
+        path.pop_back();
+    }
+}
+
+int main()
+{
+    int n,x,y,a=0,b=0;
+    cin>>n>>x>>y;
+    x--;
+    y--;
+    vector<int> adj[n],path;
+    vector<bool> vis(n,false);
+    map<pair<int,int>,bool> isInPath;
+    DisjointSet ds(n);
+    for(int i=0;i<n-1;++i)
+    {
+        int u,v;
+        cin>>u>>v;
+        u--;
+        v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        isInPath[make_pair(min(u,v),max(u,v))]=false;
+    }
+    dfsPath(x,y,adj,vis,path);
+    for(int i=0;i<path.size()-1;++i)
+    {
+        int u=min(path[i],path[i+1]),v=max(path[i],path[i+1]);
+        isInPath[make_pair(u,v)]=true;
+    }
+        
+    for(int i=0;i<n;++i)
+    {
+        for(auto &v:adj[i])
+        {
+            int v1=min(i,v),v2=max(i,v);
+            if(!isInPath[make_pair(v1,v2)])
+            {
+                ds.unionBySize(v1,v2);
+            }
+        }
+    }
+    for(int i=0;i<n;++i)
+    {
+        a+=(ds.findUltimateParent(i)==ds.findUltimateParent(x));
+        b+=(ds.findUltimateParent(i)==ds.findUltimateParent(y));
+    }
+    cout<<(long long)n*(n-1)-(long long)a*b<<endl;
     return 0;
 }
+
