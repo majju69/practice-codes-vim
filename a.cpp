4dd08f93fcfbcc0f7e4135f724ed2fcdd45f507b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 16:55:46 +0530

    https://codeforces.com/contest/231/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,130 +9,61 @@ using namespace std;
 
 typedef long long ll;
 
-class DisjointSet
-{
-
-private:
-
-    vector<long long> ultimateParent,rank,size;
-
-public:
-
-    DisjointSet(long long n)
-    {
-        ultimateParent.resize(n+1);
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
-        for(long long i=0;i<=n;++i)
-        {
-            ultimateParent[i]=i;
-        }
-    }
-
-    long long findUltimateParent(long long node)
-    {
-        if(ultimateParent[node]==node)
-        {
-            return node;
-        }
-        return ultimateParent[node]=findUltimateParent(ultimateParent[node]);
-    }
-
-    long long getSize(long long node)
-    {
-        return size[node];
-    }
-
-    long long getRank(long long node)
-    {
-        return rank[node];
-    }
-
-    void unionByRank(long long u,long long v)
-    {
-        long long ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
-        if(ultimateParentOfU==ultimateParentOfV)
-        {
-            return ;
-        }
-        if(rank[ultimateParentOfU]<rank[ultimateParentOfV])
-        {
-            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
-        }
-        else if(rank[ultimateParentOfU]>rank[ultimateParentOfV])
-        {
-            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
-        }
-        else
-        {
-            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
-            rank[ultimateParentOfU]++;
-        }
-    }
-
-    void unionBySize(long long u,long long v)
-    {
-        long long ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
-        if(ultimateParentOfU==ultimateParentOfV)
-        {
-            return ;
-        }
-        if(size[ultimateParentOfU]<size[ultimateParentOfV])
-        {
-            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
-            size[ultimateParentOfV]+=size[ultimateParentOfU];
-        }
-        else
-        {
-            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
-            size[ultimateParentOfU]+=size[ultimateParentOfV];
-        }
-    }
-
-};
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n;
-    map<ll,deque<ll>> mp;
-    cin>>n;
-    vector<ll> a(n);
-    vector<string> adjMat(n);
-    DisjointSet ds(n);
+    ll n,k,maxx=-1,num=1e18;
+    cin>>n>>k;
+    vector<ll> a(n),pre(n);
     for(auto &v:a)
     {
         cin>>v;
     }
-    for(auto &v:adjMat)
+    sort(a.begin(),a.end());
+    pre[0]=a[0];
+    for(ll i=1;i<n;++i)
     {
-        cin>>v;
+        pre[i]=pre[i-1]+a[i];
     }
-    for(ll i=0;i<n-1;++i)
+    for(ll i=0;i<n;++i)
     {
-        for(ll j=i+1;j<n;++j)
+        // idx-->a[i]-a[idx] idx+1-->a[i]-a[idx+1] ...... i-->a[i]-a[i]
+        // total=(i-idx+1)*a[i]-pre[i]+pre[idx-1]
+        ll lo=0,hi=i,idx=-1;
+        while(lo<=hi)
         {
-            if(adjMat[i][j]=='1')
+            ll mid=lo+(hi-lo)/2;
+            ll sum=(i-mid+1)*a[i]-pre[i];
+            if(mid!=0)
             {
-                ds.unionByRank(i,j);
+                sum+=pre[mid-1];
+            }
+            if(sum<=k)
+            {
+                idx=mid;
+                hi=mid-1;
+            }
+            else
+            {
+                lo=mid+1;
             }
         }
+        if(i-idx+1>maxx)
+        {
+            maxx=i-idx+1;
+            num=a[i];
+        }
+        else if(i-idx+1==maxx)
+        {
+            num=min(num,a[i]);
+        }
+        else
+        {
+            continue;
+        }
     }
-    for(ll i=0;i<n;++i)
-    {
-        mp[ds.findUltimateParent(i)].push_back(a[i]);
-    }
-    for(auto &v:mp)
-    {
-        sort(v.second.begin(),v.second.end());
-    }
-    for(ll i=0;i<n;++i)
-    {
-        cout<<mp[ds.findUltimateParent(i)][0]<<' ';
-        mp[ds.findUltimateParent(i)].pop_front();
-    }
-    cout<<'\n';
+    cout<<maxx<<' '<<num<<'\n';
     return 0;
 }
