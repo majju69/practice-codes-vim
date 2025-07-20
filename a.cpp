c13f91b76064c0f63125544ab5edc3bfc35a8152
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 16:25:08 +0530

    https://codeforces.com/contest/322/problem/B accepted

--- a.cpp
+++ a.cpp
@@ -7,62 +7,46 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-
-const ll mod=998244353;
-
-inline ll mul(ll a,ll b)
-{
-    return (a%mod*b%mod)%mod;
-}
-
-inline ll add(ll a,ll b)
-{
-    return (a%mod+b%mod)%mod;
-}
-
-vector<long long> fact(1000001),calc(1000001);
-
-void fillFact()
-{
-    long long n=fact.size();
-    fact[0]=1;
-    for(long long i=1;i<n;++i)
-    {
-        fact[i]=(i%mod*fact[i-1]%mod)%mod;
-    }
-}
-
-void fillCalc()
-{
-    ll n=calc.size();
-    calc[3]=3;
-    for(ll i=4;i<n;++i)
-    {
-        calc[i]=mul(i,calc[i-1]-1);
-    }
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n;
-    cin>>n;
-    if(n==1)
+    int r,g,b,ans=0;
+    string ord="rgbm";
+    cin>>r>>g>>b;
+    sort(ord.begin(),ord.end());
+    do
     {
-        cout<<1<<'\n';
-    }
-    else if(n==2)
-    {
-        cout<<2<<'\n';
-    }
-    else
-    {
-        fillFact();
-        fillCalc();
-        cout<<add(mul(fact[n],n-2),calc[n])<<'\n';
-    }
+        int tmp_r=r,tmp_g=g,tmp_b=b,cur_ans=0;
+        for(auto &v:ord)
+        {
+            if(v=='r')
+            {
+                cur_ans+=(tmp_r/3);
+                tmp_r%=3;
+            }
+            else if(v=='g')
+            {
+                cur_ans+=(tmp_g/3);
+                tmp_g%=3;
+            }
+            else if(v=='b')
+            {
+                cur_ans+=(tmp_b/3);
+                tmp_b%=3;
+            }
+            else
+            {
+                int minn=min({tmp_b,tmp_g,tmp_r});
+                cur_ans+=minn;
+                tmp_r-=minn;
+                tmp_g-=minn;
+                tmp_b-=minn;
+            }
+        }
+        ans=max(cur_ans,ans);
+    }while(next_permutation(ord.begin(),ord.end()));
+    cout<<ans<<'\n';
     return 0;
 }
