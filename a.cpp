4e3bccd49952b7a0cea3598a87bbd2016f2f6e58
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 17:30:45 +0530

    https://codeforces.com/contest/616/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -1,57 +1,150 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dijkstra(int src,vector<int> adj[],vector<int> &dist)
+class DisjointSet
 {
-    queue<pair<int,int> > q;
-    dist[src]=0;
-    q.push(make_pair(src,0));
-    while(q.size())
+
+private:
+
+    vector<int> ultimateParent,rank,size;
+
+public:
+
+    DisjointSet(int n)
     {
-        int node=q.front().first,dis=q.front().second;
-        q.pop();
-        for(auto &v:adj[node])
+        ultimateParent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;++i)
         {
-            if(dist[v]>1+dis)
-            {
-                dist[v]=1+dis;
-                q.push(make_pair(v,dist[v]));
-            }
+            ultimateParent[i]=i;
         }
     }
-}
+
+    int findUltimateParent(int node)
+    {
+        if(ultimateParent[node]==node)
+        {
+            return node;
+        }
+        return ultimateParent[node]=findUltimateParent(ultimateParent[node]);
+    }
+
+    int getSize(int node)
+    {
+        return size[node];
+    }
+
+    int getRank(int node)
+    {
+        return rank[node];
+    }
+
+    void unionByRank(int u,int v)
+    {
+        int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(rank[ultimateParentOfU]<rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+        }
+        else if(rank[ultimateParentOfU]>rank[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+        }
+        else
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            rank[ultimateParentOfU]++;
+        }
+    }
+
+    void unionBySize(int u,int v)
+    {
+        int ultimateParentOfU=findUltimateParent(u),ultimateParentOfV=findUltimateParent(v);
+        if(ultimateParentOfU==ultimateParentOfV)
+        {
+            return ;
+        }
+        if(size[ultimateParentOfU]<size[ultimateParentOfV])
+        {
+            ultimateParent[ultimateParentOfU]=ultimateParentOfV;
+            size[ultimateParentOfV]+=size[ultimateParentOfU];
+        }
+        else
+        {
+            ultimateParent[ultimateParentOfV]=ultimateParentOfU;
+            size[ultimateParentOfU]+=size[ultimateParentOfV];
+        }
+    }
+
+};
 
 int main()
 {
-    int n;
-    cin>>n;
-    vector<int> adj[n],dist(n,INT_MAX);
+    int n,m,dx[]={-1,1,0,0},dy[]={0,0,-1,1};
+    cin>>n>>m;
+    vector<string> str(n),ans;
+    DisjointSet ds(n*m);
     for(int i=0;i<n;++i)
     {
-        int x;
-        cin>>x;
-        if(i!=x-1)
+        cin>>str[i];
+    }
+    for(int x=0;x<n;++x)
+    {
+        for(int y=0;y<m;++y)
         {
-            adj[i].push_back(x-1);
+            if(str[x][y]=='.')
+            {
+                for(int i=0;i<4;++i)
+                {
+                    int r=x+dx[i],c=y+dy[i];
+                    if(r>=0&&r<n&&c>=0&&c<m&&str[r][c]=='.')
+                    {
+                        ds.unionBySize(r*m+c,x*m+y);
+                    }
+                }
+            }
         }
+    }
+    for(int x=0;x<n;++x)
+    {
+        string s;
+        for(int y=0;y<m;++y)
+        {
+            if(str[x][y]=='.')
+            {
+                s.push_back('.');
+            }
+            else
+            {
+                int cnt=1;
+                unordered_set<int> distinctComp;
+                for(int i=0;i<4;++i)
+                {
+                    int r=x+dx[i],c=y+dy[i];
+                    if(r>=0&&r<n&&c>=0&&c<m&&str[r][c]=='.')
+                    {
+                        distinctComp.insert(ds.findUltimateParent(r*m+c));
+                    }
+                }
+                for(auto &v:distinctComp)
+                {
+                    cnt+=ds.getSize(v);
+                }
+                cnt%=10;
+                s.push_back((char)(cnt+'0'));
+            }
+        }
+        ans.push_back(s);
     }
     for(int i=0;i<n;++i)
     {
-        if(i!=0)
-        {
-            adj[i].push_back(i-1);
-        }
-        if(i!=n-1)
-        {
-            adj[i].push_back(i+1);
-        }
+        cout<<ans[i]<<endl;
     }
-    dijkstra(0,adj,dist);
-    for(int i=0;i<n;++i)
-    {
-        cout<<dist[i]<<' ';
-    }
-    cout<<endl;
     return 0;
 }
 
