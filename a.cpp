7112e017cafb4017583108b62239a5ee91011d02
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 16:51:02 +0530

    https://codeforces.com/contest/888/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,51 +7,34 @@ using namespace std;
     #define debug(x)
 #endif
 
+typedef long long ll;
+
+ll nCr(ll n,ll k)
+{
+    if(k>n||n<0||k<0)
+    {
+        return 0;
+    }
+    ll num=1,den=1;
+    for(ll i=0;i<k;++i)
+    {
+        num*=(n-i);
+        den*=(i+1);
+    }
+    return num/den;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n,k,mx=0,ind=1e9;
+    ll n,k,ans=0,d[]={1,0,1,2,9};
     cin>>n>>k;
-    vector<int> a(n),pre(n,0);
-    for(auto &v:a)
+    for(ll i=0;i<=k;++i)
     {
-        cin>>v;
+        ans+=nCr(n,i)*d[i];
     }
-    pre[0]=(a[0]==0);
-    for(int i=1;i<n;++i)
-    {
-        pre[i]=pre[i-1]+(a[i]==0);
-    }
-    for(int i=0;i<n;++i)
-    {
-        // zero[i..idx]>k <==> pre[idx]-pre[i-1]>k <==> pre[idx]>pre[i-1]+k
-        int req=k+((i==0)?0:pre[i-1]);
-        int idx=upper_bound(pre.begin(),pre.end(),req)-pre.begin()-1;
-        if(idx-i+1>mx)
-        {
-            mx=idx-i+1;
-            ind=i;
-        }
-    }
-    for(int i=ind;i<n;++i)
-    {
-        if(k<=0)
-        {
-            break;
-        }
-        if(a[i]==0)
-        {
-            a[i]=1;
-            k--;
-        }
-    }
-    cout<<mx<<'\n';
-    for(auto &v:a)
-    {
-        cout<<v<<' ';
-    }
-    cout<<'\n';
+    cout<<ans<<'\n';
     return 0;
 }
