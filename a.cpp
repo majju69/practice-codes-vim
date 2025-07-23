476e12f939bef777e87b36cd5dd327540e204bf4
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-24 02:28:26 +0530

    https://www.codechef.com/problems/SWAPABK accepted

--- a.cpp
+++ a.cpp
@@ -7,17 +7,199 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-typedef long double ld;
+const int N=2e5+10,BLK=448;
+int a[N],b[N],start1[BLK],start2[BLK];
+long long sum1[BLK],sum2[BLK];
 
-inline bool isSetBit(ll a,ll i)
+inline int get_blk(int i)
 {
-    return ((a&(1LL<<i))!=0);
+    return i/BLK;
 }
 
-inline ll switchBit(ll a,ll i)
+void init(int n)
 {
-    return (a^(1LL<<i));
+    for(int i=0;i<n;i+=BLK)
+    {
+        start1[get_blk(i)]=-1;
+        start2[get_blk(i)]=-1;
+    }
+}
+
+void build(int n,int k)
+{
+    int cur_blk=0;
+    while(cur_blk<BLK)
+    {
+        int cur_idx=cur_blk*BLK;
+        if(cur_idx>=n)
+        {
+            break;
+        }
+        long long s1=0,s2=0;
+        bool ptr1=1,ptr2=1;
+        for(int i=cur_idx;i<min(n,cur_idx+BLK);++i)
+        {
+            if(ptr1)
+            {
+                if(a[i]+k<b[i])
+                {
+                    s1+=(long long)b[i];
+                    start1[cur_blk]=1;
+                    ptr1=!ptr1;
+                }
+                else
+                {
+                    s1+=(long long)a[i];
+                }
+            }
+            else
+            {
+                if(b[i]+k<a[i])
+                {
+                    s1+=(long long)a[i];
+                    start1[cur_blk]=2;
+                    ptr1=!ptr1;
+                }
+                else
+                {
+                    s1+=(long long)b[i];
+                }
+            }
+            if(ptr2)
+            {
+                if(b[i]+k<a[i])
+                {
+                    s2+=(long long)a[i];
+                    start2[cur_blk]=2;
+                    ptr2=!ptr2;
+                }
+                else
+                {
+                    s2+=(long long)b[i];
+                }
+            }
+            else
+            {
+                if(a[i]+k<b[i])
+                {
+                    s2+=(long long)b[i];
+                    start2[cur_blk]=1;
+                    ptr2=!ptr2;
+                }
+                else
+                {
+                    s2+=(long long)a[i];
+                }
+            }
+        }
+        sum1[cur_blk]=s1;
+        sum2[cur_blk]=s2;
+        cur_blk++;
+    }
+}
+
+void update(int i,int k,int x,int t,int n)
+{
+    if(t==1)
+    {
+        a[i]=x;
+    }
+    else
+    {
+        b[i]=x;
+    }
+    int cur_blk=get_blk(i);
+    int cur_idx=cur_blk*BLK;
+    long long s1=0,s2=0;
+    bool ptr1=1,ptr2=1;
+    start1[cur_blk]=start2[cur_blk]=-1;
+    for(int i=cur_idx;i<min(n,cur_idx+BLK);++i)
+    {
+        if(ptr1)
+        {
+            if(a[i]+k<b[i])
+            {
+                s1+=(long long)b[i];
+                start1[cur_blk]=1;
+                ptr1=!ptr1;
+            }
+            else
+            {
+                s1+=(long long)a[i];
+            }
+        }
+        else
+        {
+            if(b[i]+k<a[i])
+            {
+                s1+=(long long)a[i];
+                start1[cur_blk]=2;
+                ptr1=!ptr1;
+            }
+            else
+            {
+                s1+=(long long)b[i];
+            }
+        }
+        if(ptr2)
+        {
+            if(b[i]+k<a[i])
+            {
+                s2+=(long long)a[i];
+                start2[cur_blk]=2;
+                ptr2=!ptr2;
+            }
+            else
+            {
+                s2+=(long long)b[i];
+            }
+        }
+        else
+        {
+            if(a[i]+k<b[i])
+            {
+                s2+=(long long)b[i];
+                start2[cur_blk]=1;
+                ptr2=!ptr2;
+            }
+            else
+            {
+                s2+=(long long)a[i];
+            }
+        }
+    }
+    sum1[cur_blk]=s1;
+    sum2[cur_blk]=s2;
+}
+
+long long query(int n)
+{
+    int pvs=2;
+    long long ans=0;
+    for(int cur_blk=0;cur_blk<BLK;++cur_blk)
+    {
+        if(cur_blk*BLK>=n)
+        {
+            break;
+        }
+        if(pvs==2)
+        {
+            ans+=sum1[cur_blk];
+            if(start1[cur_blk]!=-1)
+            {
+                pvs=start1[cur_blk];
+            }
+        }
+        else
+        {
+            ans+=sum2[cur_blk];
+            if(start2[cur_blk]!=-1)
+            {
+                pvs=start2[cur_blk];
+            }
+        }
+    }
+    return ans;
 }
 
 int main()
@@ -25,46 +207,31 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n;
-    cin>>n;
-    vector<vector<ld>> p(n,vector<ld>(n));
-    vector<ld> dp((1LL<<n),0.0);
-    for(auto &vec:p)
+    int tc;
+    cin>>tc;
+    while(tc--)
     {
-        for(auto &v:vec)
+        int n,k,q;
+        cin>>n>>k;
+        for(int i=0;i<n;++i)
         {
-            cin>>v;
+            cin>>a[i];
+        }
+        for(int i=0;i<n;++i)
+        {
+            cin>>b[i];
+        }
+        init(n);
+        build(n,k);
+        cin>>q;
+        while(q--)
+        {
+            int t,i,x;
+            cin>>t>>i>>x;
+            i--;
+            update(i,k,x,t,n);
+            cout<<query(n)<<'\n';
         }
     }
-    dp[(1LL<<n)-1]=1.0;
-    for(ll mask=(1LL<<n)-1;mask>0;--mask)
-    {
-        if(__builtin_popcountll(mask)==1)
-        {
-            continue;
-        }
-        for(ll i=0;i<n;++i)
-        {
-            if(!isSetBit(mask,i))
-            {
-                continue;
-            }
-            ll cnt=__builtin_popcountll(mask);
-            for(ll j=0;j<n;++j)
-            {
-                if(!isSetBit(mask,j)||i==j)
-                {
-                    continue;
-                }
-                dp[switchBit(mask,i)]+=1.0*p[j][i]*dp[mask]/(cnt*(cnt-1));
-                dp[switchBit(mask,j)]+=1.0*p[i][j]*dp[mask]/(cnt*(cnt-1));
-            }
-        }
-    }
-    for(ll i=0;i<n;++i)
-    {
-        cout<<fixed<<setprecision(10)<<dp[(1LL<<i)]<<' ';
-    }
-    cout<<'\n';
     return 0;
 }
