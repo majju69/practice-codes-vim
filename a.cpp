e8e6c137702415b9cbbc03054550a5f86f4c883f
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-08-07 11:33:49 +0530

    https://codeforces.com/contest/1777/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,54 +7,130 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
+vector<vector<int>> divisors(100001);
+
+void fillDivisors()
+{
+    int n=divisors.size();
+    divisors[0].push_back(0);
+    divisors[1].push_back(1);
+    for(int i=2;i<n;i++)
+    {
+        divisors[i].push_back(1);
+    }
+    for(int i=2;i<n;i++)
+    {
+        for(int j=i;j<n;j+=i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+}
+
+bool check(int mid,int m,vector<int> &a)
+{
+    int n=a.size(),i1=0,i2=-1,total=0;
+    vector<int> cnt(m+1,0);
+    for(int i=0;i<n;++i)
+    {
+        if(a[i]-a[i1]<=mid)
+        {
+            for(auto &d:divisors[a[i]])
+            {
+                if(d<=m)
+                {
+                    cnt[d]++;
+                    total+=(cnt[d]==1);
+                    if(total==m)
+                    {
+                        return 1;
+                    }
+                }
+            }
+            i2=i;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while(i2<n)
+    {
+        for(auto &d:divisors[a[i1]])
+        {
+            if(d<=m)
+            {
+                if(mid==0)
+                {
+                    debug(d);
+                }
+                total-=(cnt[d]==1);
+                cnt[d]--;
+            }
+        }
+        i1++;
+        int _i2=i2;
+        for(int i=i2+1;i<n;++i)
+        {
+            if(a[i]-a[i1]<=mid)
+            {
+                for(auto &d:divisors[a[i]])
+                {
+                    if(d<=m)
+                    {
+                        cnt[d]++;
+                        total+=(cnt[d]==1);
+                        if(total==m)
+                        {
+                            return 1;
+                        }
+                    }
+                }
+                _i2=i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        i2=max(i1,_i2);
+    }
+    return 0;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll tc;
+    fillDivisors();
+    int tc;
     cin>>tc;
     while(tc--)
     {
-        ll n,sum=0,pvs=1e18;
-        bool ok=1;
-        cin>>n;
-        vector<ll> a(n<<1);
+        int n,m,lo=0,hi=-1,ans=-1;
+        cin>>n>>m;
+        vector<int> a(n);
         for(auto &v:a)
         {
             cin>>v;
         }
         sort(a.begin(),a.end());
-        while(n>0)
+        hi=a[n-1]-a[0];
+        while(lo<=hi)
         {
-            if(a[2*n-1]!=a[2*n-2])
+            int mid=lo+(hi-lo)/2;
+            if(check(mid,m,a))
             {
-                ok=0;
-                break;
+                ans=mid;
+                hi=mid-1;
             }
-            if((a[2*n-1]-2*sum)%(n<<1)||(a[2*n-1]-2*sum)<=0)
+            else
             {
-                ok=0;
-                break;
+                lo=mid+1;
             }
-            ll cur=(a[2*n-1]-2*sum)/(n<<1);
-            if(cur>=pvs)
-            {
-                ok=0;
-                break;
-            }
-            pvs=cur;
-            sum+=cur;
-            n--;
-            a.pop_back();
-            a.pop_back();
         }
-        cout<<(ok?"YES":"NO")<<'\n';
+        cout<<ans<<'\n';
     }
     return 0;
 }
-
-
-// b+x+b-x=2b
