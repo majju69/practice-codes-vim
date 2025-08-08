3d8a6f45176a233f2edc8e4207522063e2bcbc13
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-08-08 18:04:12 +0530

    https://codeforces.com/contest/1228/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -9,61 +9,87 @@ using namespace std;
 
 typedef long long ll;
 
+const ll mod=1e9+7;
+
+inline bool mul_check(long long a,long long b)
+{
+    return (a==0||b==0||(a<=(long long)2e18/b&&b<=(long long)2e18/a));
+}
+
+inline ll mul(ll a,ll b)
+{
+    return ((a%mod)*(b%mod))%mod;
+}
+
+inline ll add(ll a,ll b)
+{
+    return ((a%mod)+(b%mod))%mod;
+}
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+{
+    long long ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(1ll*(ans%mod)*(a%mod))%mod;
+        }
+        a=(1ll*(a%mod)*(a%mod))%mod;       
+        b>>=1;
+    }
+    return ans%mod;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    #ifdef LOCAL
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-        freopen("error.txt", "w", stderr);
-    #endif
-    ll n,k,ans=0;
-    map<ll,ll> mp1,mp2,mp3;
-    cin>>n>>k;
-    for(ll i=0;i<n;++i)
+    ll x,n,ans=1;
+    vector<ll> primes;
+    cin>>x>>n;
+    if(x%2==0)
     {
-        ll x;
-        cin>>x;
-        mp1[x]++;
-        if(x%k==0)
+        primes.push_back(2);
+        while(x%2==0)
         {
-            if(mp1.find(x/k)!=mp1.end())
+            x/=2;
+        }
+    }
+    for(ll i=3;i*i<=x;i+=2)
+    {
+        if(x%i==0)
+        {
+            primes.push_back(i);
+            while(x%i==0)
             {
-                mp2[x]+=mp1[x/k];
-            }
-            if(mp2.find(x/k)!=mp2.end())
-            {
-                mp3[x]+=mp2[x/k];
+                x/=i;
             }
         }
     }
-    if(k!=1)
+    if(x>2)
     {
-        for(auto &v:mp3)
-        {
-            if(v.first!=0)
-            {
-                ans+=v.second;
-            }
-        }
-        for(auto &v:mp1)
-        {
-            if(v.first==0)
-            {
-                ll x=v.second;
-                ans+=(x*(x-1)*(x-2)/6);
-            }
-        }
+        primes.push_back(x);
     }
-    else
+    for(auto &p:primes)
     {
-        for(auto &v:mp1)
+        ll cur=p;
+        while(cur<=n)
         {
-            ll x=v.second;
-            ans+=(x*(x-1)*(x-2)/6);
+            ll big=(mul_check(cur,p)?cur*p:2e18),small=cur;
+            ans=mul(ans,power(cur,n/small-n/big));
+            if(mul_check(cur,p))
+            {
+                cur*=p;
+            }
+            else
+            {
+                cur=2e18;
+            }
         }
+
     }
     cout<<ans<<'\n';
     return 0;
