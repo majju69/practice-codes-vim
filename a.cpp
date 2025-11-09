68a21e1e18dba678b1666d2de33966ce207139eb
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-10 03:08:36 +0530

    https://codeforces.com/contest/1537/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,11 +7,17 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-
-inline bool bit(ll a,ll i)
+inline bool get(int n)
 {
-    return (a>>i&1);
+    if(n&1)
+    {
+        return 0;
+    }
+    if((n&(n-1))==0&&(__builtin_ctz(n)&1))
+    {
+        return 0;
+    }
+    return 1;
 }
 
 int main()
@@ -19,53 +25,13 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n=0,m,ans=-1;
-    string s;
-    cin>>s>>m;
-    n=s.size();
-    vector<ll> mult(n,1),fact(n+1,1),freq(10,0);
-    vector<vector<ll>> dp((1<<n),vector<ll>(m,0));
-    for(ll i=n-2;i>=0;--i)
+    int tc;
+    cin>>tc;
+    while(tc--)
     {
-        mult[i]=(mult[i+1]*10)%m;
+        int n;
+        cin>>n;
+        cout<<(get(n)?"Alice":"Bob")<<'\n';
     }
-    for(ll i=2;i<=n;++i)
-    {
-        fact[i]=i*fact[i-1];
-    }
-    for(ll i=0;i<n;++i)
-    {
-        freq[s[i]-'0']++;
-        if(s[i]!='0')
-        {
-            dp[(1<<i)][((s[i]-'0')%m*mult[0])%m]=1;
-        }
-    }
-    for(ll mask=2;mask<(1<<n);++mask)
-    {
-        if(__builtin_popcountll(mask)<2)
-        {
-            continue;
-        }
-        ll cnt=__builtin_popcountll(mask);
-        for(ll i=0;i<n;++i)
-        {
-            if(bit(mask,i))
-            {
-                ll nmask=(mask^(1<<i));
-                for(ll j=0;j<m;++j)
-                {
-                    dp[mask][(j+(s[i]-'0')*mult[cnt-1])%m]+=dp[nmask][j];
-                }
-            }
-        }
-    }
-    ans=dp[(1<<n)-1][0];
-    for(auto &v:freq)
-    {
-        assert(ans%fact[v]==0);
-        ans/=fact[v];
-    }
-    cout<<ans<<'\n';
     return 0;
 }
