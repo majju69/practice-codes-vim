c0f87bc6e986db2fa7ad173930c0d40c22f96ef4
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-14 04:21:16 +0530

    https://codeforces.com/contest/580/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,52 +7,78 @@ using namespace std;
     #define debug(x)
 #endif
 
+typedef long long ll;
+
+inline bool isSetBit(ll a,ll i)
+{
+    return ((a&(1LL<<i))!=0);
+}
+
+inline ll switchBit(ll a,ll i)
+{
+    return (a^(1LL<<i));
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    long long u,v;
-    cin>>u>>v;
-    if(u>v)
+    ll n,m,k,ans=0;
+    cin>>n>>m>>k;
+    vector<ll> a(n);
+    vector<vector<ll>> dp((1<<n),vector<ll>(n,0)),rules(n,vector<ll>(n,0));
+    for(auto &v:a)
     {
-        cout<<-1<<'\n';
+        cin>>v;
     }
-    else
+    for(ll i=0;i<k;++i)
     {
-        long long diff=v-u;
-        if(diff&1)
+        ll x,y,c;
+        cin>>x>>y>>c;
+        x--;
+        y--;
+        rules[x][y]=c;
+    }
+    for(ll i=0;i<n;++i)
+    {
+        dp[(1<<i)][i]=a[i];
+    }
+    for(ll mask=1;mask<(1<<n);++mask)
+    {
+        if(__builtin_popcount(mask)==1)
         {
-            cout<<-1<<'\n';
+            continue;
         }
-        else
+        for(ll i=0;i<n;++i)
         {
-            vector<long long> ans;
-            if(u!=0)
+            if(isSetBit(mask,i))
             {
-                ans.push_back(u);
-            }
-            if(diff!=0)
-            {
-                ans.push_back(diff>>1);
-                ans.push_back(diff>>1);
-            }
-            if((long long)ans.size()>2)
-            {
-                sort(ans.begin(),ans.end());
-                if((ans[0]&ans[2])==0)
+                ll x=switchBit(mask,i),cur=dp[mask][i];
+                for(ll j=0;j<n;++j)
                 {
-                    ans[0]+=ans[2];
-                    ans.pop_back();
+                    if(isSetBit(x,j))
+                    {
+                        cur=max(cur,dp[x][j]+a[i]+rules[j][i]);
+                    }
+                }
+                dp[mask][i]=cur;
+            }
+        }
+    }
+    for(ll i=1;i<(1<<n);++i)
+    {
+        if(__builtin_popcount(i)==m)
+        {
+            for(ll j=0;j<n;++j)
+            {
+                if(isSetBit(i,j))
+                {
+                    ans=max(ans,dp[i][j]);
                 }
             }
-            cout<<(long long)ans.size()<<'\n';
-            for(auto &v:ans)
-            {
-                cout<<v<<' ';
-            }
-            cout<<'\n';
         }
     }
+    cout<<ans<<'\n';
     return 0;
 }
