17c80b20505fd8737b1cdb270252659aebd82e70
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-16 17:28:45 +0530

    https://codeforces.com/contest/1437/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,6 +7,39 @@ using namespace std;
     #define debug(x)
 #endif
 
+int dp[201][402];
+
+int solve(int i,int j,vector<int> &a)
+{
+    if(i>=(int)a.size())
+    {
+        return 0;
+    }
+    if(j>2*(int)a.size())
+    {
+        return 1e9;
+    }
+    if(dp[i][j]!=-1)
+    {
+        return dp[i][j];
+    }
+    return dp[i][j]=min(solve(i,j+1,a),solve(i+1,j+1,a)+abs(j-a[i]));
+}
+
+int minTime(vector<int> &a)
+{
+    int n=a.size();
+    sort(a.begin(),a.end());
+    for(int i=0;i<=n;++i)
+    {
+        for(int j=0;j<=2*n+1;++j)
+        {
+            dp[i][j]=-1;
+        }
+    }
+    return solve(0,1,a);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -16,24 +49,14 @@ int main()
     cin>>tc;
     while(tc--)
     {
-        int n,pvs=-1,a0=-1,mn_val=0;
+        int n;
         cin>>n;
-        while(n--)
+        vector<int> a(n);
+        for(auto &v:a)
         {
-            if(pvs==-1)
-            {
-                cin>>pvs;
-                a0=pvs;
-            }
-            else
-            {
-                int x;
-                cin>>x;
-                mn_val+=max(0,pvs-x);
-                pvs=x;
-            }
+            cin>>v;
         }
-        cout<<((a0<mn_val)?"NO":"YES")<<'\n';
+        cout<<minTime(a)<<'\n';
     }
     return 0;
 }
