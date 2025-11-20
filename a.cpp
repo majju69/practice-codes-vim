02a5f13a2152f4738cc7e1c78a6b7031f274aad2
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-20 06:39:18 +0530

    https://codeforces.com/contest/1076/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,38 +7,105 @@ using namespace std;
 	#define debug(x)
 #endif
 
-int gcd(int a,int b)
-{
-	return ((b==0)?a:gcd(b,a%b));
-}
+typedef long long ll;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n,k,g=0;
-	cin>>n>>k;
-	while(n--)
+	ll n,m,k;
+	set<pair<ll,ll>> st;
+	map<pair<ll,ll>,ll> mp;
+	set<ll> ans;
+	cin>>n>>m>>k;
+	vector<pair<ll,ll>> *adj=new vector<pair<ll,ll>>[n];
+	ll *d1=new ll[n],*d2=new ll[n];
+	for(int i=0;i<m;++i)
 	{
-		int x;
-		cin>>x;
-		x%=k;
-		g=gcd(g,x);
-	}
-	g=gcd(g,k);
-	if(g==0)
-	{
-		cout<<1<<'\n'<<0<<'\n';
-	}
-	else
-	{
-		cout<<k/g<<'\n';
-		for(int i=0;i<k;i+=g)
+		ll u,v,w;
+		cin>>u>>v>>w;
+		u--;
+		v--;
+		if(u>v)
 		{
-			cout<<i<<' ';
+			swap(u,v);
 		}
-		cout<<'\n';
+		adj[u].push_back({v,w});
+		adj[v].push_back({u,w});
+		mp[{u,v}]=i;
 	}
+	fill(d1,d1+n,(ll)1e18);
+	fill(d2,d2+n,(ll)1e18);
+	st.insert({0,0});
+	d1[0]=0;
+	while((ll)st.size())
+	{
+		auto it=st.begin();
+		ll node=it->second,dist=it->first;
+		st.erase(*it);
+		for(auto &v:adj[node])
+		{
+			ll curNode=v.first,curDist=v.second;
+			if(d1[curNode]>dist+curDist)
+			{
+				if(d1[curNode]!=(ll)1e18)
+				{
+					st.erase({d1[curNode],curNode});
+				}
+				d1[curNode]=curDist+dist;
+				st.insert({d1[curNode],curNode});
+			}
+		}
+	}
+	st.insert({0,0});
+	d2[0]=0;
+	while((ll)st.size())
+	{
+		if(k<=0)
+		{
+			st.clear();
+			break;
+		}
+		auto it=st.begin();
+		ll node=it->second,dist=it->first;
+		st.erase(*it);
+		for(auto &v:adj[node])
+		{
+			if(k<=0)
+			{
+				break;
+			}
+			ll curNode=v.first,curDist=v.second;
+			if(d2[curNode]>dist+curDist)
+			{
+				if(d1[curNode]!=(ll)1e18)
+				{
+					st.erase({d2[curNode],curNode});
+				}
+				d2[curNode]=curDist+dist;
+				st.insert({d2[curNode],curNode});
+				if(d2[curNode]==d1[curNode])
+				{
+					ll u=curNode,v=node;
+					if(u>v)
+					{
+						swap(u,v);
+					}
+					ans.insert(mp[{u,v}]);
+					k--;
+				}
+			}
+		}
+	}
+	cout<<(ll)ans.size()<<'\n';
+	for(auto &v:ans)
+	{
+		cout<<v+1<<' ';
+	}
+	cout<<'\n';
+	delete []adj;
+	delete []d1;
+	delete []d2;
 	return 0;
 }
