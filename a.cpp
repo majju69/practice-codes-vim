206db5e19cc083d573e9e9c31f5d0978ce7f7d07
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-24 06:01:04 +0530

    https://codeforces.com/contest/2094/problem/H accepted

--- a.cpp
+++ a.cpp
@@ -2,110 +2,92 @@
 using namespace std;
 
 #ifdef LOCAL
-	#include"debug.h"
+    #include"debug.h"
 #else
-	#define debug(x)
+    #define debug(x)
 #endif
 
-typedef long long ll;
+vector<vector<int>> divisors(100001);
+vector<vector<int>> idx(100001);
+
+void fillDivisors()
+{
+    int n=divisors.size();
+    for(int i=2;i<n;i++)
+    {
+        for(int j=i;j<n;j+=i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ll n,m,k;
-	set<pair<ll,ll>> st;
-	map<pair<ll,ll>,ll> mp;
-	set<ll> ans;
-	cin>>n>>m>>k;
-	vector<pair<ll,ll>> *adj=new vector<pair<ll,ll>>[n];
-	ll *d1=new ll[n],*d2=new ll[n];
-	for(int i=0;i<m;++i)
-	{
-		ll u,v,w;
-		cin>>u>>v>>w;
-		u--;
-		v--;
-		if(u>v)
-		{
-			swap(u,v);
-		}
-		adj[u].push_back({v,w});
-		adj[v].push_back({u,w});
-		mp[{u,v}]=i;
-	}
-	fill(d1,d1+n,(ll)1e18);
-	fill(d2,d2+n,(ll)1e18);
-	st.insert({0,0});
-	d1[0]=0;
-	while((ll)st.size())
-	{
-		auto it=st.begin();
-		ll node=it->second,dist=it->first;
-		st.erase(*it);
-		for(auto &v:adj[node])
-		{
-			ll curNode=v.first,curDist=v.second;
-			if(d1[curNode]>dist+curDist)
-			{
-				if(d1[curNode]!=(ll)1e18)
-				{
-					st.erase({d1[curNode],curNode});
-				}
-				d1[curNode]=curDist+dist;
-				st.insert({d1[curNode],curNode});
-			}
-		}
-	}
-	st.insert({0,0});
-	d2[0]=0;
-	while((ll)st.size())
-	{
-		if(k<=0)
-		{
-			st.clear();
-			break;
-		}
-		auto it=st.begin();
-		ll node=it->second,dist=it->first;
-		st.erase(*it);
-		for(auto &v:adj[node])
-		{
-			if(k<=0)
-			{
-				break;
-			}
-			ll curNode=v.first,curDist=v.second;
-			if(d2[curNode]>dist+curDist)
-			{
-				if(d1[curNode]!=(ll)1e18)
-				{
-					st.erase({d2[curNode],curNode});
-				}
-				d2[curNode]=curDist+dist;
-				st.insert({d2[curNode],curNode});
-				if(d2[curNode]==d1[curNode])
-				{
-					ll u=curNode,v=node;
-					if(u>v)
-					{
-						swap(u,v);
-					}
-					ans.insert(mp[{u,v}]);
-					k--;
-				}
-			}
-		}
-	}
-	cout<<(ll)ans.size()<<'\n';
-	for(auto &v:ans)
-	{
-		cout<<v+1<<' ';
-	}
-	cout<<'\n';
-	delete []adj;
-	delete []d1;
-	delete []d2;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    fillDivisors();
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        int n,q;
+        cin>>n>>q;
+        vector<int> a(n);
+        for(int i=0;i<n;++i)
+        {
+            cin>>a[i];
+            idx[a[i]].push_back(i);
+        }
+        while(q--)
+        {
+            int k,l,r;
+            cin>>k>>l>>r;
+            if(k==1)
+            {
+                cout<<r-l+1<<'\n';
+            }
+            else
+            {
+                l--;
+                r--;
+                int cur=l;
+                long long ans=0;
+                while(cur<=r)
+                {
+                    if(k%a[cur]==0)
+                    {
+                        while(k%a[cur]==0)
+                        {
+                            k/=a[cur];
+                        }
+                        ans+=1ll*k;
+                        cur++;
+                        continue;
+                    }
+                    int nxt=r+1;
+                    for(auto &v:divisors[k])
+                    {
+                        int _idx=lower_bound(idx[v].begin(),idx[v].end(),cur)-idx[v].begin();
+                        if(_idx<(int)idx[v].size())
+                        {
+                            if(idx[v][_idx]<=r)
+                            {
+                                nxt=min(nxt,idx[v][_idx]);
+                            }
+                        }
+                    }
+                    ans+=1ll*(nxt-cur)*k;
+                    cur=nxt;
+                }
+                cout<<ans<<'\n';
+            }
+        }
+        for(auto &v:a)
+        {
+            idx[v].clear();
+        }
+    }
+    return 0;
 }
