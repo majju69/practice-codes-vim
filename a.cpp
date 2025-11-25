d74dd86a5b17f75bd1a6f9a36bdf1e94e22c9fab
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-26 02:10:36 +0530

    https://codeforces.com/contest/2167/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -9,48 +9,25 @@ using namespace std;
 
 typedef long long ll;
 
-ll minCost(vector<ll> &a,vector<ll> &c)
+deque<pair<ll,ll>> mergeOverlap(deque<pair<ll,ll>> &a)
 {
-    ll n=a.size();
-    if(n==1)
+    sort(a.begin(),a.end());
+    deque<pair<ll,ll>> res;
+    res.push_back(a[0]);
+    for(int i=1;i<a.size();++i)
     {
-        return 0;
-    }
-    ll ind=0;
-    map<ll,ll> mp;
-    for(auto &v:a)
-    {
-        mp[v]=0;
-    }
-    for(auto &v:mp)
-    {
-        v.second=ind++;
-    }
-    for(auto &v:a)
-    {
-        v=mp[v];
-    }
-    vector<ll> dp(ind+1,c[0]);
-    dp[a[0]]=0;
-    for(ll i=1;i<n;++i)
-    {
-        vector<ll> ndp(ind+1,1e18);
-        ll mn=1e18;
-        for(ll j=0;j<ind;++j)
+        pair<ll,ll> &last=res.back();
+        pair<ll,ll> &curr=a[i];
+        if(curr.first<=last.second)
         {
-            mn=min(mn,dp[j]);
-            if(j==a[i])
-            {
-                ndp[j]=mn;
-            }
-            else
-            {
-                ndp[j]=mn+c[i];
-            }
+            last.second=max(last.second,curr.second);
+        } 
+        else 
+        {
+            res.push_back(curr);
         }
-        dp=ndp;
     }
-    return *min_element(dp.begin(),dp.end());
+    return res;
 }
 
 int main()
@@ -62,18 +39,69 @@ int main()
     cin>>tc;
     while(tc--)
     {
-        ll n;
-        cin>>n;
-        vector<ll> a(n),c(n);
+        ll n,k,x,lo=0,hi=1e9,cur=0,ans=-1;
+        deque<pair<ll,ll>> finalRes;
+        cin>>n>>k>>x;
+        vector<ll> a(n);
         for(auto &v:a)
         {
             cin>>v;
         }
-        for(auto &v:c)
+        while(lo<=hi)
         {
-            cin>>v;
+            ll mid=lo+(hi-lo)/2,cnt=0;
+            deque<pair<ll,ll>> bad,res;
+            for(auto &v:a)
+            {
+                ll l=v-mid+1,r=v+mid-1;
+                if(l<0)
+                {
+                    l=0;
+                }
+                if(r>x)
+                {
+                    r=x;
+                }
+                if(l<=r)
+                {
+                    bad.push_back({l,r});
+                }
+            }
+            if((ll)bad.size()>0)
+            {
+                res=mergeOverlap(bad);
+                for(auto &v:res)
+                {
+                    cnt+=(v.second-v.first+1);
+                }
+            }
+            if(x+1-cnt>=k)
+            {
+                ans=mid;
+                finalRes=res;
+                lo=mid+1;
+            }
+            else
+            {
+                hi=mid-1;
+            }
         }
-        cout<<minCost(a,c)<<'\n';
+        sort(finalRes.begin(),finalRes.end());
+        while(k>0)
+        {
+            if((ll)finalRes.size()>0&&cur==finalRes[0].first)
+            {
+                cur=finalRes[0].second+1;
+                finalRes.pop_front();
+            }
+            else
+            {
+                cout<<cur<<' ';
+                cur++;
+                k--;
+            }
+        }
+        cout<<'\n';
     }
     return 0;
 }
