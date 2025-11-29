44b1e99eae7a9624b5f8d35d1c048e02fa4b54c7
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-11-29 09:00:32 +0530

    https://codeforces.com/contest/1627/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,27 +7,9 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-
-deque<pair<ll,ll>> mergeOverlap(deque<pair<ll,ll>> &a)
+int gcd(int a,int b)
 {
-    sort(a.begin(),a.end());
-    deque<pair<ll,ll>> res;
-    res.push_back(a[0]);
-    for(int i=1;i<a.size();++i)
-    {
-        pair<ll,ll> &last=res.back();
-        pair<ll,ll> &curr=a[i];
-        if(curr.first<=last.second)
-        {
-            last.second=max(last.second,curr.second);
-        } 
-        else 
-        {
-            res.push_back(curr);
-        }
-    }
-    return res;
+    return ((b==0)?a:gcd(b,a%b));
 }
 
 int main()
@@ -35,73 +17,45 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll tc;
-    cin>>tc;
-    while(tc--)
+    int n,g=0,mx=-1,ans=0;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &v:a)
     {
-        ll n,k,x,lo=0,hi=1e9,cur=0,ans=-1;
-        deque<pair<ll,ll>> finalRes;
-        cin>>n>>k>>x;
-        vector<ll> a(n);
-        for(auto &v:a)
-        {
-            cin>>v;
-        }
-        while(lo<=hi)
-        {
-            ll mid=lo+(hi-lo)/2,cnt=0;
-            deque<pair<ll,ll>> bad,res;
-            for(auto &v:a)
-            {
-                ll l=v-mid+1,r=v+mid-1;
-                if(l<0)
-                {
-                    l=0;
-                }
-                if(r>x)
-                {
-                    r=x;
-                }
-                if(l<=r)
-                {
-                    bad.push_back({l,r});
-                }
-            }
-            if((ll)bad.size()>0)
-            {
-                res=mergeOverlap(bad);
-                for(auto &v:res)
-                {
-                    cnt+=(v.second-v.first+1);
-                }
-            }
-            if(x+1-cnt>=k)
-            {
-                ans=mid;
-                finalRes=res;
-                lo=mid+1;
-            }
-            else
-            {
-                hi=mid-1;
-            }
-        }
-        sort(finalRes.begin(),finalRes.end());
-        while(k>0)
-        {
-            if((ll)finalRes.size()>0&&cur==finalRes[0].first)
-            {
-                cur=finalRes[0].second+1;
-                finalRes.pop_front();
-            }
-            else
-            {
-                cout<<cur<<' ';
-                cur++;
-                k--;
-            }
-        }
-        cout<<'\n';
+        cin>>v;
+        g=gcd(g,v);
     }
+    for(auto &v:a)
+    {
+        v/=g;
+        mx=max(mx,v);
+    }
+    vector<bool> present(mx+1,0);
+    for(auto &v:a)
+    {
+        present[v]=1;
+    }
+    for(int i=mx;i>=1;--i)
+    {
+        if(present[i])
+        {
+            continue;
+        }
+        int cnt=1,g=0;
+        for(int j=(i<<1);j<=mx;j+=i)
+        {
+            cnt++;
+            if(present[j])
+            {
+                g=gcd(cnt,g);
+            }
+        }
+        if(g==1)
+        {
+            ans++;
+            present[i]=i;
+        }
+    }
+    cout<<ans<<'\n';
     return 0;
 }
