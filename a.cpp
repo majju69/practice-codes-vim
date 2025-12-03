c55559af37ee104ca08e965f821dd2bda1ae6efe
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-03 15:22:15 +0530

    https://codeforces.com/contest/1206/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,56 +7,40 @@ using namespace std;
     #define debug(x)
 #endif
 
-int bfs(int s,int t,vector<int> &par,vector<int> adj[],vector<vector<int>> &capacity)
+inline bool bit(long long a,int i)
 {
-    fill(par.begin(),par.end(),-1);
-    par[s]=-2;
-    queue<pair<int,int>> q;
-    q.push({s,1e9});
+    return (a>>i&1);
+}
+
+int bfs(int src,int n,vector<int> adj[])
+{
+    int mn=1e9;
+    vector<int> dist(n,1e9),par(n,-1);
+    queue<int> q;
+    dist[src]=0;
+    q.push(src);
     while(q.size())
     {
-        int node=q.front().first,flow=q.front().second;
+        int node=q.front();
         q.pop();
-        for (auto &v:adj[node])
+        for(auto &v:adj[node])
         {
-            if(par[v]==-1&&capacity[node][v])
+            if(dist[v]>1+dist[node])
             {
+                dist[v]=1+dist[node];
+                q.push(v);
                 par[v]=node;
-                int new_flow=min(flow,capacity[node][v]);
-                if(v==t)
+            }
+            else
+            {
+                if(par[node]!=v&&par[v]!=node)
                 {
-                    return new_flow;
+                    mn=min(mn,dist[node]+dist[v]+1);
                 }
-                q.push({v,new_flow});
             }
         }
     }
-
-    return 0;
-}
-
-int maxflow(int s,int t,int n,vector<int> adj[],vector<vector<int>> &capacity)
-{
-    int flow=0,new_flow=-1;
-    vector<int> par(n);
-    while(1)
-    {
-        new_flow=bfs(s,t,par,adj,capacity);
-        if(new_flow==0)
-        {
-            break;
-        }
-        flow+=new_flow;
-        int cur=t;
-        while(cur!=s)
-        {
-            int pvs=par[cur];
-            capacity[pvs][cur]-=new_flow;
-            capacity[cur][pvs]+=new_flow;
-            cur=pvs;
-        }
-    }
-    return flow;
+    return mn;
 }
 
 int main()
@@ -64,97 +48,65 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n,m,ans=0;
-    set<int> primes;
-    cin>>n>>m;
-    vector<int> a(n),candy;
-    vector<pair<int,int>> edges(m);
-    for(auto &v:a)
+    int n;
+    cin>>n;
+    bool three=0;
+    vector<long long> a;
+    vector<vector<int>> edges(60);
+    for(int i=0;i<n;++i)
     {
-        cin>>v;
-        int x=v;
-        if(!(x&1))
+        long long x;
+        cin>>x;
+        if(x!=0)
         {
-            primes.insert(2);
-            while(!(x&1))
-            {
-                x>>=1;
-            }
+            a.push_back(x);
         }
-        for(int i=3;i*i<=x;i+=2)
+    }
+    n=a.size();
+    for(int i=0;i<n;++i)
+    {
+        if(three)
         {
-            if(x%i==0)
+            break;
+        }
+        for(int j=0;j<60;++j)
+        {
+            if(bit(a[i],j))
             {
-                primes.insert(i);
-                while(x%i==0)
+                edges[j].push_back(i);
+                if((int)edges[j].size()>=3)
                 {
-                    x/=i;
+                    three=1;
+                    break;
                 }
             }
         }
-        if(x>2)
-        {
-            primes.insert(x);
-        }
     }
-    for(auto &v:primes)
+    if(three)
     {
-        candy.push_back(v);
+        cout<<3<<'\n';
     }
-    for(auto &edge:edges)
+    else
     {
-        cin>>edge.first>>edge.second;
-        if(edge.second&1)
+        int mn=1e9;
+        vector<int> adj[n];
+        for(auto &vec:edges)
         {
-            swap(edge.first,edge.second);
-        }
-    }
-    for(auto &p:candy)
-    {
-        vector<int> adj[n+2];
-        vector<vector<int>> capacity(n+2,vector<int>(n+2,0));
-        for(int i=1;i<=n;++i)
-        {
-            if(i&1)
+            if((int)vec.size()>1)
             {
-                adj[0].push_back(i);
-                adj[i].push_back(0);
-                int x=a[i-1],cnt=0;
-                while(x%p==0)
-                {
-                    x/=p;
-                    cnt++;
-                }
-                capacity[0][i]=cnt;
-            }
-            else
-            {
-                adj[n+1].push_back(i);
-                adj[i].push_back(n+1);
-                int x=a[i-1],cnt=0;
-                while(x%p==0)
-                {
-                    x/=p;
-                    cnt++;
-                }
-                capacity[i][n+1]=cnt;
+                adj[vec[0]].push_back(vec[1]);
+                adj[vec[1]].push_back(vec[0]);
             }
         }
-        for(auto &edge:edges)
+        for(int i=0;i<n;++i)
         {
-            int x=a[edge.first-1],y=a[edge.second-1],cnt=0;
-            while(x%p==0&&y%p==0)
-            {
-                cnt++;
-                x/=p;
-                y/=p;
-            }
-            adj[edge.first].push_back(edge.second);
-            adj[edge.second].push_back(edge.first);
-            capacity[edge.first][edge.second]=cnt;
+            mn=min(mn,bfs(i,n,adj));
         }
-        ans+=maxflow(0,n+1,n+2,adj,capacity);
+        if(mn==(int)1e9)
+        {
+            mn=-1;
+        }
+        cout<<mn<<'\n';
     }
-    cout<<ans<<'\n';
     return 0;
 }
