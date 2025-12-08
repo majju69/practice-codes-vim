8ebe24e150d088c7c37827da7b66a9161a4c192e
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-08 10:22:50 +0530

    https://codeforces.com/contest/1999/problem/G2 accepted

--- a.cpp
+++ a.cpp
@@ -2,64 +2,62 @@
 using namespace std;
 
 #ifdef LOCAL
-    #include"debug.h"
+	#include"debug.h"
 #else
-    #define debug(x)
+	#define debug(x)
 #endif
 
-const int mod=998244353;
-
-inline int add(int a,int b)
+inline int ask(int a,int b)
 {
-    return ((a%mod)+(b%mod))%mod;
+	cout<<"? "<<a<<' '<<b<<endl;
+	int x;
+	cin>>x;
+	if(x==-1)
+	{
+		exit(1);
+	}
+	return x;
 }
 
-inline int mul(int a,int b)
+inline void reply(int x)
 {
-    return (1ll*(a%mod)*(b%mod))%mod;
+	cout<<"! "<<x<<endl;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n,ans=0;
-    cin>>n;
-    vector<int> a(n),cnt(2,0),sum(2,0),_cnt(2,0),_sum(2,0);
-    for(auto &v:a)
-    {
-        cin>>v;
-    }
-    for(int i=0;i<30;++i)
-    {
-        int cur_ans=0;
-        cnt[0]=cnt[1]=0;
-        sum[0]=sum[1]=0;
-        for(auto &v:a)
-        {
-            bool bit=(v>>i&1);
-            if(!bit)
-            {
-                _cnt[0]=cnt[0]+1;
-                _cnt[1]=cnt[1];
-                _sum[0]=add(1,add(sum[0],cnt[0]));
-                _sum[1]=add(sum[1],cnt[1]);
-            }
-            else
-            {
-                _cnt[0]=cnt[1];
-                _cnt[1]=cnt[0]+1;
-                _sum[0]=add(sum[1],cnt[1]);
-                _sum[1]=add(1,add(sum[0],cnt[0]));
-            }
-            cnt=_cnt;
-            sum=_sum;
-            cur_ans=add(cur_ans,sum[1]);
-        }
-        cur_ans=mul(cur_ans,(1<<i));
-        ans=add(ans,cur_ans);
-    }
-    cout<<ans<<'\n';
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int tc;
+	cin>>tc;
+	while(tc--)
+	{
+		int lo=2,hi=999;
+		while(1)
+		{
+			if(lo==hi)
+			{
+				reply(lo);
+				break;
+			}
+			int mid1=lo+(hi-lo)/3;
+			int mid2=lo+2*(hi-lo)/3;
+			int cur=ask(mid1,mid2);
+			if(cur==mid1*mid2)
+			{
+				lo=mid2+1;
+			}
+			else if(cur==mid1*(mid2+1))
+			{
+				lo=mid1+1;
+				hi=mid2;
+			}
+			else
+			{
+				hi=mid1;
+			}
+		}
+	}
+	return 0;
 }
