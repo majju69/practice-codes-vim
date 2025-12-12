e2d8b5af0aee80fd51b4a0c6eeb12e62cdbec1b9
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-12 17:44:42 +0530

    https://codeforces.com/contest/2124/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -2,62 +2,76 @@
 using namespace std;
 
 #ifdef LOCAL
-	#include"debug.h"
+    #include"debug.h"
 #else
-	#define debug(x)
+    #define debug(x)
 #endif
-
-inline int ask(int a,int b)
-{
-	cout<<"? "<<a<<' '<<b<<endl;
-	int x;
-	cin>>x;
-	if(x==-1)
-	{
-		exit(1);
-	}
-	return x;
-}
-
-inline void reply(int x)
-{
-	cout<<"! "<<x<<endl;
-}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int tc;
-	cin>>tc;
-	while(tc--)
-	{
-		int lo=2,hi=999;
-		while(1)
-		{
-			if(lo==hi)
-			{
-				reply(lo);
-				break;
-			}
-			int mid1=lo+(hi-lo)/3;
-			int mid2=lo+2*(hi-lo)/3;
-			int cur=ask(mid1,mid2);
-			if(cur==mid1*mid2)
-			{
-				lo=mid2+1;
-			}
-			else if(cur==mid1*(mid2+1))
-			{
-				lo=mid1+1;
-				hi=mid2;
-			}
-			else
-			{
-				hi=mid1;
-			}
-		}
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> a(n);
+        for(auto &v:a)
+        {
+            cin>>v;
+        }
+        if(k<=2)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            vector<int> _a=a,tmp;
+            sort(_a.begin(),_a.end());
+            for(auto &v:a)
+            {
+                if(v<=_a[k-2])
+                {
+                    tmp.push_back(v);
+                }
+            }
+            int rem=(int)tmp.size()-k+1,i=0,j=(int)tmp.size()-1;
+            bool ok=1;
+            while(i<j)
+            {
+                if(tmp[i]==tmp[j])
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    if(rem<=0)
+                    {
+                        ok=0;
+                        break;
+                    }
+                    if(tmp[i]==_a[k-2])
+                    {
+                        i++;
+                        rem--;
+                        continue;
+                    }
+                    if(tmp[j]==_a[k-2])
+                    {
+                        j--;
+                        rem--;
+                        continue;
+                    }
+                    ok=0;
+                    break;
+                }
+            }
+            cout<<(ok?"YES":"NO")<<'\n';
+        }
+    }
+    return 0;
 }
