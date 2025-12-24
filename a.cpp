9e7ebe700f3673e97cdec4b89358c0a771411dde
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-24 22:17:47 +0530

    https://codeforces.com/contest/1754/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -12,48 +12,25 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int tc;
-    cin>>tc;
-    while(tc--)
+    int n,x,cur_mul=0;
+    bool ok=1;
+    cin>>n>>x;
+    vector<int> freq(x+1,0);
+    for(int i=0;i<n;++i)
     {
-        int n,ans=0;
-        multiset<int> st;
-        cin>>n;
-        vector<int> adj[n],sub(n);
-        vector<vector<int>> dp(n,vector<int>(2));
-        for(int i=1;i<n;++i)
-        {
-            int u,v;
-            cin>>u>>v;
-            u--;
-            v--;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        for(int i=0;i<n;++i)
-        {
-            st.insert((int)adj[i].size());
-        }
-        for(int i=0;i<n;++i)
-        {
-            st.erase(st.find((int)adj[i].size()));
-            for(auto &v:adj[i])
-            {
-                ans=max(ans,(int)adj[v].size()+(int)adj[i].size()-2);
-                st.erase(st.find((int)adj[v].size()));
-            }
-            if((int)st.size()>0)
-            {
-                int x=(*(--st.end()));
-                ans=max(ans,(int)adj[i].size()+x-1);
-            }
-            st.insert((int)adj[i].size());
-            for(auto &v:adj[i])
-            {
-                st.insert((int)adj[v].size());
-            }
-        }
-        cout<<ans<<'\n';
+        int num;
+        cin>>num;
+        freq[num]++;
     }
+    for(int i=1;i<=x;++i)
+    {
+        if(cur_mul%i!=0)
+        {
+            ok=0;
+            break;
+        }
+        cur_mul=freq[i]+(cur_mul/i);
+    }
+    cout<<(ok?"Yes":"No")<<'\n';
     return 0;
 }
