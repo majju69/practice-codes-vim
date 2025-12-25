93a9ed9ce5ca3908085528271fbf712b121dd7a1
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-25 16:04:11 +0530

    https://codeforces.com/contest/1555/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -2,35 +2,51 @@
 using namespace std;
 
 #ifdef LOCAL
-    #include"debug.h"
+	#include"debug.h"
 #else
-    #define debug(x)
+	#define debug(x)
 #endif
+
+inline char get(int i)
+{
+	return (char)(i+'a');
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n,x,cur_mul=0;
-    bool ok=1;
-    cin>>n>>x;
-    vector<int> freq(x+1,0);
-    for(int i=0;i<n;++i)
-    {
-        int num;
-        cin>>num;
-        freq[num]++;
-    }
-    for(int i=1;i<=x;++i)
-    {
-        if(cur_mul%i!=0)
-        {
-            ok=0;
-            break;
-        }
-        cur_mul=freq[i]+(cur_mul/i);
-    }
-    cout<<(ok?"Yes":"No")<<'\n';
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,q,idx=0,perm[]={0,1,2};
+	string str;
+	cin>>n>>q>>str;
+	vector<int> cost[6];
+	do
+	{
+		int s=0,cur=0;
+		for(auto &v:str)
+		{
+			s+=(v!=get(perm[cur]));
+			cur++;
+			if(cur==3)
+			{
+				cur=0;
+			}
+			cost[idx].push_back(s);
+		}
+		idx++;
+	}while(next_permutation(perm,perm+3));
+	while(q--)
+	{
+		int l,r,ans=1e9;
+		cin>>l>>r;
+		l--;
+		r--;
+		for(int i=0;i<6;++i)
+		{
+			ans=min(ans,cost[i][r]-((l==0)?0:cost[i][l-1]));
+		}
+		cout<<ans<<'\n';
+	}
+	return 0;
 }
