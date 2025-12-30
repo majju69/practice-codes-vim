68e4485b1a8d7a7cf889a195dfaa4a3c9db973b6
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-12-30 08:41:03 +0530

    https://codeforces.com/contest/2182/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,103 +7,68 @@ using namespace std;
     #define debug(x)
 #endif
 
+typedef long long ll;
+
+bool cmp(const pair<ll,ll> &a,const pair<ll,ll> &b)
+{
+    if(a.first==b.first)
+    {
+        return a.second<b.second;
+    }
+    return a.first>b.first;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int tc;
+    ll tc;
     cin>>tc;
     while(tc--)
     {
-        int n,m;
-        cin>>n>>m;
-        vector<pair<int,int>> a(n);
-        for(int i=0;i<n;++i)
+        ll n,m,k,ans=0,sum=0;
+        multiset<ll> st;
+        vector<ll> res;
+        cin>>n>>m>>k;
+        for(ll i=0;i<m;++i)
         {
-            cin>>a[i].first;
-            a[i].second=i+1;
+            ll x;
+            cin>>x;
+            st.insert(x);
         }
-        sort(a.begin(),a.end());
-        if(m>(n>>1))
+        vector<pair<ll,ll>> a(n);
+        for(auto &v:a)
         {
-            cout<<-1<<'\n';
+            ll y;
+            cin>>v.second>>y>>v.first;
+            k-=y;
+            v.first-=y;
         }
-        else
+        sort(a.begin(),a.end(),cmp);
+        for(auto &v:a)
         {
-            if(m==0)
+            auto it=st.lower_bound(v.second);
+            if(it==st.end())
             {
-                int s=0,idx=-1;
-                for(int i=n-2;i>=0;--i)
-                {
-                    if(s+a[i].first>=a[n-1].first)
-                    {
-                        break;
-                    }
-                    s+=a[i].first;
-                    idx=i;
-                }
-                if(idx==0)
-                {
-                    if(s<a[n-1].first)
-                    {
-                        cout<<-1<<'\n';
-                    }
-                    else
-                    {
-                        cout<<n-1<<'\n';
-                        for(int i=0;i<n-1;++i)
-                        {
-                            cout<<a[i].second<<' '<<a[n-1].second<<'\n';
-                        }
-                    }
-                    continue;
-                }
-                vector<pair<int,int>> ans;
-                for(int i=idx;i<n-1;++i)
-                {
-                    ans.push_back({a[i].second,a[n-1].second});
-                }
-                for(int i=0;i<idx-1;++i)
-                {
-                    ans.push_back({a[i].second,a[i+1].second});
-                }
-                ans.push_back({a[n-1].second,a[idx-1].second});
-                cout<<(int)ans.size()<<'\n';
-                for(auto &v:ans)
-                {
-                    cout<<v.first<<' '<<v.second<<'\n';
-                }
-            }
-            else if(m==1)
-            {
-                cout<<n-1<<'\n';
-                for(int i=0;i<n-1;++i)
-                {
-                    cout<<a[i].second<<' '<<a[i+1].second<<'\n';
-                }
+                res.push_back(v.first);
             }
             else
             {
-                int idx=0,rem=n;
-                vector<pair<int,int>> ans;
-                while(rem>(m<<1))
-                {
-                    ans.push_back({a[idx].second,a[idx+1].second});
-                    idx++;
-                    rem--;
-                }
-                for(int i=idx;i<n;i+=2)
-                {
-                    ans.push_back({a[i+1].second,a[i].second});
-                }
-                cout<<(int)ans.size()<<'\n';
-                for(auto &v:ans)
-                {
-                    cout<<v.first<<' '<<v.second<<'\n';
-                }
+                ans++;
+                st.erase(it);
             }
         }
+        sort(res.begin(),res.end());
+        for(auto &v:res)
+        {
+            if(sum+v<=k)
+            {
+                ans++;
+                sum+=v;
+            }
+        }
+        cout<<ans<<'\n';
     }
     return 0;
 }
