c89f4a38881314dd8969a78135175cacfa651db4
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-02-03 23:56:13 +0530

    https://codeforces.com/contest/402/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,57 +7,76 @@ using namespace std;
     #define debug(x)
 #endif
 
+int gcd(int a,int b)
+{
+    return ((b==0)?a:gcd(b,a%b));
+}
+
+int get(int n,set<int> &bad)
+{
+    int ans=0,inc=(bad.count(2)?-1:1);
+    while((n&1)==0)
+    {
+        ans+=inc;
+        n>>=1;
+    }
+    for(int i=3;i*i<=n;i+=2)
+    {
+        if(n%i==0)
+        {
+            inc=(bad.count(i)?-1:1);
+            while(n%i==0)
+            {
+                n/=i;
+                ans+=inc;
+            }
+        }
+    }
+    if(n>2)
+    {
+        inc=(bad.count(n)?-1:1);
+        ans+=inc;
+    }
+    return ans;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int tc;
-    cin>>tc;
-    while(tc--)
+    int n,m,g=0;
+    set<int> bad;
+    cin>>n>>m;
+    vector<int> a(n),score[2];
+    vector<vector<int>> dp(n,vector<int>(n+1,-1e9));
+    for(auto &v:a)
     {
-        int n,ax,ay,bx,by,ans=0;
-        long long dp[]={0,0},ndp[]={-1,-1};
-        cin>>n>>ax>>ay>>bx>>by;
-        map<int,pair<int,int>> mp;
-        mp[ax]={ay,ay};
-        mp[bx]={by,by};
-        vector<int> x(n),y(n);
-        vector<pair<int,int>> a;
-        for(auto &v:x)
-        {
-            cin>>v;
-        }
-        for(auto &v:y)
-        {
-            cin>>v;
-        }
-        for(int i=0;i<n;++i)
-        {
-            if(!mp.count(x[i]))
-            {
-                mp[x[i]]={y[i],y[i]};
-            }
-            else
-            {
-                mp[x[i]].first=min(mp[x[i]].first,y[i]);
-                mp[x[i]].second=max(mp[x[i]].second,y[i]);
-            }
-        }
-        ans=bx-ax;
-        for(auto &v:mp)
-        {
-            a.push_back(v.second);
-        }
-        n=a.size();
-        for(int i=1;i<n;++i)
-        {
-            ndp[0]=min(dp[0]+abs(a[i-1].first-a[i].second),dp[1]+abs(a[i-1].second-a[i].second))+(a[i].second-a[i].first);
-            ndp[1]=min(dp[0]+abs(a[i-1].first-a[i].first),dp[1]+abs(a[i-1].second-a[i].first))+(a[i].second-a[i].first);
-            dp[0]=ndp[0];
-            dp[1]=ndp[1];
-        }
-        cout<<ans+min(dp[0],dp[1])<<'\n';
+        cin>>v;
     }
+    while(m--)
+    {
+        int x;
+        cin>>x;
+        bad.insert(x);
+    }
+    for(auto &v:a)
+    {
+        g=gcd(g,v);
+        score[0].push_back(get(v,bad));
+        score[1].push_back(get(g,bad));
+    }
+    dp[n-1][n-1]=score[0][n-1]-score[1][n-1];
+    dp[n-1][n]=score[0][n-1];
+    for(int i=n-2;i>=0;--i)
+    {
+        dp[i][i]=score[0][i]-score[1][i]+(*max_element(dp[i+1].begin(),dp[i+1].end()));
+        dp[i][n]=score[0][i]+dp[i+1][n];
+        for(int j=i+1;j<n;++j)
+        {
+            dp[i][j]=score[0][i]-score[1][j]+dp[i+1][j];
+        }
+    }
+    cout<<(*max_element(dp[0].begin(),dp[0].end()))<<'\n';
     return 0;
 }
