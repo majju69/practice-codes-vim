ecca810961e305507fec7d2a78073671e2a75702
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-02-17 02:05:20 +0530

    https://codeforces.com/contest/678/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,49 +7,77 @@ using namespace std;
     #define debug(x)
 #endif
 
+typedef long long ll;
+
+const ll mod=1e9+7;
+
+inline ll add(ll a,ll b)
+{
+    return ((a%mod)+(b%mod))%mod;
+}
+
+inline ll mul(ll a,ll b)
+{
+    return ((a%mod)*(b%mod))%mod;
+}
+
+inline ll sub(ll a,ll b)
+{
+    return ((a%mod)-(b%mod)+mod)%mod;
+}
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
+{
+    long long ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(1ll*(ans%mod)*(a%mod))%mod;
+        }
+        a=(1ll*(a%mod)*(a%mod))%mod;       
+        b>>=1;
+    }
+    return ans%mod;
+}
+
+inline ll divide(ll a,ll b)
+{
+    return mul(a,power(b,mod-2));
+}
+
+inline ll sum_gp(ll a,ll n,ll r)
+{
+    if(r==1)
+    {
+        return mul(n,a);
+    }
+    return mul(a,divide(sub(power(r,n),1),sub(r,1)));
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n,k,mn=0,mx=0;
-    cin>>n>>k;
-    vector<int> value(n),cost(n);
-    for(auto &v:value)
-    {
-        cin>>v;
-    }
-    for(int i=0;i<n;++i)
-    {
-        cin>>cost[i];
-        cost[i]=value[i]-k*cost[i];
-        if(cost[i]<0)
-        {
-            mn+=cost[i];
-        }
-        else
-        {
-            mx+=cost[i];
-        }
-    }
-    vector<int> dp(mx-mn+1,-1e9),ndp(mx-mn+1,-1e9);
-    dp[-mn]=0;
-    for(int i=0;i<n;++i)
-    {
-        ndp=dp;
-        for(int sum=mn;sum<=mx;++sum)
-        {
-            if(sum-cost[i]-mn>=0&&sum-cost[i]<=mx)
-            {
-                ndp[sum-mn]=max(ndp[sum-mn],dp[sum-cost[i]-mn]+value[i]);
-            }
-        }
-        dp=ndp;
-    }
-    if(dp[-mn]<=0||dp[-mn]>accumulate(value.begin(),value.end(),0))
-    {
-        dp[-mn]=-1;
-    }
-    cout<<dp[-mn]<<'\n';
+    ll a,b,n,x;
+    cin>>a>>b>>n>>x;
+    cout<<add(mul(power(a,n),x),sum_gp(b,n,a))<<'\n';
     return 0;
 }
+
+/*
+
+
+
+a(ax+b)+b = a^2x+b(1+a)
+
+
+a(a^2x+b(1+a))+b
+
+f^n(x) = a^n*x + b(1+a+a^2+..+a^(n-1))
+
+
+
+*/
