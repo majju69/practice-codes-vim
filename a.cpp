591b3fcd9bd8bf880402b886cc269cf22e553eec
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-02-20 14:14:43 +0530

    https://codeforces.com/contest/934/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -7,53 +7,52 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
+const int N=2e3+10;
+int a[N],pre[N],lis[N][N][2],lds[N][N][2];
 
-const ll mod=1e9+7;
-
-inline ll add(ll a,ll b)
+inline int get_lis0(int l,int r)
 {
-    return ((a%mod)+(b%mod))%mod;
-}
-
-inline ll mul(ll a,ll b)
-{
-    return ((a%mod)*(b%mod))%mod;
-}
-
-inline ll sub(ll a,ll b)
-{
-    return ((a%mod)-(b%mod)+mod)%mod;
-}
-
-long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
-{
-    long long ans=1;
-    a=a%mod;
-    while(b)
+    if(l>r)
     {
-        if(b&1)
-        {
-            ans=(1ll*(ans%mod)*(a%mod))%mod;
-        }
-        a=(1ll*(a%mod)*(a%mod))%mod;       
-        b>>=1;
+        return 0;
     }
-    return ans%mod;
+    return lis[l][r][0];
 }
 
-inline ll divide(ll a,ll b)
+inline int get_lis1(int l,int r)
 {
-    return mul(a,power(b,mod-2));
-}
-
-inline ll sum_gp(ll a,ll n,ll r)
-{
-    if(r==1)
+    if(l>r)
     {
-        return mul(n,a);
+        return 0;
     }
-    return mul(a,divide(sub(power(r,n),1),sub(r,1)));
+    return lis[l][r][1];
+}
+
+inline int get_lds0(int l,int r)
+{
+    if(l>r)
+    {
+        return 0;
+    }
+    return lds[l][r][0];
+}
+
+inline int get_lds1(int l,int r)
+{
+    if(l>r)
+    {
+        return 0;
+    }
+    return lds[l][r][1];
+}
+
+inline int get(int l,int r)
+{
+    if(l>r)
+    {
+        return 0;
+    }
+    return pre[r]-((l==0)?0:pre[l-1]);
 }
 
 int main()
@@ -61,23 +60,60 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll a,b,n,x;
-    cin>>a>>b>>n>>x;
-    cout<<add(mul(power(a,n),x),sum_gp(b,n,a))<<'\n';
+    int n,s=0,ans=0;
+    cin>>n;
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+        a[i]--;
+        s+=a[i];
+        pre[i]=s;
+    }
+    for(int i=0;i<n;++i)
+    {
+        int cnt[]={0,0};
+        for(int j=i;j<n;++j)
+        {
+            if(a[j]==0)
+            {
+                cnt[0]++;
+            }
+            else
+            {
+                cnt[1]=max(cnt[0],cnt[1])+1;
+            }
+            lis[i][j][0]=cnt[0];
+            lis[i][j][1]=cnt[1];
+        }
+    }
+    for(int i=0;i<n;++i)
+    {
+        int cnt[2][2]={{0,0},{0,0}};
+        for(int j=i;j<n;++j)
+        {
+            if(a[j]==0)
+            {
+                cnt[0][0]++;
+                cnt[1][0]=max(cnt[1][1],cnt[1][0])+1;
+            }
+            else
+            {
+                cnt[1][1]++;   
+            }
+            lds[i][j][0]=cnt[0][0];
+            lds[i][j][1]=max(cnt[1][0],cnt[1][1]);
+        }
+    }
+    for(int i=0;i<n;++i)    // 000 001 011 111
+    {
+        for(int j=i;j<n;++j)
+        {
+            ans=max(ans,get_lis0(0,i-1)+get_lds0(i,j)+get_lis0(j+1,n-1));
+            ans=max(ans,get_lis0(0,i-1)+get_lds0(i,j)+get_lis1(j+1,n-1));
+            ans=max(ans,get_lis0(0,i-1)+get_lds1(i,j)+get(j+1,n-1));
+            ans=max(ans,get_lis1(0,i-1)+get(i,j)+get(j+1,n-1));
+        }
+    }
+    cout<<ans<<'\n';
     return 0;
 }
-
-/*
-
-
-
-a(ax+b)+b = a^2x+b(1+a)
-
-
-a(a^2x+b(1+a))+b
-
-f^n(x) = a^n*x + b(1+a+a^2+..+a^(n-1))
-
-
-
-*/
