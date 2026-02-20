4aff1d3a1a0c1e6d5c1fc53b2526edfb951ee477
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-02-21 03:40:08 +0530

    https://codeforces.com/contest/894/problem/B accepted

--- a.cpp
+++ a.cpp
@@ -7,9 +7,27 @@ using namespace std;
     #define debug(x)
 #endif
 
-inline bool bit(long long a,int i)
+const long long mod=1e9+7;
+
+long long power(long long a,long long b)        // Use when mod is of order 10^9 or less
 {
-    return a>>i&1;
+    long long ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(1ll*(ans%mod)*(a%mod))%mod;
+        }
+        a=(1ll*(a%mod)*(a%mod))%mod;       
+        b>>=1;
+    }
+    return ans%mod;
+}
+
+inline long long get(const long long a,const long long b)
+{
+    return power(power(2,a),b);
 }
 
 int main()
@@ -17,33 +35,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    long long s,x;
-    cin>>s>>x;
-    if(s<x||((s-x)&1))
-    {
-        cout<<0<<'\n';
-    }
-    else
-    {
-        long long a=((s-x)>>1),ans=1;
-        for(int i=0;i<45;++i)
-        {
-            if(bit(x,i))
-            {
-                if(bit(a,i))
-                {
-                    ans=0;
-                    break;
-                }
-                ans<<=1;
-            }
-        }
-        if(s==x&&ans!=0)
-        {
-            ans-=2;
-            ans=max(ans,0ll);
-        }
-        cout<<ans<<'\n';
-    }
+    long long n,m,k;
+    cin>>n>>m>>k;
+    cout<<((k==-1&&(abs(n-m)&1))?0:get(n-1,m-1));
     return 0;
 }
