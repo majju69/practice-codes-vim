cf65bb9b6e02cd453d42518aa439e043b2181a1a
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-05 04:21:47 +0530

    https://codeforces.com/contest/1365/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,36 +7,85 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
+const int dx[]={-1,1,0,0},dy[]={0,0,-1,1};
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n,t,mn=1e18,ans=0;
-    cin>>n>>t;
-    vector<ll> a(n);
-    for(auto &v:a)
+    int tc;
+    cin>>tc;
+    while(tc--)
     {
-        cin>>v;
-        mn=min(mn,v);
-    }
-    while(t>=mn)
-    {
-        ll cur=t,sum=0,cnt=0;
+        int n,m;
+        bool ok=1;
+        queue<pair<int,int>> q;
+        cin>>n>>m;
+        vector<string> a(n);
+        vector<vector<bool>> vis(n,vector<bool>(m,0));
         for(auto &v:a)
         {
-            if(v<=cur)
+            cin>>v;
+        }
+        for(int x=0;x<n;++x)
+        {
+            for(int y=0;y<m;++y)
             {
-                cur-=v;
-                sum+=v;
-                cnt++;
+                if(a[x][y]=='.')
+                {
+                    bool mark=0;
+                    for(int i=0;i<4;++i)
+                    {
+                        int r=x+dx[i],c=y+dy[i];
+                        if(r>=0&&r<n&&c>=0&&c<m&&a[r][c]=='B')
+                        {
+                            mark=1;
+                            break;
+                        }
+                    }
+                    if(mark)
+                    {
+                        a[x][y]='#';
+                    }
+                }
             }
         }
-        ans+=(t/sum)*cnt;
-        t%=sum;
+        if(a[n-1][m-1]!='#')
+        {
+            q.push({n-1,m-1});
+            vis[n-1][m-1]=1;
+            while(!q.empty())
+            {
+                int x=q.front().first,y=q.front().second;
+                q.pop();
+                for(int i=0;i<4;++i)
+                {
+                    int r=x+dx[i],c=y+dy[i];
+                    if(r>=0&&r<n&&c>=0&&c<m&&!vis[r][c]&&a[r][c]!='#')
+                    {
+                        q.push({r,c});
+                        vis[r][c]=1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;++i)
+        {
+            if(!ok)
+            {
+                break;
+            }
+            for(int j=0;j<m;++j)
+            {
+                if((a[i][j]=='G'&&!vis[i][j])||(a[i][j]=='B'&&vis[i][j]))
+                {
+                    ok=0;
+                    break;
+                }
+            }
+        }
+        cout<<(ok?"Yes":"No")<<'\n';
     }
-    cout<<ans<<'\n';
     return 0;
 }
