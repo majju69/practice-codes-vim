ffc492c0dccfb5250b40f4b20a3cf77f701fd450
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-07 08:15:05 +0530

    https://cses.fi/problemset/task/1133/ accepted

--- a.cpp
+++ a.cpp
@@ -10,12 +10,12 @@ using namespace std;
 typedef long long ll;
 
 const ll N=2e5+10;
-ll a[N],sub[N],cost[N],dist[N];
+ll sub[N],cost[N],dist[N];
 vector<ll> adj[N];
 
 void dfs(ll node,ll p)
 {
-    sub[node]=a[node];
+    sub[node]=1;
     for(auto &v:adj[node])
     {
         if(v!=p)
@@ -27,15 +27,15 @@ void dfs(ll node,ll p)
     }
 }
 
-void dfs(ll node,ll p,ll sum)
+void dfs(ll node,ll p,ll n)
 {
     for(auto &v:adj[node])
     {
         if(v!=p)
         {
             ll s=sub[v];
-            cost[v]=cost[node]+sum-(s<<1);
-            dfs(v,node,sum);
+            cost[v]=cost[node]+n-(s<<1);
+            dfs(v,node,n);
         }
     }
 }
@@ -45,13 +45,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n,sum=0;
+    ll n;
     cin>>n;
-    for(ll i=0;i<n;++i)
-    {
-        cin>>a[i];
-        sum+=a[i];
-    }
     for(ll i=1;i<n;++i)
     {
         int u,v;
@@ -64,9 +59,13 @@ int main()
     dfs(0,-1);
     for(ll i=1;i<n;++i)
     {
-        cost[0]+=a[i]*dist[i];
+        cost[0]+=dist[i];
     }
-    dfs(0,-1,sum);
-    cout<<(*max_element(cost,cost+n))<<'\n';
+    dfs(0,-1,n);
+    for(ll i=0;i<n;++i)
+    {
+        cout<<cost[i]<<' ';
+    }
+    cout<<'\n';
     return 0;
 }
