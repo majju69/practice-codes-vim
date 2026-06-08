0bb6c4f65132909e5873163b7b75fcb22788a80e
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-08 06:14:28 +0530

    https://codeforces.com/contest/600/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,37 +7,76 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
-
-const ll N=2e5+10;
-ll sub[N],cost[N],dist[N];
-vector<ll> adj[N];
-
-void dfs(ll node,ll p)
+struct Query
 {
-    sub[node]=1;
-    for(auto &v:adj[node])
+    int idx,lt,rt,blk;
+};
+
+const int N=1e5+10,BLK=317;
+int a[N],freq[N],pre[N],post[N],val[N],timer=0;
+long long ans[N];
+Query qry[N];
+vector<int> adj[N];
+map<int,long long> mp;
+
+bool cmp(const Query &a,const Query &b)
+{
+    if(a.blk==b.blk)
     {
-        if(v!=p)
+        if(a.blk&1)
         {
-            dist[v]=dist[node]+1;
-            dfs(v,node);
-            sub[node]+=sub[v];
+            return a.rt<b.rt;
         }
+        return a.rt>b.rt;
+    }
+    return a.blk<b.blk;
+}
+
+void addElement(const int &pos)
+{
+    if(freq[val[pos]]!=0)
+    {
+        const int &key=freq[val[pos]];
+        long long &value=mp[key];
+        value-=(long long)val[pos];
+        if(value==0)
+        {
+            mp.erase(key);
+        }
+    }
+    freq[val[pos]]++;
+    mp[freq[val[pos]]]+=(long long)val[pos];
+}
+
+void removeElement(const int &pos)
+{
+    const int &key=freq[val[pos]];
+    long long &value=mp[key];
+    value-=(long long)val[pos];
+    if(value==0)
+    {
+        mp.erase(key);
+    }
+    freq[val[pos]]--;
+    if(freq[val[pos]]!=0)
+    {
+        mp[freq[val[pos]]]+=(long long)val[pos];
     }
 }
 
-void dfs(ll node,ll p,ll n)
+void dfs(int node,int p)
 {
+    pre[node]=timer;        
+    val[timer]=a[node];
     for(auto &v:adj[node])
     {
         if(v!=p)
         {
-            ll s=sub[v];
-            cost[v]=cost[node]+n-(s<<1);
-            dfs(v,node,n);
+            timer++;
+            dfs(v,node);
         }
     }
+    post[node]=timer;
 }
 
 int main()
@@ -45,9 +84,13 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll n;
+    int n,l=0,r=-1;
     cin>>n;
-    for(ll i=1;i<n;++i)
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+    }
+    for(int i=1;i<n;++i)
     {
         int u,v;
         cin>>u>>v;
@@ -57,14 +100,43 @@ int main()
         adj[v].push_back(u);
     }
     dfs(0,-1);
-    for(ll i=1;i<n;++i)
+    for(int i=0;i<n;++i)
     {
-        cost[0]+=dist[i];
+        qry[i].lt=pre[i];
+        qry[i].rt=post[i];
+        qry[i].blk=pre[i]/BLK;
+        qry[i].idx=i;
     }
-    dfs(0,-1,n);
-    for(ll i=0;i<n;++i)
+    sort(qry,qry+n,cmp);
+    for(int i=0;i<n;++i)
     {
-        cout<<cost[i]<<' ';
+        Query &v=qry[i];
+        while(v.lt<l)
+        {
+            l--;
+            addElement(l);
+        }
+        while(v.rt>r)
+        {
+            r++;
+            addElement(r);
+        }
+        while(v.lt>l)
+        {
+            removeElement(l);
+            l++;
+        }
+        while(v.rt<r)
+        {
+            removeElement(r);
+            r--;
+        }
+        pair<const int,long long> &p=(*(--mp.end()));
+        ans[v.idx]=p.second;
+    }
+    for(int i=0;i<n;++i)
+    {
+        cout<<ans[i]<<' ';
     }
     cout<<'\n';
     return 0;
