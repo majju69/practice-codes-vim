9a0fa3c6634eb19cc523a51bd820ee755524e185
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-18 17:24:09 +0530

    https://codeforces.com/contest/895/problem/C accepted

--- a.cpp
+++ a.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstring>
 using namespace std;
 
 #ifdef LOCAL
@@ -7,96 +8,101 @@ using namespace std;
     #define debug(x)
 #endif
 
-const int N=105,M=105,K=15;
-int dp[N][M][K],a[N][M];
+const int sz=19;
+const int MASK=(1<<sz),mod=1e9+7,primes[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67};
+int freq[MASK],dp[MASK],ndp[MASK];
+
+int add(const int &a,const int &b)
+{
+    return ((a%mod)+(b%mod))%mod;
+}
+
+int sub(const int &a,const int &b)
+{
+    return ((a%mod)-(b%mod)+mod)%mod;
+}
+
+int mul(const int &a,const int &b)
+{
+    return (1ll*(a%mod)*(b%mod))%mod;
+}
+
+int power(int a,int b)        // Use when mod is of order 10^9 or less
+{
+    int ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(1ll*(ans%mod)*(a%mod))%mod;
+        }
+        a=(1ll*(a%mod)*(a%mod))%mod;
+        b>>=1;
+    }
+    return ans%mod;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    memset(dp,-1,sizeof(dp));
-    int n,m,k,idx=-1,mx=-1;
-    cin>>n>>m>>k;
-    k++;
+    int n;
+    cin>>n;
+    vector<int> present;
+    vector<pair<int,int>> a;
     for(int i=0;i<n;++i)
     {
-        string s;
-        cin>>s;
-        for(int j=0;j<m;++j)
+        int x,mask=0;
+        cin>>x;
+        for(int j=0;j<sz;++j)
         {
-            a[i][j]=(s[j]-'0');
-        }
-    }
-    for(int i=n-1;i>=0;--i)
-    {
-        if(i==n-1)
-        {
-            for(int j=0;j<m;++j)
+            const int p=primes[j];
+            int cnt=0;
+            while(x%p==0)
             {
-                dp[i][j][a[i][j]%k]=a[i][j];
+                cnt=1-cnt;
+                x/=p;
+            }
+            if(cnt==1)
+            {
+                mask^=(1<<j);
             }
         }
-        else
+        if(mask==0)
         {
-            for(int j=0;j<m;++j)
+            freq[0]++;
+            continue;
+        }
+        if(freq[mask]==0)
+        {
+            present.push_back(mask);
+        }
+        freq[mask]++;
+    }
+    for(auto &v:present)
+    {
+        a.push_back({v,freq[v]});
+    }
+    if(!a.empty())
+    {
+        dp[a[0].first]=power(2,a[0].second-1);
+        dp[0]=sub(power(2,a[0].second-1),1);
+        for(int i=1;i<(int)a.size();++i)
+        {
+            memset(ndp,0,sizeof(ndp));
+            const int ways_odd=power(2,a[i].second-1),mask_odd=a[0].first,ways_even=sub(power(2,a[i].second-1),1),cur_mask=a[i].first;
+            ndp[cur_mask]=ways_odd;
+            ndp[0]=ways_even;
+            for(int mask=0;mask<MASK;++mask)
             {
-                if(j+1<m)
-                {
-                    for(int rem=0;rem<k;++rem)
-                    {
-                        if(dp[i+1][j+1][(rem-(a[i][j]%k)+k)%k]!=-1)
-                        {
-                            dp[i][j][rem]=max(dp[i][j][rem],dp[i+1][j+1][(rem-(a[i][j]%k)+k)%k]+a[i][j]);
-                        }
-                    }
-                }
-                if(j-1>=0)
-                {
-                    for(int rem=0;rem<k;++rem)
-                    {
-                        if(dp[i+1][j-1][(rem-(a[i][j]%k)+k)%k]!=-1)
-                        {
-                            dp[i][j][rem]=max(dp[i][j][rem],dp[i+1][j-1][(rem-(a[i][j]%k)+k)%k]+a[i][j]);
-                        }
-                    }
-
-                }
+                ndp[mask]=add(ndp[mask],add(dp[mask],mul(dp[mask],ways_even)));
+                ndp[mask]=add(ndp[mask],mul(dp[mask^a[i].first],ways_odd));
             }
+            memcpy(dp,ndp,sizeof(dp));
         }
     }
-    for(int j=0;j<m;++j)
-    {
-        if(dp[0][j][0]>mx)
-        {
-            mx=dp[0][j][0];
-            idx=j;
-        }
-    }
-    if(mx==-1)
-    {
-        cout<<-1<<'\n';
-    }
-    else
-    {
-        cout<<mx<<'\n';
-        int cur_idx=idx,cur_rem=0;
-        string ans;
-        for(int i=1;i<n;++i)
-        {
-            if(cur_idx+1<m&&dp[i][cur_idx+1][(cur_rem-(a[i-1][cur_idx])%k+k)%k]==dp[i-1][cur_idx][cur_rem]-a[i-1][cur_idx])
-            {
-                ans.push_back('L');
-                cur_rem=(cur_rem-(a[i-1][cur_idx])%k+k)%k;
-                cur_idx++;
-                continue;
-            }
-            ans.push_back('R');
-            cur_rem=(cur_rem-(a[i-1][cur_idx])%k+k)%k;
-            cur_idx--;
-        }
-        reverse(ans.begin(),ans.end());
-        cout<<cur_idx+1<<'\n'<<ans<<'\n';
-    }
+    cout<<sub(mul(add(1,dp[0]),power(2,freq[0])),1)<<'\n';    
     return 0;
 }
