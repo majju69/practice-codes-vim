4f6a183e609de3b4e67c525eb8427a3b7bb03755
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-27 20:13:49 +0530

    https://codeforces.com/contest/2210/problem/C2 accepted

--- a.cpp
+++ a.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,78 +7,94 @@ using namespace std;
     #define debug(x)
 #endif
 
-const int N=2010;
-int umbrella[N],dp[N][N];
-bool isRain[N];
+typedef long long ll;
+
+
+const ll N=5e4+10,M=18,primes[]={0,1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53};
+ll a[N],b[N],c[N],dp[N][M];
+
+ll gcd(const ll &a,const ll &b)
+{
+    return ((b==0)?a:gcd(b,a%b));
+}
+
+ll lcm(const ll &a,const ll &b)
+{
+    return (a/gcd(a,b))*b;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    memset(umbrella,-1,sizeof(umbrella));
-    memset(dp,0x3f,sizeof(dp));
-    int a,n,m,mn=1e9+10,ans=-1;
-    cin>>a>>n>>m;
-    for(int i=0;i<n;++i)
+    ll tc;
+    cin>>tc;
+    while(tc--)
     {
-        int l,r;
-        cin>>l>>r;
-        for(int j=l;j<r;++j)
+        ll n;
+        cin>>n;
+        for(ll i=0;i<n;++i)
         {
-            isRain[j]=1;
+            cin>>a[i];
         }
-    }
-    for(int i=0;i<m;++i)
-    {
-        int x,p;
-        cin>>x>>p;
-        if(umbrella[x+1]==-1)
+        for(ll i=0;i<n;++i)
         {
-            umbrella[x+1]=p;
+            cin>>b[i];
         }
-        else
+        for(ll i=0;i<n;++i)
         {
-            umbrella[x+1]=min(umbrella[x+1],p);
-        }
-    }
-    if(!isRain[0])
-    {
-        dp[0][0]=0;
-        mn=0;
-    }
-    if(umbrella[1]!=-1)
-    {
-        dp[0][1]=umbrella[1];
-        mn=min(mn,umbrella[1]);
-    }
-    for(int i=1;i<a;++i)
-    {
-        if(!isRain[i])
-        {
-            dp[i][0]=min(mn,dp[i-1][0]);
-        }
-        if(umbrella[i+1]!=-1)
-        {
-            dp[i][i+1]=mn+umbrella[i+1];
-        }
-        mn=1e9+10;
-        for(int j=1;j<=i;++j)
-        {
-            if(dp[i-1][j]<(int)1e9&&umbrella[j]!=-1)
+            ll left=1,right=1,l=-1;
+            if(i>0)
             {
-                dp[i][j]=min(dp[i][j],dp[i-1][j]+umbrella[j]);
-                mn=min(mn,dp[i][j]);
+                left=gcd(a[i],a[i-1]);
+            }
+            if(i+1<n)
+            {
+                right=gcd(a[i],a[i+1]);
+            }
+            l=lcm(left,right);
+            if(l<=b[i])
+            {
+                c[i]=l;
+            }
+            else
+            {
+                c[i]=a[i];
             }
         }
-        mn=min(mn,dp[i][i+1]);
-        mn=min(mn,dp[i][0]);
+        for(ll j=0;j<M;++j)
+        {
+            ll cur=((j!=0)?c[0]*primes[j]:a[0]);
+            ll score=(cur!=a[0]);
+            if(cur<=b[0]||cur==a[0])
+            {
+                dp[1][j]=score;
+            }
+        }
+        for(ll i=1;i<n;++i)
+        {
+            dp[i+1][0]=max(dp[i+1][0],dp[i][0]);
+            for(ll prev=0;prev<M;++prev)
+            {
+                for(ll curr=0;curr<M;++curr)
+                {
+                    ll pvs=((prev!=0)?primes[prev]*c[i-1]:a[i-1]),cur=((curr!=0)?c[i]*primes[curr]:a[i]);
+                    ll score=(cur!=a[i]);
+                    if(gcd(pvs,cur)==gcd(a[i-1],a[i])&&(pvs<=b[i-1]||pvs==a[i-1])&&(cur<=b[i]||cur==a[i]))
+                    {
+                        dp[i+1][curr]=max(dp[i+1][curr],dp[i][prev]+score);
+                    }
+                }
+            }
+        }
+        cout<<(*max_element(dp[n],dp[n]+M))<<'\n';
+        for(ll i=0;i<=n;++i)
+        {
+            a[i]=b[i]=c[i]=0;
+            memset(dp[i],0,sizeof(dp[i]));
+        }
     }
-    ans=(*min_element(dp[a-1],dp[a-1]+a+5));
-    if(ans>(int)2e8+10)
-    {
-        ans=-1;
-    }
-    cout<<ans<<'\n';
     return 0;
 }
+
