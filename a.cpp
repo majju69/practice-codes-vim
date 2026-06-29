9ae4e809a10bb81854cbda2d7687269662526ff9
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-06-29 23:34:34 +0530

    https://codeforces.com/contest/1797/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -9,18 +9,25 @@ using namespace std;
 
 typedef long long ll;
 
+const ll N=1e5+10;
+ll a[N],par[N],subSum[N],subSize[N];
+vector<ll> adj[N];
+multiset<array<ll,3>> st[N];
 
-const ll N=5e4+10,M=18,primes[]={0,1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53};
-ll a[N],b[N],c[N],dp[N][M];
-
-ll gcd(const ll &a,const ll &b)
+void dfs(ll node,ll p)
 {
-    return ((b==0)?a:gcd(b,a%b));
-}
-
-ll lcm(const ll &a,const ll &b)
-{
-    return (a/gcd(a,b))*b;
+    subSize[node]=1;
+    subSum[node]=a[node];
+    for(auto &v:adj[node])
+    {
+        if(v!=p)
+        {
+            par[v]=node;
+            dfs(v,node);
+            subSize[node]+=subSize[v];
+            subSum[node]+=subSum[v];
+        }
+    }
 }
 
 int main()
@@ -28,73 +35,55 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll tc;
-    cin>>tc;
-    while(tc--)
+    ll n,m;
+    cin>>n>>m;
+    for(ll i=0;i<n;++i)
     {
-        ll n;
-        cin>>n;
-        for(ll i=0;i<n;++i)
+        cin>>a[i];
+    }
+    for(int i=1;i<n;++i)
+    {
+        int u,v;
+        cin>>u>>v;
+        u--;
+        v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dfs(0,-1);
+    for(ll i=1;i<n;++i)
+    {
+        st[par[i]].insert({-subSize[i],i,subSum[i]});
+    }
+    while(m--)
+    {
+        ll t,x;
+        cin>>t>>x;
+        x--;
+        if(t==1)
         {
-            cin>>a[i];
+            cout<<subSum[x]<<'\n';
         }
-        for(ll i=0;i<n;++i)
+        else
         {
-            cin>>b[i];
-        }
-        for(ll i=0;i<n;++i)
-        {
-            ll left=1,right=1,l=-1;
-            if(i>0)
+            if(!st[x].empty())
             {
-                left=gcd(a[i],a[i-1]);
+                auto it=st[x].begin();
+                array<ll,3> son=*it;
+                st[x].erase(it);
+                ll f=par[x],s=son[1];
+                st[f].erase(st[f].find({-subSize[x],x,subSum[x]}));
+                st[f].insert({-subSize[x],s,subSum[x]});
+                par[s]=f;
+                par[x]=s;
+                ll old_x_sz=subSize[x],old_x_sum=subSum[x],old_s_sz=subSize[s],old_s_sum=subSum[s];
+                subSize[x]-=old_s_sz;
+                subSum[x]-=old_s_sum;
+                subSum[s]+=subSum[x];
+                subSize[s]+=subSize[x];
+                st[s].insert({-subSize[x],x,subSum[x]});
             }
-            if(i+1<n)
-            {
-                right=gcd(a[i],a[i+1]);
-            }
-            l=lcm(left,right);
-            if(l<=b[i])
-            {
-                c[i]=l;
-            }
-            else
-            {
-                c[i]=a[i];
-            }
-        }
-        for(ll j=0;j<M;++j)
-        {
-            ll cur=((j!=0)?c[0]*primes[j]:a[0]);
-            ll score=(cur!=a[0]);
-            if(cur<=b[0]||cur==a[0])
-            {
-                dp[1][j]=score;
-            }
-        }
-        for(ll i=1;i<n;++i)
-        {
-            dp[i+1][0]=max(dp[i+1][0],dp[i][0]);
-            for(ll prev=0;prev<M;++prev)
-            {
-                for(ll curr=0;curr<M;++curr)
-                {
-                    ll pvs=((prev!=0)?primes[prev]*c[i-1]:a[i-1]),cur=((curr!=0)?c[i]*primes[curr]:a[i]);
-                    ll score=(cur!=a[i]);
-                    if(gcd(pvs,cur)==gcd(a[i-1],a[i])&&(pvs<=b[i-1]||pvs==a[i-1])&&(cur<=b[i]||cur==a[i]))
-                    {
-                        dp[i+1][curr]=max(dp[i+1][curr],dp[i][prev]+score);
-                    }
-                }
-            }
-        }
-        cout<<(*max_element(dp[n],dp[n]+M))<<'\n';
-        for(ll i=0;i<=n;++i)
-        {
-            a[i]=b[i]=c[i]=0;
-            memset(dp[i],0,sizeof(dp[i]));
         }
     }
     return 0;
 }
-
