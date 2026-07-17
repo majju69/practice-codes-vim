37cfb9d05d56448945b4275e52793120a01f75f5
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-07-17 18:50:10 +0530

    https://codeforces.com/contest/2236/problem/F1 accepted

--- a.cpp
+++ a.cpp
@@ -7,85 +7,35 @@ using namespace std;
     #define debug(x)
 #endif
 
-const int N=2e5+10;
-int p[N],a[N],mn[N],mx[N];
-bool ok[N];
-vector<int> adj[N];
+vector<int> lpf(500001,0);
+vector<int> primes;
 
-bool checkCircularSorted(vector<int> &a)
+void leastPrimeFactor()
 {
-    const int n=a.size();
-    int cnt=0;
-    for(int i=1;i<n;++i)
+    int n=lpf.size();
+    for(int i=2;i<n;++i)
     {
-        if(a[i-1]>a[i])
+        if(lpf[i]==0)
         {
-            cnt++;
-            if(cnt>1)
+            lpf[i]=i;
+            primes.push_back(i);
+        }
+        for(int j=0;i*primes[j]<n;++j)
+        {
+            lpf[i*primes[j]]=primes[j];
+            if(primes[j]==lpf[i])
             {
-                return 0;
+                break;
             }
         }
     }
-    if(cnt==1)
-    {
-        return (a[0]>a[n-1]);
-    }
-    return 1;
 }
 
-bool check(const vector<pair<int,int>> &a)
-{
-    const int n=a.size();
-    int ind=0;
-    vector<pair<int,int>> tmp=a;
-    vector<int> perm;
-    sort(tmp.begin(),tmp.end());
-    for(int i=1;i<n;++i)
-    {
-        if(tmp[i].first<tmp[i-1].second)
-        {
-            return 0;
-        }
-    }
-    for(auto &v:a)
-    {
-        perm.push_back(v.first);
-    }
-    return checkCircularSorted(perm);
-}
+const int mod=1e9+7;
 
-void dfs(int node)
+int mul(const int &a,const int &b)
 {
-    if(a[node]==-1)
-    {
-        mn[node]=1e9;
-        mx[node]=-1e9;
-    }
-    else
-    {
-        ok[node]=1;
-        mn[node]=a[node];
-        mx[node]=a[node];
-    }
-    bool node_ok=1;
-    vector<pair<int,int>> a; 
-    for(auto &v:adj[node])
-    {
-        if(v!=p[node])
-        {
-            dfs(v);
-            mn[node]=min(mn[node],mn[v]);
-            mx[node]=max(mx[node],mx[v]);
-            node_ok=(node_ok&&ok[v]);
-            a.push_back({mn[v],mx[v]});
-        }
-    }
-    if(node_ok)
-    {
-        node_ok=check(a);
-    }
-    ok[node]=node_ok;
+    return (1ll*(a%mod)*(b%mod))%mod;
 }
 
 int main()
@@ -93,34 +43,38 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    leastPrimeFactor();
     int tc;
     cin>>tc;
     while(tc--)
     {
-        int n;
-        cin>>n;
-        for(int i=1;i<n;++i)
+        int n,x,ans=1;
+        map<int,int> mp;
+        cin>>n>>x;
+        assert(x==1);
+        while(n--)
         {
-            int x;
-            cin>>x;
-            x--;
-            adj[i].push_back(x);
-            adj[x].push_back(i);
-            p[i]=x;
+            int cur;
+            cin>>cur;
+            while(cur>1)
+            {
+                const int p=lpf[cur];
+                if(!mp.count(p))
+                {
+                    mp[p]=1;
+                }
+                while(cur%p==0)
+                {
+                    mp[p]++;
+                    cur/=p;
+                }
+            }
         }
-        for(int i=0;i<n;++i)
+        for(auto &v:mp)
         {
-            cin>>a[i];
-            a[i]--;
+            ans=mul(ans,v.second);
         }
-        dfs(0);
-        cout<<(ok[0]?"YES":"NO")<<'\n';
-        for(int i=0;i<n;++i)
-        {
-            p[i]=a[i]=mn[i]=mx[i]=0;
-            ok[i]=0;
-            adj[i].clear();
-        }
+        cout<<ans<<'\n';
     }
     return 0;
 }
