bf4a53ba49ef5c3ebb64b45160c2403506f349d3
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-07-20 23:58:30 +0530

    https://codeforces.com/contest/1778/problem/D accepted

--- a.cpp
+++ a.cpp
@@ -7,49 +7,81 @@ using namespace std;
     #define debug(x)
 #endif
 
-typedef long long ll;
+const int mod=998244353;
+const int N=1e6+10;
+array<int,2> dp[N];
+
+int power(int a,int b)        // Use when mod is of order 10^9 or less
+{
+    int ans=1;
+    a=a%mod;
+    while(b)
+    {
+        if(b&1)
+        {
+            ans=(1ll*(ans%mod)*(a%mod))%mod;
+        }
+        a=(1ll*(a%mod)*(a%mod))%mod;
+        b>>=1;
+    }
+    return ans%mod;
+}
+
+int add(const int &a,const int &b)
+{
+    return ((a%mod)+(b%mod))%mod;
+}
+
+int mul(const int &a,const int &b)
+{
+    return (1ll*(a%mod)*(b%mod))%mod;
+}
+
+int sub(const int &a,const int &b)
+{
+    return ((a%mod)-(b%mod)+mod)%mod;
+}
+
+int divide(const int &a,const int &b)
+{
+    return mul(a,power(b,mod-2));
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ll tc;
+    int tc;
     cin>>tc;
     while(tc--)
     {
-        ll n,k,x,ans=0;
-        cin>>n>>k>>x;
-        ll dp[k+1],ndp[k+1];
-        memset(dp,0xc0,sizeof(dp));
-        dp[0]=0;
-        for(ll i=0;i<n;++i)
+        int n,cnt=0,h0=-1;
+        string s,t;
+        cin>>n>>s>>t;
+        for(int i=0;i<n;++i)
         {
-            ll cur;
-            cin>>cur;
-            ndp[0]=max(dp[0]+cur-x,cur-x);
-            for(ll j=1;j<=k;++j)
-            {
-                ndp[j]=max(dp[j]+cur-x,dp[j-1]+cur+x);
-                if(dp[j]>=(ll)-2e14)
-                {
-                    ndp[j]=max(ndp[j],cur-x);
-                }
-                if(dp[j-1]>=(ll)-2e14)
-                {
-                    ndp[j]=max(ndp[j],cur+x);
-                }
-            }
-            memcpy(dp,ndp,sizeof(dp));
-            for(ll j=0;j<=k;++j)
-            {
-                if(j+n-i-1>=k)
-                {
-                    ans=max(ans,dp[j]);
-                }
-            }
+            cnt+=(s[i]==t[i]);
         }
-        cout<<ans<<'\n';
+        dp[0]={0,1};
+        dp[1]={sub(0,1),1};
+        for(int i=1;i<n;++i)
+        {
+            const int a0=dp[i-1][0],a1=dp[i-1][1],b0=dp[i][0],b1=dp[i][1];
+            dp[i+1][0]=add(dp[i+1][0],mul(b0,n));
+            dp[i+1][0]=sub(dp[i+1][0],mul(a0,i));
+            dp[i+1][0]=sub(dp[i+1][0],n);
+            dp[i+1][0]=divide(dp[i+1][0],sub(n,i));
+            dp[i+1][1]=add(dp[i+1][1],mul(b1,n));
+            dp[i+1][1]=sub(dp[i+1][1],mul(a1,i));
+            dp[i+1][1]=divide(dp[i+1][1],sub(n,i));
+        }
+        h0=divide(sub(0,dp[n][0]),dp[n][1]);
+        cout<<add(dp[cnt][0],mul(dp[cnt][1],h0))<<'\n';
+        for(int i=0;i<=n;++i)
+        {
+            dp[i]={0,0};
+        }
     }
     return 0;
 }
