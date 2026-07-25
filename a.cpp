b7e9f4427934faae66423682f662dc09785905c9
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-07-26 01:36:19 +0530

    https://codeforces.com/contest/2241/problem/E accepted

--- a.cpp
+++ a.cpp
@@ -7,44 +7,35 @@ using namespace std;
     #define debug(x)
 #endif
 
-const int mod=998244353;
-const int N=1e6+10;
-array<int,2> dp[N];
+const int N=2e5+10,MX=1e6+10;
+int a[N],sub[N],par[N];
+vector<int> adj[N];
+bool isSquare[MX];
 
-int power(int a,int b)        // Use when mod is of order 10^9 or less
+void dfs(int node,int p=-1)
 {
-    int ans=1;
-    a=a%mod;
-    while(b)
+    sub[node]=1;
+    for(auto &v:adj[node])
     {
-        if(b&1)
+        if(v!=p)
         {
-            ans=(1ll*(ans%mod)*(a%mod))%mod;
+            par[v]=node;
+            dfs(v,node);
+            sub[node]+=sub[v];
         }
-        a=(1ll*(a%mod)*(a%mod))%mod;
-        b>>=1;
     }
-    return ans%mod;
 }
 
-int add(const int &a,const int &b)
+array<long long,3> get(const vector<int> &a)
 {
-    return ((a%mod)+(b%mod))%mod;
-}
-
-int mul(const int &a,const int &b)
-{
-    return (1ll*(a%mod)*(b%mod))%mod;
-}
-
-int sub(const int &a,const int &b)
-{
-    return ((a%mod)-(b%mod)+mod)%mod;
-}
-
-int divide(const int &a,const int &b)
-{
-    return mul(a,power(b,mod-2));
+    array<long long,3> ans={0,0,0};
+    for(auto &v:a)
+    {
+        ans[2]+=1ll*v*ans[1];
+        ans[1]+=1ll*v*ans[0];
+        ans[0]+=1ll*v;
+    }
+    return ans;
 }
 
 int main()
@@ -52,35 +43,57 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    for(int i=0;i*i<MX;++i)
+    {
+        isSquare[i*i]=1;
+    }
     int tc;
     cin>>tc;
     while(tc--)
     {
-        int n,cnt=0,h0=-1;
-        string s,t;
-        cin>>n>>s>>t;
+        int n;
+        long long ans=0;
+        cin>>n;
         for(int i=0;i<n;++i)
         {
-            cnt+=(s[i]==t[i]);
+            cin>>a[i];
         }
-        dp[0]={0,1};
-        dp[1]={sub(0,1),1};
         for(int i=1;i<n;++i)
         {
-            const int a0=dp[i-1][0],a1=dp[i-1][1],b0=dp[i][0],b1=dp[i][1];
-            dp[i+1][0]=add(dp[i+1][0],mul(b0,n));
-            dp[i+1][0]=sub(dp[i+1][0],mul(a0,i));
-            dp[i+1][0]=sub(dp[i+1][0],n);
-            dp[i+1][0]=divide(dp[i+1][0],sub(n,i));
-            dp[i+1][1]=add(dp[i+1][1],mul(b1,n));
-            dp[i+1][1]=sub(dp[i+1][1],mul(a1,i));
-            dp[i+1][1]=divide(dp[i+1][1],sub(n,i));
+            int u,v;
+            cin>>u>>v;
+            u--;
+            v--;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        h0=divide(sub(0,dp[n][0]),dp[n][1]);
-        cout<<add(dp[cnt][0],mul(dp[cnt][1],h0))<<'\n';
-        for(int i=0;i<=n;++i)
+        dfs(0);
+        for(int i=0;i<n;++i)
         {
-            dp[i]={0,0};
+            if(!isSquare[a[i]])
+            {
+                continue;
+            }
+            vector<int> cur;
+            for(auto &v:adj[i])
+            {
+                if(v!=par[i])
+                {
+                    cur.push_back(sub[v]);
+                }
+            }
+            if(i!=0)
+            {
+                cur.push_back(n-sub[i]);
+            }
+            array<long long,3> cur_ans=get(cur);
+            ans+=cur_ans[1]+cur_ans[2];
+        }
+        cout<<ans<<'\n';
+        for(int i=0;i<n;++i)
+        {
+            a[i]=sub[i]=par[i]=0;
+            adj[i].clear();
         }
     }
     return 0;
